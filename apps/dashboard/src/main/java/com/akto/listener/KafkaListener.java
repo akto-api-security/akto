5c019c3671ec9caab047e99e249d3f985e87ea57
Avneesh Hota <iamavneesh.hota@gmail.com>
2021-12-29 00:13:55 +0530

    har file can now be parsed and sent to kafka

---
+++ apps/dashboard/src/main/java/com/akto/listener/KafkaListener.java
@@ -0,0 +1,17 @@
+package com.akto.listener;
+
+
+import com.akto.kafka.Kafka;
+
+import javax.servlet.ServletContextListener;
+
+public class KafkaListener implements ServletContextListener {
+    public static Kafka kafka;
+    @Override
+    public void contextInitialized(javax.servlet.ServletContextEvent sce) {
+//        String brokerIP = System.getenv("AKTO_KAFKA_BROKER_URL");
+        String brokerIP = "172.18.0.4:9092";
+        kafka = new Kafka(brokerIP);
+
+    }
+}
