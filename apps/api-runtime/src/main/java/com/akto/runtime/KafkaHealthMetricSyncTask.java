bc7537291a4767b497030d3180599204c1bcdad7
Avneesh Hota <iamavneesh.hota@gmail.com>
2022-01-02 13:30:28 +0530

    wrapped run() of kafka schedular task with try catch

--- apps/api-runtime/src/main/java/com/akto/runtime/KafkaHealthMetricSyncTask.java
+++ apps/api-runtime/src/main/java/com/akto/runtime/KafkaHealthMetricSyncTask.java
@@ -5,27 +5,34 @@ import com.akto.dto.KafkaHealthMetric;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ReplaceOptions;
 import org.bson.conversions.Bson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class KafkaHealthMetricSyncTask implements Runnable{
     public Map<String,KafkaHealthMetric> kafkaHealthMetricsMap = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(KafkaHealthMetricSyncTask.class);
     @Override
     public void run() {
-        System.out.println("SYNCING");
-        for (String key: kafkaHealthMetricsMap.keySet()) {
-            KafkaHealthMetric kafkaHealthMetric = kafkaHealthMetricsMap.get(key);
-            Bson filter = Filters.and(
-                    Filters.eq(KafkaHealthMetric.TOPIC_NAME, kafkaHealthMetric.getTopicName()),
-                    Filters.eq(KafkaHealthMetric.PARTITION, kafkaHealthMetric.getPartition())
-            );
+        try {
+            logger.info("SYNCING");
+            for (String key: kafkaHealthMetricsMap.keySet()) {
+                KafkaHealthMetric kafkaHealthMetric = kafkaHealthMetricsMap.get(key);
+                Bson filter = Filters.and(
+                        Filters.eq(KafkaHealthMetric.TOPIC_NAME, kafkaHealthMetric.getTopicName()),
+                        Filters.eq(KafkaHealthMetric.PARTITION, kafkaHealthMetric.getPartition())
+                );
 
-            ReplaceOptions replaceOptions = new ReplaceOptions();
-            replaceOptions.upsert(true);
+                ReplaceOptions replaceOptions = new ReplaceOptions();
+                replaceOptions.upsert(true);
 
-            KafkaHealthMetricsDao.instance.getMCollection().replaceOne(filter, kafkaHealthMetric, replaceOptions);
+                KafkaHealthMetricsDao.instance.getMCollection().replaceOne(filter, kafkaHealthMetric, replaceOptions);
+            }
+            logger.info("SYNC DONE");
+        } catch (Exception e) {
+            logger.error("ERROR in kafka data sync from api runtime", e);
         }
-        System.out.println("SYNC DONE");
     }
 }
