228697cb31ffce24ffd40d6db5274b2e3d015add
Avneesh Hota <iamavneesh.hota@gmail.com>
2022-01-02 13:11:14 +0530

    wrapped filter with try catch

--- apps/dashboard/src/main/java/com/akto/filter/InfraMetricsFilter.java
+++ apps/dashboard/src/main/java/com/akto/filter/InfraMetricsFilter.java
@@ -3,19 +3,20 @@ package com.akto.filter;
 import com.akto.listener.InfraMetricsListener;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Timer;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InfraMetricsFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(InfraMetricsFilter.class);
+
     @Override
     public void init(FilterConfig filterConfig) { }
 
@@ -26,33 +27,34 @@ public class InfraMetricsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        long start = System.currentTimeMillis();
-
         filterChain.doFilter(request, response);
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        HttpServletRequest  httpServletRequest = (HttpServletRequest) request;
+        try {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            HttpServletRequest  httpServletRequest = (HttpServletRequest) request;
 
-        int statusCode = httpServletResponse.getStatus();
-        long duration = System.currentTimeMillis() - start;
-        String uri = httpServletRequest.getRequestURI();
-        String method = httpServletRequest.getMethod();
+            int statusCode = httpServletResponse.getStatus();
+            String uri = httpServletRequest.getRequestURI();
+            String method = httpServletRequest.getMethod();
 
-        ArrayList<Tag> tags = new ArrayList<>(Arrays.asList(
-                Tag.of("uri", uri),
-                Tag.of("method", method)
-        ));
+            ArrayList<Tag> tags = new ArrayList<>(Arrays.asList(
+                    Tag.of("uri", uri),
+                    Tag.of("method", method)
+            ));
 
-        if (statusCode >= 200 && statusCode< 300) {
-            tags.add(Tag.of("status", "good"));
-        } else {
-            tags.add(Tag.of("status", "bad"));
+            if (statusCode >= 200 && statusCode< 300) {
+                tags.add(Tag.of("status", "good"));
+            } else {
+                tags.add(Tag.of("status", "bad"));
+            }
+
+            Counter.builder("api_requests_total")
+                    .description("API Requests Total")
+                    .tags(tags)
+                    .register(InfraMetricsListener.registry)
+                    .increment();
+        } catch (Exception e) {
+            logger.error("Inframetrics filter Error: ", e);;
         }
-
-        Counter.builder("api_requests_total")
-                .description("API Requests Total")
-                .tags(tags)
-                .register(InfraMetricsListener.registry)
-                .increment();
 
     }
 
