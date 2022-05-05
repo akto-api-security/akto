a9efaf5b89f2afffcfecc2df08bfd9c31da29355
Ankush Jain <ankush@akto.io>
2022-05-05 16:54:25 +0530

    Front end for Slack

--- libs/dao/src/main/java/com/akto/dto/notifications/SlackWebhook.java
+++ libs/dao/src/main/java/com/akto/dto/notifications/SlackWebhook.java
@@ -1,18 +1,28 @@
 package com.akto.dto.notifications;
 
+import org.bson.codecs.pojo.annotations.BsonId;
+
 public class SlackWebhook {
+
+    @BsonId
+    int id;
 
     String webhook;
     int smallerDuration;
     int largerDuration;
+    int timestamp;
+    String userEmail;
 
     public SlackWebhook() {
     }
 
-    public SlackWebhook(String webhook, int smallerDuration, int largerDuration) {
+    public SlackWebhook(String webhook, int smallerDuration, int largerDuration, int timestamp, String userEmail, int id) {
         this.webhook = webhook;
         this.smallerDuration = smallerDuration;
         this.largerDuration = largerDuration;
+        this.timestamp = timestamp;
+        this.userEmail = userEmail;
+        this.id = id;
     }
 
     public String getWebhook() {
@@ -39,14 +49,40 @@ public class SlackWebhook {
         this.largerDuration = largerDuration;
     }
 
+    public int getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUserEmail() {
+        return this.userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "{" +
             " webhook='" + getWebhook() + "'" +
             ", smallerDuration='" + getSmallerDuration() + "'" +
             ", largerDuration='" + getLargerDuration() + "'" +
+            ", timestamp='" + getTimestamp() + "'" +
+            ", userEmail='" + getUserEmail() + "'" +
+            ", id='" + getId() + "'" +
             "}";
     }
 
-    
 }
