7eac86074be6877dede3118468ec2bb592fd81e7
Avneesh Hota <avneesh@akto.io>
2022-08-16 02:03:38 +0530

    added my sms integration

--- apps/dashboard/src/main/java/com/akto/action/misc/OtpAction.java
+++ apps/dashboard/src/main/java/com/akto/action/misc/OtpAction.java
@@ -4,10 +4,20 @@ import com.akto.action.UserAction;
 import com.akto.dao.OtpMessagesDao;
 import com.akto.dao.context.Context;
 import com.akto.dto.OTPMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.model.Filters;
 import com.opensymphony.xwork2.Action;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,20 +48,110 @@ public class OtpAction extends UserAction {
         List<OTPMessage> OTPMessageList = OtpMessagesDao.instance.findAll(Filters.gte("timestamp", Context.now() - 90));
         if (OTPMessageList.isEmpty()) return Action.ERROR.toUpperCase();
 
-        OTPMessage OTPMessage = OTPMessageList.get(OTPMessageList.size()-1); // latest
+        OTPMessage otpMessage = OTPMessageList.get(OTPMessageList.size()-1); // latest
 
-        Pattern pattern = Pattern.compile("(\\d{6})");
-        Matcher matcher = pattern.matcher(OTPMessage.getMessage());
-        String val = null;
-        if (matcher.find()) {
-            val = matcher.group(0);
-        }
+        String val = extractOtp(otpMessage.getMessage());
         if (val == null || val.isEmpty()) return ERROR.toUpperCase();
 
         otp = val;
         System.out.println("found otp: " + otp);
 
         return SUCCESS.toUpperCase();
+    }
+
+    private String extractOtp(String message) {
+        if (!message.contains("OTP") && !message.contains("otp")) return null;
+        Pattern pattern = Pattern.compile("(\\d{6})");
+        Matcher matcher = pattern.matcher(message);
+        String val = null;
+        if (matcher.find()) {
+            val = matcher.group(0);
+        }
+
+        return val;
+    }
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private Integer latestMessageId = null;
+    public String fetchLatestMessageId() {
+        BasicDBObject result;
+        try {
+            result = makeRequestToMySms();
+            System.out.println("****");
+            System.out.println(result);
+            System.out.println("****");
+
+            List<Map> messages = (List<Map>) result.get("messages");
+            if (messages.size() == 0) return SUCCESS.toUpperCase();
+
+            latestMessageId = (Integer) messages.get(0).get("messageId");
+        } catch (Exception e) {
+            return ERROR.toUpperCase();
+        }
+
+        return SUCCESS.toUpperCase();
+    }
+
+    private Integer lastMessageId;
+    public String fetchOtpFromMySms() {
+        try {
+            BasicDBObject result = makeRequestToMySms();
+
+            System.out.println("((((");
+            System.out.println(result);
+            System.out.println("((((");
+
+            List<Map> messages = (List<Map>) result.get("messages");
+
+            Integer messageId = (Integer) messages.get(0).get("messageId");
+            if (Objects.equals(messageId, lastMessageId)) return ERROR.toUpperCase();
+
+            String message = (String) messages.get(0).get("message");
+
+            String val = extractOtp(message);
+            if (val == null || val.isEmpty()) return ERROR.toUpperCase();
+
+            otp = val;
+            System.out.println("found otp: " + otp);
+
+        } catch (Exception e) {
+            return ERROR.toUpperCase();
+        }
+
+        return SUCCESS.toUpperCase();
+    }
+
+    private String apiKey ;
+    private String authToken;
+    private String address;
+    private BasicDBObject makeRequestToMySms() throws Exception {
+        String path = "https://app.mysms.com/json/user/message/get/by/conversation";
+
+        URL url = new URL(path);
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // PUT is another valid option
+        http.setDoOutput(true);
+
+        BasicDBObject req = new BasicDBObject();
+        req.put("apiKey", apiKey);
+        req.put("authToken", authToken);
+        req.put("address", address);
+
+        String json = req.toJson();
+
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(json.getBytes(StandardCharsets.UTF_8));
+        }
+
+        System.out.println(http.getResponseCode());
+        InputStream inputStream = http.getInputStream();
+
+
+        return mapper.readValue(inputStream, BasicDBObject.class);
     }
 
     public String getOtp() {
@@ -64,5 +164,25 @@ public class OtpAction extends UserAction {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void setLastMessageId(Integer lastMessageId) {
+        this.lastMessageId = lastMessageId;
+    }
+
+    public Integer getLatestMessageId() {
+        return latestMessageId;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
