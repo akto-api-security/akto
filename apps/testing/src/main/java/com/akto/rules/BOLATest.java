781dcd23c4025c984b16672f5ae3151550b13c3e
Avneesh Hota <avneesh@akto.io>
2022-09-17 11:41:29 +0530

    change confidence to HIGH if non vulnerable

--- apps/testing/src/main/java/com/akto/rules/BOLATest.java
+++ apps/testing/src/main/java/com/akto/rules/BOLATest.java
@@ -3,15 +3,14 @@ package com.akto.rules;
 import com.akto.dto.*;
 import com.akto.dto.testing.AuthMechanism;
 import com.akto.dto.testing.TestResult;
+import com.akto.dto.testing.TestResult.Confidence;
 import com.akto.dto.type.SingleTypeInfo;
-import com.akto.store.AuthMechanismStore;
-import com.akto.store.SampleMessageStore;
 import com.akto.testing.ApiExecutor;
 import com.akto.testing.StatusCodeAnalyser;
+
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class BOLATest extends TestPlugin {
@@ -27,6 +26,7 @@ public class BOLATest extends TestPlugin {
         boolean vulnerable = false;
         ExecutorResult result = null;
         TestResult.TestError testError = null;
+
         for (RawApi rawApi: filteredMessages) {
             result = execute(rawApi, apiInfoKey, authMechanism);
             testError = result.testError;
@@ -74,6 +74,10 @@ public class BOLATest extends TestPlugin {
         authMechanism.addAuthToRequest(originalHttpRequest);
 
         ContainsPrivateResourceResult containsPrivateResourceResult = containsPrivateResource(originalHttpRequest, apiInfoKey);
+        // We consider API contains private resources if : 
+        //      a) Contains 1 or more private resources
+        //      b) We couldn't find uniqueCount or publicCount for some of the request params
+        // When it comes to case b we still say private resource but with low confidence, hence the below line
         TestResult.Confidence confidence = containsPrivateResourceResult.findPrivateOnes().size() > 0 ? TestResult.Confidence.HIGH : TestResult.Confidence.LOW;
 
         OriginalHttpResponse response;
@@ -86,6 +90,9 @@ public class BOLATest extends TestPlugin {
         int statusCode = StatusCodeAnalyser.getStatusCode(response.getBody(), response.getStatusCode());
         double percentageMatch = compareWithOriginalResponse(originalHttpResponse.getBody(), response.getBody());
         boolean vulnerable = isStatusGood(statusCode) && !containsPrivateResourceResult.isPrivate && percentageMatch > 90;
+
+        // We can say with high confidence if an api is not vulnerable and we don't need help of private resources for this
+        if (!vulnerable) confidence = Confidence.HIGH;
 
         return new ExecutorResult(vulnerable,confidence, containsPrivateResourceResult.singleTypeInfos, percentageMatch, rawApi, null);
 
