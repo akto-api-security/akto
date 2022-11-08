a566be7d3a8019ced7f95618a54ef72c83b8a27b
shivamrawat101192 <shivam@akto.io>
2022-11-08 14:21:29 +0530

    Changed key to ApiKeyInfo deleted EndpointParameters.java and modified the id parameter

--- libs/dao/src/main/java/com/akto/dto/test_run_findings/TestingIssuesId.java
+++ libs/dao/src/main/java/com/akto/dto/test_run_findings/TestingIssuesId.java
@@ -3,11 +3,29 @@ package com.akto.dto.test_run_findings;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import static com.akto.util.enums.GlobalEnums.*;
+import com.akto.dto.ApiInfo.ApiInfoKey;
 
 public class TestingIssuesId {
+//    public static final String ENDPOINT_PARAMETERS = "endpoint_parameters";
+//    @BsonProperty(value = ENDPOINT_PARAMETERS)
 
-    public EndpointParameters getEndpointParameters() {
-        return endpointParameters;
+//    public static final String ISSUE_SOURCE = "issue_source";
+//    @BsonProperty(value = ISSUE_SOURCE)
+
+//    public static final String TEST_CATEGORY = "test_category";
+//    @BsonProperty(value = TEST_CATEGORY)
+
+    private final ApiInfoKey apiInfoKey;
+    private final TestErrorSource testErrorSource;
+    private final TestCategory testCategory;
+    public TestingIssuesId(ApiInfoKey apiInfoKey, TestErrorSource source, TestCategory category) {
+        this.apiInfoKey = apiInfoKey;
+        this.testErrorSource = source;
+        this.testCategory = category;
+    }
+
+    public ApiInfoKey getApiInfoKey() {
+        return apiInfoKey;
     }
 
     public TestErrorSource getTestErrorSource() {
@@ -18,23 +36,5 @@ public class TestingIssuesId {
         return testCategory;
     }
 
-
-    TestingIssuesId(EndpointParameters endpointParameters, TestErrorSource source, TestCategory category) {
-        this.endpointParameters = endpointParameters;
-        this.testErrorSource = source;
-        this.testCategory = category;
-    }
-
-    public static final String ENDPOINT_PARAMETERS = "endpoint_parameters";
-    @BsonProperty(value = ENDPOINT_PARAMETERS)
-    private final EndpointParameters endpointParameters;
-
-    public static final String ISSUE_SOURCE = "issue_source";
-    @BsonProperty(value = ISSUE_SOURCE)
-    private final TestErrorSource testErrorSource;
-
-    public static final String TEST_CATEGORY = "test_category";
-    @BsonProperty(value = TEST_CATEGORY)
-    private final TestCategory testCategory;
 
 }
--- libs/dao/src/main/java/com/akto/dto/test_run_findings/TestingRunIssues.java
+++ libs/dao/src/main/java/com/akto/dto/test_run_findings/TestingRunIssues.java
@@ -1,42 +1,39 @@
 package com.akto.dto.test_run_findings;
 
 import com.akto.util.enums.GlobalEnums;
-import org.bson.codecs.pojo.annotations.BsonProperty;
 
 public class TestingRunIssues {
+    //    public static final String TESTING_ISSUES_ID = "testing_issues_id";
+    //    @BsonProperty(value = TESTING_ISSUES_ID)
+
+    //    public static final String ISSUE_SEVERITY = "i_s";
+    //    @BsonProperty(value = ISSUE_SEVERITY)
+
+    //    public static final String CREATION_TIME = "c_t";
+    //    @BsonProperty(value = CREATION_TIME)
+
+    //    public static final String START_TIME = "s_t";
+    //    @BsonProperty(value = START_TIME)
 
 
-    public static final String TESTING_ISSUES_ID = "testing_issues_id";
-    @BsonProperty(value = TESTING_ISSUES_ID)
-    private final TestingIssuesId testingIssuesId;
-
-    public static final String ISSUE_SEVERITY = "i_s";
-    @BsonProperty(value = ISSUE_SEVERITY)
+    //    public static final String TEST_RUN_ISSUES = "t_r_i";
+    //    @BsonProperty(value = TEST_RUN_ISSUES)
+    private GlobalEnums.TestRunIssueStatus testRunIssueStatus;
+    private final TestingIssuesId id;
+    private final int startTime;
+    private final int creationTime;
     private final GlobalEnums.Severity severity;
 
-    public static final String CREATION_TIME = "c_t";
-    @BsonProperty(value = CREATION_TIME)
-    private final int creationTime;
-
-    public static final String START_TIME = "s_t";
-    @BsonProperty(value = START_TIME)
-    private final int startTime;
-
-
-    public static final String TEST_RUN_ISSUES = "t_r_i";
-    @BsonProperty(value = TEST_RUN_ISSUES)
-    private GlobalEnums.TestRunIssueStatus testRunIssueStatus;
-
-    TestingRunIssues(TestingIssuesId testingIssuesId, GlobalEnums.Severity severity,
+    public TestingRunIssues(TestingIssuesId id, GlobalEnums.Severity severity,
                      int creationTime, int startTime) {
         this.creationTime = creationTime;
         this.startTime = startTime;
-        this.testingIssuesId = testingIssuesId;
+        this.id = id;
         this.severity = severity;
     }
 
-    public TestingIssuesId getTestingIssuesId() {
-        return testingIssuesId;
+    public TestingIssuesId getId() {
+        return id;
     }
 
     public int getCreationTime() {
