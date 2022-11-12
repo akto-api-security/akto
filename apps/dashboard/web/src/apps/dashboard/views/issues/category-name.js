e7fe51bbc8a905e7c837a2be7785ecdae9579e3e
shivamrawat101192 <shivam@akto.io>
2022-11-12 18:47:14 +0530

    Deleting unnecessary js files

--- apps/dashboard/web/src/apps/dashboard/views/issues/category-name.js
+++
@@ -1,6 +0,0 @@
-export default {
-  BOLA: "Broken Object Level Authorization (BOLA)",
-  ADD_USER_ID: "Broken Object Level Authorization (BOLA)",
-  PRIVILEGE_ESCALATION: "Broken Object Level Authorization (BOLA)",
-  NO_AUTH: "Broken User Authentication (BUA)"
-}
