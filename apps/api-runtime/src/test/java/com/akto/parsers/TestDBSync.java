ba7d25dee4eddba208c6bc18a23aae3ee73c6ee8
Ankush Jain <ankush@akto.io>
2022-11-16 03:22:23 -0800

    fix test

--- apps/api-runtime/src/test/java/com/akto/parsers/TestDBSync.java
+++ apps/api-runtime/src/test/java/com/akto/parsers/TestDBSync.java
@@ -420,16 +420,12 @@ public class TestDBSync extends MongoBasedTest {
         httpCallParser.filterHttpResponseParams(Collections.singletonList(h4));
 
         apiCollections = ApiCollectionsDao.instance.findAll(new BasicDBObject());
-        Assertions.assertEquals(apiCollections.size(),6);
+        Assertions.assertEquals(apiCollections.size(),5);
 
         id = domain4.hashCode();
         id += 1; // since duplicate so increased by 1 will work
         ApiCollection apiCollection4 = ApiCollectionsDao.instance.findOne(Filters.eq("_id", id));
-        Assertions.assertEquals(apiCollection4.getVxlanId(), 0);
-        Assertions.assertEquals(apiCollection4.getHostName(), domain4);
-        Assertions.assertEquals(httpCallParser.getHostNameToIdMap().size(),5);
-        Assertions.assertEquals(httpCallParser.getHostNameToIdMap().get(domain4), id);
-
+        Assertions.assertNull(apiCollection4);
     }
 
     @Test
