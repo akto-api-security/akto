fe21472fbc8cbe6017e58fd9b69e1c93402e7819
shivamrawat101192 <shivam@akto.io>
2022-11-19 20:15:07 +0530

    test cases for IssuesAction

--- apps/dashboard/src/test/java/com/akto/action/testing_issues/IssuesActionTest.java
+++ apps/dashboard/src/test/java/com/akto/action/testing_issues/IssuesActionTest.java
@@ -1,26 +1,93 @@
 package com.akto.action.testing_issues;
 
 import com.akto.MongoBasedTest;
-import com.akto.action.testing_issues.IssuesAction;
+import com.akto.dao.ApiCollectionsDao;
+import com.akto.dao.context.Context;
+import com.akto.dao.testing_run_findings.TestingRunIssuesDao;
+import com.akto.dto.ApiCollection;
+import com.akto.dto.ApiInfo;
+import com.akto.dto.test_run_findings.TestingIssuesId;
 import com.akto.dto.test_run_findings.TestingRunIssues;
-import org.junit.Assert;
+import com.akto.dto.type.URLMethods;
+import com.akto.util.enums.GlobalEnums;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
 
 public class IssuesActionTest extends MongoBasedTest {
 
     @Test
     public void fetchAllIssues () {
 
-        List<TestingRunIssues> list = new ArrayList<>();
-        list.add(new TestingRunIssues());
+        TestingRunIssues issue = new TestingRunIssues(
+                new TestingIssuesId(
+                        new ApiInfo.ApiInfoKey(
+                                123,
+                                "url",
+                                URLMethods.Method.POST
+                        ),
+                        GlobalEnums.TestErrorSource.AUTOMATED_TESTING,
+                        GlobalEnums.TestCategory.BOLA
+                ),
+                GlobalEnums.Severity.HIGH,
+                GlobalEnums.TestRunIssueStatus.OPEN,
+                Context.now(),
+                Context.now()
+        );
 
+        ApiCollection collection = new ApiCollection(
+                123,
+                "name",
+                Context.now(),
+                new HashSet<>(Arrays.asList("url1","url2")),
+                "hostName",
+                12345
+        );
+
+        TestingRunIssuesDao.instance.insertOne(issue);
+        ApiCollectionsDao.instance.insertOne(collection);
         IssuesAction action = new IssuesAction();
-        String success = action.fetchAllIssues();
-        action.setIssues(action.getIssues());
-        action.setCollections(action.getCollections());
-        Assert.assertTrue("success".equalsIgnoreCase(success));
+        action.fetchAllIssues();
+        assertEquals(issue.getId(), action.getIssues().get(0).getId());
+        assertEquals(collection.getId(), action.getCollections().get(0).getId());
+
+        issue = new TestingRunIssues(
+                new TestingIssuesId(
+                        new ApiInfo.ApiInfoKey(
+                                1234,
+                                "url",
+                                URLMethods.Method.POST
+                        ),
+                        GlobalEnums.TestErrorSource.AUTOMATED_TESTING,
+                        GlobalEnums.TestCategory.BOLA
+                ),
+                GlobalEnums.Severity.HIGH,
+                GlobalEnums.TestRunIssueStatus.OPEN,
+                Context.now(),
+                Context.now()
+        );
+
+        ArrayList<TestingRunIssues> list = new ArrayList<>();
+        list.add(issue);
+        action.setIssues(list);
+        collection = new ApiCollection(
+                1234,
+                "name",
+                Context.now(),
+                new HashSet<>(Arrays.asList("url1","url2")),
+                "hostName",
+                12345
+        );
+
+        ArrayList<ApiCollection> listOfCollection = new ArrayList<>();
+        listOfCollection.add(collection);
+        action.setCollections(listOfCollection);
+
+        assertEquals(issue.getId(), action.getIssues().get(0).getId());
+        assertEquals(collection.getId(), action.getCollections().get(0).getId());
     }
 }
