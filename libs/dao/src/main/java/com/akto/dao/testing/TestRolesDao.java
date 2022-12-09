c0e43c844f8a87f3be221bbc6e1447129ab0b394
shivamrawat101192 <shivam@akto.io>
2022-12-09 13:07:54 +0530

    Index creation on name of TestRoles Error messages for role creation

--- libs/dao/src/main/java/com/akto/dao/testing/TestRolesDao.java
+++ libs/dao/src/main/java/com/akto/dao/testing/TestRolesDao.java
@@ -1,8 +1,13 @@
 package com.akto.dao.testing;
 
 import com.akto.dao.AccountsContextDao;
+import com.akto.dao.context.Context;
 import com.akto.dto.testing.TestRoles;
 import com.akto.util.enums.MongoDBEnums;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 
 public class TestRolesDao extends AccountsContextDao<TestRoles> {
@@ -15,6 +20,32 @@ public class TestRolesDao extends AccountsContextDao<TestRoles> {
     @Override
     public Class<TestRoles> getClassT() {
         return TestRoles.class;
+    }
+    public void createIndicesIfAbsent() {
+
+        boolean exists = false;
+        for (String col: clients[0].getDatabase(Context.accountId.get()+"").listCollectionNames()){
+            if (getCollName().equalsIgnoreCase(col)){
+                exists = true;
+                break;
+            }
+        };
+
+        if (!exists) {
+            clients[0].getDatabase(Context.accountId.get()+"").createCollection(getCollName());
+        }
+
+        MongoCursor<Document> cursor = instance.getMCollection().listIndexes().cursor();
+        int counter = 0;
+        while (cursor.hasNext()) {
+            counter++;
+            cursor.next();
+        }
+
+        if (counter == 1) {//Only _id as index available
+            String[] fieldNames = {TestRoles.NAME};
+            instance.getMCollection().createIndex(Indexes.ascending(fieldNames), new IndexOptions().unique(true));
+        }
     }
 
     public TestRoles createTestRole (String roleName, ObjectId endpointLogicalGroupId) {
