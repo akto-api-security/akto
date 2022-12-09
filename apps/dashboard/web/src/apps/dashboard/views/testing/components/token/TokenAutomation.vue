dcf058ce6e76e79a8fc61e743edbe214d457fedb
ayushaga14 <ayush@akto.io>
2022-12-09 12:13:44 +0530

    commit ui file

---
+++ apps/dashboard/web/src/apps/dashboard/views/testing/components/token/TokenAutomation.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import func from '@/util/func'
+import obj from '@/util/obj'
+import testing from '@/util/testing'
+
+import {mapState} from 'vuex'
+
+export default {
+    name: "TokenAutomation",
+    props: {
+    },
+    data() {
+        return {
+
+        }
+    },
+    components: {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
