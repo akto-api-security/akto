9400bf7d53b0563da6d3be403c8d7149a33efdf3
shivamrawat101192 <shivam@akto.io>
2022-12-26 14:35:13 +0530

    Test roles unit test case

--- apps/dashboard/src/test/java/com/akto/action/testing/TestRolesActionTest.java
+++ apps/dashboard/src/test/java/com/akto/action/testing/TestRolesActionTest.java
@@ -1,12 +1,74 @@
 package com.akto.action.testing;
 
 import com.akto.MongoBasedTest;
+import com.akto.dto.User;
+import com.akto.dto.data_types.Conditions;
+import com.akto.dto.data_types.Predicate;
+import com.akto.dto.testing.TestRoles;
+import com.mongodb.BasicDBObject;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestRolesActionTest extends MongoBasedTest {
 
+    /*
+    *
+    * */
     @Test
     public void testRoleCreationFlow () {
+        TestRolesAction action = new TestRolesAction();
+        action.setRoleName("admin");
+
+
+        Map<String,Object> session = new HashMap<>();
+        User user = new User();
+        user.setLogin("test@akto.io");
+        session.put("user",user);
+        action.setSession(session);
+
+        TestRolesAction.RolesConditionUtils orConditionUtils = new TestRolesAction.RolesConditionUtils();
+        orConditionUtils.setOperator(Conditions.Operator.OR);
+        List<BasicDBObject> list = new ArrayList<>();
+        list.add(new BasicDBObject()
+                .append(Predicate.TYPE, Predicate.Type.CONTAINS.name())
+                .append(Predicate.VALUE, "contains"));
+        orConditionUtils.setPredicates(list);
+        TestRolesAction.RolesConditionUtils andConditionUtils = new TestRolesAction.RolesConditionUtils();
+        andConditionUtils.setOperator(Conditions.Operator.AND);
+        list.clear();
+        list.add(new BasicDBObject()
+                .append(Predicate.TYPE, Predicate.Type.CONTAINS.name())
+                .append(Predicate.VALUE, "contains"));
+        andConditionUtils.setPredicates(list);
+
+        action.setOrConditions(orConditionUtils);
+        action.setAndConditions(andConditionUtils);
+
+        if (TestRolesAction.SUCCESS.toUpperCase().equals(action.createTestRole())) {
+            assertEquals("admin", action.getSelectedRole().getName());
+        } else {
+            fail();
+        }
+
+        assertEquals(TestRolesAction.SUCCESS.toUpperCase(), action.fetchAllRolesAndLogicalGroups());
+        TestRoles role = action.getTestRoles().get(0);
+        assertEquals("admin", role.getName());
+
+        list.clear();
+        list.add(new BasicDBObject()
+                .append(Predicate.TYPE, Predicate.Type.CONTAINS.name())
+                .append(Predicate.VALUE, "containsAnd"));
+        andConditionUtils.setPredicates(list);
+
+        assertEquals(TestRolesAction.SUCCESS.toUpperCase(), action.updateTestRoles());
+
 
     }
 
