207ad75ac15d01ba3b8f4c380da85dc6e0629d9b
Avneesh Hota <avneesh@akto.io>
2023-01-07 23:44:28 +0530

    backend for faster burp done

---
+++ apps/dashboard/src/main/java/com/akto/action/BurpJarAction.java
@@ -0,0 +1,138 @@
+package com.akto.action;
+
+import com.akto.dao.ApiTokensDao;
+import com.akto.dto.ApiToken;
+import com.mongodb.client.model.Filters;
+import kotlin.text.Charsets;
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+public class BurpJarAction extends UserAction implements ServletResponseAware, ServletRequestAware {
+
+
+    @Override
+    public String execute() throws IOException { //todo: remove exception
+        String host = servletRequest.getHeader("Origin"); //todo
+
+        ApiToken apiToken = ApiTokensDao.instance.findOne(
+                Filters.and(
+                        Filters.eq(ApiToken.USER_NAME, getSUser().getLogin()),
+                        Filters.eq(ApiToken.UTILITY, ApiToken.Utility.BURP)
+                )
+        );
+
+        // if api token not found generate one
+        if (apiToken == null) {
+            ApiTokenAction apiTokenAction = new ApiTokenAction();
+            apiTokenAction.setSession(this.getSession());
+            apiTokenAction.addBurpToken();
+            List<ApiToken> apiTokenList = apiTokenAction.getApiTokenList();
+            if (apiTokenList == null || apiTokenList.isEmpty()) {
+                addActionError("Couldn't generate burp token");
+                return ERROR.toUpperCase();
+            }
+            apiToken = apiTokenList.get(0);
+        }
+
+        String token = apiToken.getKey();
+
+        String collectionName = "Burp";
+
+        File tmpJarFile = File.createTempFile("temp", "jar");
+        URL url = this.getClass().getResource("/Akto.jar");
+
+        if (url == null) {
+            addActionError("Akto plugin not found!");
+            return ERROR.toUpperCase();
+        }
+
+        JarFile jarFile = new JarFile(url.getPath());
+        boolean jarUpdated = false;
+
+        File credFile = File.createTempFile("creds", "txt"); // todo: remove
+        FileUtils.writeStringToFile(credFile,host + "\n" + token + "\n" + collectionName, Charsets.UTF_8);
+
+        try {
+            try (JarOutputStream tempJarOutputStream = new JarOutputStream(Files.newOutputStream(tmpJarFile.toPath()))) {
+                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                Set<String> done = new HashSet<>();
+
+                // copy existing elements
+                while (jarEntries.hasMoreElements()) {
+                    JarEntry entry = jarEntries.nextElement();
+                    if (done.contains(entry.getName())) continue;
+                    done.add(entry.getName());
+
+                    InputStream entryInputStream = jarFile.getInputStream(entry);
+                    tempJarOutputStream.putNextEntry(entry);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    while ((bytesRead = entryInputStream.read(buffer)) != -1) {
+                        tempJarOutputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+
+                JarEntry entryNew = new JarEntry("credentials.txt");
+                InputStream entryInputStream = new FileInputStream(credFile);
+                tempJarOutputStream.putNextEntry(entryNew);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = 0;
+                while ((bytesRead = entryInputStream.read(buffer)) != -1) {
+                    tempJarOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                jarUpdated = true;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } finally {
+            jarFile.close();
+        }
+
+        servletResponse.setContentType("application/octet-stream");
+        servletResponse.setHeader("Content-Disposition", "filename=\"Akto.jar\"");
+        System.out.println("set header done");
+        File srcFile = new File(tmpJarFile.getPath());
+        FileUtils.copyFile(srcFile, servletResponse.getOutputStream());
+
+        System.out.println("done");
+
+        credFile.delete();
+        tmpJarFile.delete();
+
+        return null;
+    }
+
+    protected HttpServletResponse servletResponse;
+    @Override
+    public void setServletResponse(HttpServletResponse response) {
+        this.servletResponse= response;
+    }
+
+    protected HttpServletRequest servletRequest;
+
+    @Override
+    public void setServletRequest(HttpServletRequest request) {
+        this.servletRequest = request;
+    }
+}
