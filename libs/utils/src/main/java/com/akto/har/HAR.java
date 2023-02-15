d49bc3a5ee75f6379b35c6f0c9781343c8e8c0ba
shivamrawat101192 <shivam@akto.io>
2023-02-15 16:50:09 +0530

    Refactoring methods

--- libs/utils/src/main/java/com/akto/har/HAR.java
+++ libs/utils/src/main/java/com/akto/har/HAR.java
@@ -1,16 +1,20 @@
 package com.akto.har;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.akto.graphql.GraphQLUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import de.sstoehr.harreader.HarReader;
 import de.sstoehr.harreader.HarReaderException;
 import de.sstoehr.harreader.HarReaderMode;
 import de.sstoehr.harreader.model.*;
+import graphql.language.*;
+import graphql.parser.Parser;
+import graphql.validation.DocumentVisitor;
+import graphql.validation.LanguageTraversal;
+import org.mortbay.util.ajax.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.net.URLDecoder;
 import java.util.*;
 
 public class HAR {
@@ -19,6 +23,7 @@ public class HAR {
     private static final Logger logger = LoggerFactory.getLogger(Har.class);
     public static final String JSON_CONTENT_TYPE = "application/json";
     public static final String FORM_URL_ENCODED_CONTENT_TYPE = "application/x-www-form-urlencoded";
+
     public List<String> getMessages(String harString, int collection_id) throws HarReaderException {
         HarReader harReader = new HarReader();
         Har har = harReader.readFromString(harString, HarReaderMode.LAX);
@@ -29,24 +34,43 @@ public class HAR {
         List<String> entriesList =  new ArrayList<>();
         int idx=0;
         for (HarEntry entry: entries) {
+            List<OperationDefinition> operationDefinitions = GraphQLUtils.getUtils().parseGraphQLRequest(entry.getRequest().getPostData().getText());
             idx += 1;
-            try {
-                Map<String,String> result = getResultMap(entry);
-                if (result != null) {
-                    result.put("akto_vxlan_id", collection_id+"");
-                    entriesList.add(mapper.writeValueAsString(result));
+            if (operationDefinitions.isEmpty()) {
+                updateEntriesList(entry, null, null, null, collection_id, entriesList, idx);
+            } else {
+                for (OperationDefinition definition : operationDefinitions) {
+                    OperationDefinition.Operation operation = definition.getOperation();
+                    SelectionSet selectionSets = definition.getSelectionSet();
+                    List<Selection> selectionList = selectionSets.getSelections();
+                    for (Selection selection : selectionList) {
+                        if (selection instanceof Field) {
+                            Field field = (Field) selection;
+                            updateEntriesList(entry, operation, field.getName(), field, collection_id, entriesList, idx);
+                        }
+                    }
+
                 }
-                
-            } catch (Exception e) {
-                logger.error("Error while parsing har file on entry: " + idx + " ERROR: " + e);
-                errors.add("Error in entry " + idx);
             }
         }
-
         return entriesList;
     }
 
-    public static Map<String,String> getResultMap(HarEntry entry) throws Exception {
+    private void updateEntriesList(HarEntry entry, OperationDefinition.Operation operation, String fieldName, Field field1, int collection_id, List<String> entriesList, int idx) {
+        try {
+            Map<String,String> result = getResultMap(entry, operation, fieldName, field1);
+            logger.info("results map : {}", mapper.writeValueAsString(result));
+            result.put("akto_vxlan_id", collection_id +"");
+            entriesList.add(mapper.writeValueAsString(result));
+        } catch (Exception e) {
+            logger.error("Error while parsing har file on entry: " + idx + " ERROR: " + e);
+            errors.add("Error in entry " + idx);
+        }
+    }
+
+    public static Map<String,String> getResultMap(HarEntry entry,
+                                                  OperationDefinition.Operation operation,
+                                                  String fieldName, Field field) throws Exception {
         HarRequest request = entry.getRequest();
         HarResponse response = entry.getResponse();
         Date dateTime = entry.getStartedDateTime();
@@ -57,44 +81,33 @@ public class HAR {
         Map<String,String> requestHeaderMap = convertHarHeadersToMap(requestHarHeaders);
         Map<String,String> responseHeaderMap = convertHarHeadersToMap(responseHarHeaders);
 
-        String requestContentType = getContentType(requestHarHeaders);
+        String requestPayload = request.getPostData().getText();
 
-        String requestPayload;
-        if (requestContentType == null) {
-            // get request data from querystring
-            Map<String,Object> paramMap = new HashMap<>();
-            requestPayload = mapper.writeValueAsString(paramMap);
-        } else if (requestContentType.contains(JSON_CONTENT_TYPE)) {
-            String postData = request.getPostData().getText();
-            if (postData == null) {
-                postData = "{}";
-            }
+        if (requestPayload == null) requestPayload = "";
 
-            if (postData.startsWith("[")) {
-                requestPayload = postData;
-            } else {
-                Map<String,Object> paramMap = mapper.readValue(postData, new TypeReference<HashMap<String,Object>>() {});
-                requestPayload = mapper.writeValueAsString(paramMap);
-            }
-        } else if (requestContentType.contains(FORM_URL_ENCODED_CONTENT_TYPE)) {
-            String postText = request.getPostData().getText();
-            if (postText == null) {
-                postText = "";
-            }
-
-            requestPayload = postText;
-        } else {
-            return null;
+        String akto_account_id = 1_000_000 + ""; // TODO:
+        String path = getPath(request);
+        if (operation != null) {
+            path += "/" + operation.name().toLowerCase() + "/" + fieldName;
         }
 
+        if (field != null) {
+            Map<String, Object> map = GraphQLUtils.getUtils().fieldTraversal(field);
+            Object obj = JSON.parse(requestPayload);
+            if (obj instanceof Map) {
+                Map tempMap = (Map) obj;
+                for (String key : map.keySet()) {
+                    tempMap.put(key.substring(1), map.get(key));
+                }
+                requestPayload = JSON.toString(obj);
+            }
+        }
 
-        String akto_account_id = 1_000_000 + "";
-        String path = getPath(request);
         String requestHeaders = mapper.writeValueAsString(requestHeaderMap);
         String responseHeaders = mapper.writeValueAsString(responseHeaderMap);
         String method = request.getMethod().toString();
         String responsePayload = response.getContent().getText();;
-        String ip = "null"; 
+        String ip = "null"; // TODO:
         String time = (int) (dateTime.getTime() / 1000) + "";
         String statusCode = response.getStatus() + "";
         String type = request.getHttpVersion();
@@ -126,18 +139,18 @@ public class HAR {
             return false;
         }
         return contentType.contains(JSON_CONTENT_TYPE);
-     }
+    }
 
-     public static String getContentType(List<HarHeader> headers) {
-         for (HarHeader harHeader: headers) {
-             if (harHeader.getName().equalsIgnoreCase("content-type")) {
-                 return harHeader.getValue();
-             }
-         }
-         return null;
-     }
+    public static String getContentType(List<HarHeader> headers) {
+        for (HarHeader harHeader: headers) {
+            if (harHeader.getName().equalsIgnoreCase("content-type")) {
+                return harHeader.getValue();
+            }
+        }
+        return null;
+    }
 
-     public static Map<String,String> convertHarHeadersToMap(List<HarHeader> headers) {
+    public static Map<String,String> convertHarHeadersToMap(List<HarHeader> headers) {
         Map<String,String> headerMap = new HashMap<>();
 
         for (HarHeader harHeader: headers) {
@@ -147,9 +160,10 @@ public class HAR {
         }
 
         return headerMap;
-     }
+    }
 
     public static void addQueryStringToMap(List<HarQueryParam> params, Map<String,Object> paramsMap) {
+        // TODO: which will take preference querystring or post value
         for (HarQueryParam param: params) {
             paramsMap.put(param.getName(), param.getValue());
         }
