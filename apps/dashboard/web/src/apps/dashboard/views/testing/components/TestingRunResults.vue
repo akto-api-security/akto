0bf48e21ff4915b00afafe68fe77126db97428d9
Avneesh Hota <avneesh@akto.io>
2023-03-04 14:31:25 +0530

    added jokes to loading screen and auto refresh testing results page

--- apps/dashboard/web/src/apps/dashboard/views/testing/components/TestingRunResults.vue
+++ apps/dashboard/web/src/apps/dashboard/views/testing/components/TestingRunResults.vue
@@ -7,90 +7,104 @@
             <span>{{collectionName}}</span>
         </div>
 
-        <div class="testing-runs-history" v-if="!isWorkflow">
-            <div class="d-flex jc-end">
-                <date-range v-model="dateRange"/>
+        <div class="loading-bar" v-if="loading">
+            <div>
+                <spinner :size="50" color="#6200EA"/>
             </div>
-            <stacked-chart
-                type='column'
-                color='#FF000080'
-                :areaFillHex="false"
-                :height="250"
-                title="Test results"
-                :data="testResultsChartData()"
-                :defaultChartOptions="{legend:{enabled: false}}"
-                background-color="rgba(0,0,0,0.0)"
-                :text="true"
-                class="pa-5"
-                @dateClicked=dateClicked
-            />     
-            <div class="testing-results-header" v-if="currentTest">
-                <div>
-                    <span>Test results: </span>    
-                    <span>{{selectedDateStr()}}</span>
+            <div style="padding-top: 12px;">
+                <div class="joke-line" v-for="line in jokes[jokeIndex]">
+                    {{ line }}
                 </div>
-                <div style="display: flex; text-transform: capitalize;">
-                    <div v-if="this.currentTest.state">Test status: {{this.currentTest.state.toLowerCase()}}</div>
-                    <div v-if="this.currentTest.state === 'SCHEDULED' || this.currentTest.state === 'RUNNING' " style="padding-left: 6px; padding-top: 4px;">
-                        <v-progress-circular indeterminate color="primary" :size="12" :width="1.5"></v-progress-circular>
-                    </div>
-                    <span v-if="this.currentTest.state === 'COMPLETED'" style="padding-top: 4px;">
-                        <v-icon color="green" :size="14">$fas_check-circle</v-icon>
-                    </span>
-                </div>
-            </div>                  
-            <simple-table
-                :headers="testingRunResultsHeaders" 
-                :items="testingRunResultsItems" 
-                name="" 
-                sortKeyDefault="vulnerable" 
-                :sortDescDefault="true"
-                @rowClicked="openDetails"
-            >
-                <template #item.severity="{item}">
-                    <sensitive-chip-group 
-                        :sensitiveTags="item.severity ? [item.severity] : []" 
-                        :chipColor="getColor(item.severity)"
-                        :hideTag="true"
-                    />
-                </template>
-            
-            </simple-table>
+            </div>
+        </div>
 
-            <v-dialog v-model="openDetailsDialog">
-                <div class="details-dialog">
-                    <a-card
-                        title="Test details"
-                        color="rgba(33, 150, 243)"
-                        subtitle=""
-                        icon="$fas_stethoscope"
-                    >
-                        <template #title-bar>
-                            <v-btn
-                                plain
-                                icon
-                                @click="openDetailsDialog = false"
-                                style="margin-left: auto"
-                            >
-                                <v-icon>$fas_times</v-icon>
-                            </v-btn>
-                        </template>
-                        <div class="pa-4">
-                            <test-results-dialog 
-                                :similarlyAffectedIssues="similarlyAffectedIssues"
-                                :testingRunResult="testingRunResult"
-                                :subCatogoryMap="subCatogoryMap"
-                                :issuesDetails="dialogBoxIssue"
-                                isTestingPage
-                                :mapCollectionIdToName="mapCollectionIdToName"/>
-                        </div>
-                    </a-card>
-                </div>
-            </v-dialog>
-        </div>
         <div v-else>
-            <workflow-test-builder :endpointsList="[]" apiCollectionId=0 :originalStateFromDb="originalStateFromDb" :defaultOpenResult="true" class="white-background"/>
+            <div class="testing-runs-history" v-if="!isWorkflow">
+                <div class="d-flex jc-end">
+                    <date-range v-model="dateRange"/>
+                </div>
+                <stacked-chart
+                    type='column'
+                    color='#FF000080'
+                    :areaFillHex="false"
+                    :height="250"
+                    title="Test results"
+                    :data="testResultsChartData()"
+                    :defaultChartOptions="{legend:{enabled: false}}"
+                    background-color="rgba(0,0,0,0.0)"
+                    :text="true"
+                    class="pa-5"
+                    @dateClicked=dateClicked
+                />     
+                <div class="testing-results-header" v-if="currentTest">
+                    <div>
+                        <span>Test results: </span>    
+                        <span>{{selectedDateStr()}}</span>
+                    </div>
+                    <div style="display: flex; text-transform: capitalize;">
+                        <div v-if="this.currentTest.state">Test status: {{this.currentTest.state.toLowerCase()}}</div>
+                        <div v-if="this.currentTest.state === 'SCHEDULED' || this.currentTest.state === 'RUNNING' " style="padding-left: 6px; padding-top: 4px;">
+                            <v-progress-circular indeterminate color="primary" :size="12" :width="1.5"></v-progress-circular>
+                        </div>
+                        <span v-if="this.currentTest.state === 'COMPLETED'" style="padding-top: 4px;">
+                            <v-icon color="green" :size="14">$fas_check-circle</v-icon>
+                        </span>
+                    </div>
+                </div>                  
+                <simple-table
+                    :headers="testingRunResultsHeaders" 
+                    :items="testingRunResultsItems" 
+                    name="" 
+                    sortKeyDefault="vulnerable" 
+                    :sortDescDefault="true"
+                    @rowClicked="openDetails"
+                >
+                    <template #item.severity="{item}">
+                        <sensitive-chip-group 
+                            :sensitiveTags="item.severity ? [item.severity] : []" 
+                            :chipColor="getColor(item.severity)"
+                            :hideTag="true"
+                        />
+                    </template>
+                
+                </simple-table>
+
+                <v-dialog v-model="openDetailsDialog">
+                    <div class="details-dialog">
+                        <a-card
+                            title="Test details"
+                            color="rgba(33, 150, 243)"
+                            subtitle=""
+                            icon="$fas_stethoscope"
+                        >
+                            <template #title-bar>
+                                <v-btn
+                                    plain
+                                    icon
+                                    @click="openDetailsDialog = false"
+                                    style="margin-left: auto"
+                                >
+                                    <v-icon>$fas_times</v-icon>
+                                </v-btn>
+                            </template>
+                            <div class="pa-4">
+                                <test-results-dialog 
+                                    :similarlyAffectedIssues="similarlyAffectedIssues"
+                                    :testingRunResult="testingRunResult"
+                                    :subCatogoryMap="subCatogoryMap"
+                                    :issuesDetails="dialogBoxIssue"
+                                    isTestingPage
+                                    :mapCollectionIdToName="mapCollectionIdToName"/>
+                            </div>
+                        </a-card>
+                    </div>
+                </v-dialog>
+            </div>
+            <div v-else>
+                <workflow-test-builder :endpointsList="[]" apiCollectionId=0 :originalStateFromDb="originalStateFromDb" :defaultOpenResult="true" class="white-background"/>
+            </div>
         </div>
+
     </div>
 </template>
 
@@ -102,6 +116,7 @@ import SimpleTable from '@/apps/dashboard/shared/components/SimpleTable'
 import SensitiveChipGroup from '@/apps/dashboard/shared/components/SensitiveChipGroup'
 import TestResultsDialog from "./TestResultsDialog";
 import WorkflowTestBuilder from '../../observe/inventory/components/WorkflowTestBuilder'
+import Spinner from '@/apps/dashboard/shared/components/Spinner'
 
 import api from '../api'
 import issuesApi from '../../issues/api'
@@ -126,7 +141,8 @@ export default {
         SimpleTable,
         SensitiveChipGroup,
         TestResultsDialog,
-        WorkflowTestBuilder
+        WorkflowTestBuilder,
+        Spinner
     },
     data () {
         let endTimestamp = this.defaultEndTimestamp || func.timeNow()
@@ -169,7 +185,21 @@ export default {
             isWorkflow: false,
             originalStateFromDb: null,
             dialogBoxIssue: {},
-            similarlyAffectedIssues: []
+            similarlyAffectedIssues: [],
+            loading: true,
+            jokes: [
+                ["Looking for love in all the wrong places", "../../etc/pwd"],
+                ["May the API security be with you"],
+                ["Unsecured API: I will look for you, I will find you, and I will kill you"],
+                ["Why waste time use lot tool when few tool do trick", "Akto API Security"],
+                ["You can't handle the truth", "?id=1 OR 2=2"],
+                ["Break Bugs Not Hearts"],
+                ["Houston, we have API security!"],
+                ["Your mission: API security, should you choose to accept it?"],
+                ["API Security matters!!!"]
+            ],
+            refreshSummariesInterval: null,
+            refreshTestResultsInterval : null
         }
     },
     methods: {
@@ -231,7 +261,7 @@ export default {
             this.selectedDate = point / 1000
         },
         refreshSummaries() {
-            api.fetchTestingRunResultSummaries(this.startTimestamp, this.endTimestamp, this.testingRunHexId).then(resp => {
+            return api.fetchTestingRunResultSummaries(this.startTimestamp, this.endTimestamp, this.testingRunHexId).then(resp => {
                 if (resp.testingRun.testIdConfig == 1) {
                     this.isWorkflow = true
                     this.originalStateFromDb = resp.workflowTest
@@ -283,15 +313,46 @@ export default {
         },
     },
     async mounted() {
-        this.refreshSummaries()
         await this.$store.dispatch('issues/fetchAllSubCategories')
+        await this.refreshSummaries()
+
+        if (this.testingRunResultSummaries.length !== 0) {
+            this.loading = false
+        } else {
+            this.refreshSummariesInterval = setInterval(() => {
+                this.refreshSummaries().then(() => {
+                if (this.testingRunResultSummaries.length !== 0) {
+                    this.loading = false
+                    clearInterval(interval)
+                }
+                })
+            }, 5000)
+        }
+
+        this.refreshTestResultsInterval = setInterval(() => {
+            if (this.currentTest && (this.currentTest.state === "SCHEDULED" || this.currentTest.state === "RUNNING")) {
+                this.refreshSummaries()
+            }
+        }, 5000)
+
     },
+
+    destroyed() {
+        clearInterval(this.refreshTestResultsInterval)
+        clearInterval(this.refreshSummariesInterval)
+    },
+
     computed: {
         ...mapState('testing', ['testingRuns', 'pastTestingRuns']),
         subCatogoryMap: {
             get() {
                 return this.$store.state.issues.subCatogoryMap
             }
+        },
+        jokeIndex() {
+            let min = 0
+            let max = this.jokes.length - 1
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         },
         subCategoryFromSourceConfigMap: {
             get() {
@@ -368,4 +429,21 @@ export default {
     display: flex
     justify-content: space-between
     padding-right: 24px
+
+.loading-bar
+    display: flex
+    justify-content: center
+    height: 500px
+    align-items: center
+    flex-direction: column
+
+.joke-line
+    padding-top: 12px
+    align-items: center
+    display: flex
+    justify-content: center
+    color: #6200EA
+    font-weight: 500
+    font-size: 16px
+
 </style>
