2e50206e46d772029d2f67ee3903b9d7a4b243bb
SpeedX <>
2023-03-29 12:31:14 +0530

    add filetype checks

---
+++ apps/dashboard/src/test/java/com/akto/listener/TestFileTypes.java
@@ -0,0 +1,51 @@
+package com.akto.listener;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+
+import com.akto.MongoBasedTest;
+import com.akto.dao.CustomDataTypeDao;
+import com.akto.dao.context.Context;
+import com.akto.dao.pii.PIISourceDao;
+import com.akto.dto.CustomDataType;
+import com.akto.dto.pii.PIISource;
+import com.mongodb.BasicDBObject;
+
+import org.junit.Test;
+
+public class TestFileTypes extends MongoBasedTest {
+
+    @Test
+    public void testTypes() {
+        String fileUrl = "https://raw.githubusercontent.com/TheSpeedX/akto/master/pii-types/filetypes.json";
+        PIISource piiSource = new PIISource(fileUrl, 0, 1638571050, 0, new HashMap<>(), true);
+        piiSource.setId("File");
+        PIISourceDao.instance.insertOne(piiSource);
+        InitializerListener.executePIISourceFetch();
+        Context.accountId.set(ACCOUNT_ID);
+        for (CustomDataType cdt : CustomDataTypeDao.instance.findAll(new BasicDBObject())) {
+            switch (cdt.getName().toUpperCase()) {
+                case "IMAGE":
+                    assertTrue(cdt.validate("image.jpg", "foo"));
+                    assertTrue(cdt.validate("image.jpeg", "foo"));
+                    assertTrue(cdt.validate("image.SvG", "foo"));
+                    assertFalse(cdt.validate("sample_file", "foo"));
+                    break;
+                case "DATA FILE":
+                    assertTrue(cdt.validate("data.txt", "foo"));
+                    assertTrue(cdt.validate("test.svg.css", "foo"));
+                    assertTrue(cdt.validate("image.pdf", "foo"));
+                    assertFalse(cdt.validate("sample.file", "foo"));
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+
+}
