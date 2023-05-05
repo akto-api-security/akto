5c6fe991d1f19e1b3d1a97c10058e62e1cb74f0b
aktoboy <bhavik@akto.io>
2023-05-05 18:13:45 +0530

    Refactored duplicate code

--- apps/dashboard/src/main/java/com/akto/action/gpt/handlers/GenerateCurlForTest.java
+++ apps/dashboard/src/main/java/com/akto/action/gpt/handlers/GenerateCurlForTest.java
@@ -3,6 +3,7 @@ package com.akto.action.gpt.handlers;
 import com.akto.action.ExportSampleDataAction;
 import com.akto.action.gpt.GptAction;
 import com.akto.action.gpt.result_fetchers.ResultFetcherStrategy;
+import com.akto.action.gpt.utils.HeadersUtils;
 import com.akto.dto.OriginalHttpRequest;
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
@@ -11,18 +12,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 public class GenerateCurlForTest implements QueryHandler{
-
-    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(GenerateCurlForTest.class);
-    public static final String AUTH_TOKEN = "%AUTH_TOKEN%";
-    public static final String ACCESS_TOKEN = "%ACCESS_TOKEN%";
-    public static final String COOKIE = "%COOKIE%";
-
-    private String auth_token_value= null;
-    private String access_token_value= null;
-    private String cookie_value= null;
 
     private final ResultFetcherStrategy<BasicDBObject> resultFetcherStrategy;
 
@@ -35,7 +28,8 @@ public class GenerateCurlForTest implements QueryHandler{
         BasicDBObject request = new BasicDBObject();
         String curl = "";
         String sampleData = meta.getString("sample_data");
-        String modifiedSampleData = minifyRequestHeaders(sampleData);
+        Pair<String, List<Pair<String, String>>> modifiedHeaders = HeadersUtils.minifyHeaders(sampleData);
+        String modifiedSampleData = modifiedHeaders.getLeft();
         try {
             curl = ExportSampleDataAction.getCurl(modifiedSampleData);
             System.out.println("curl: " + curl);
@@ -50,61 +44,11 @@ public class GenerateCurlForTest implements QueryHandler{
         System.out.println("request: " + request.toJson());
         BasicDBObject resp =  this.resultFetcherStrategy.fetchResult(request);
         String respStr = resp.toJson();
-        respStr = populateRequestHeaders(respStr);
+        respStr = HeadersUtils.replaceHeadersWithValues(Pair.of(respStr, modifiedHeaders.getRight()));
         return BasicDBObject.parse(respStr);
     }
 
-    private String populateRequestHeaders(String respStr) {
-        if(auth_token_value != null) {
-            respStr = respStr.replace(AUTH_TOKEN, auth_token_value);
-        }
-        if(access_token_value != null) {
-            respStr = respStr.replace(ACCESS_TOKEN, access_token_value);
-        }
-        if (cookie_value != null) {
-            respStr = respStr.replace(COOKIE, cookie_value);
-        }
-        return respStr;
-    }
 
-    private static final Gson gson = new Gson();
-    private String minifyRequestHeaders(String sampleData) {
-        try {
-            Map<String, Object> json = gson.fromJson(sampleData, Map.class);
-            Map<String, String> requestHeaders = convertMap(OriginalHttpRequest.buildHeadersMap(json, "requestHeaders"));
-            for (Map.Entry<String, String> reqHeaderKeyValue : requestHeaders.entrySet()) {
-                String key = reqHeaderKeyValue.getKey();
-                if (StringUtils.containsAnyIgnoreCase(key, "Authorization")) {
-                    auth_token_value = reqHeaderKeyValue.getValue();
-                    requestHeaders.put(key, AUTH_TOKEN);
-                } else if (StringUtils.containsAnyIgnoreCase(key, "access-token")) {
-                    access_token_value = reqHeaderKeyValue.getValue();
-                    requestHeaders.put(key, ACCESS_TOKEN);
-                } else if (StringUtils.containsAnyIgnoreCase(key, "cookie")) {
-                    cookie_value = reqHeaderKeyValue.getValue();
-                    requestHeaders.put(key, COOKIE);
-                }
-            }
-            String modifiedRequestHeaders = gson
-                    .toJson(requestHeaders)
-                    .replaceAll("\"", "\\\"");
-            json.put("requestHeaders", modifiedRequestHeaders);
-            return gson.toJson(json);
-        } catch (Exception e){
-            logger.error("Error while purifying sample data", e);
-            return sampleData;
-        }
-    }
 
-    private Map<String, String> convertMap(Map<String, List<String>> requestHeaders) {
-        Map<String, String> result = new java.util.HashMap<>();
-        for (Map.Entry<String, List<String>> entry : requestHeaders.entrySet()) {
-            String key = entry.getKey();
-            List<String> value = entry.getValue();
-            if (value != null && !value.isEmpty()) {
-                result.put(key, value.get(0));
-            }
-        }
-        return result;
-    }
+
 }
---
+++ apps/dashboard/src/main/java/com/akto/action/gpt/utils/HeadersUtils.java
@@ -0,0 +1,71 @@
+package com.akto.action.gpt.utils;
+
+import com.akto.dto.OriginalHttpRequest;
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HeadersUtils {
+
+    public static final String AUTH_TOKEN = "%AUTH_TOKEN%";
+    public static final String ACCESS_TOKEN = "%ACCESS_TOKEN%";
+    public static final String COOKIE = "%COOKIE%";
+
+    private static final Logger logger = LoggerFactory.getLogger(HeadersUtils.class);
+
+    private static final Gson gson = new Gson();
+    public static Pair<String, List<Pair<String, String>>> minifyHeaders(String sampleData){
+        List<Pair<String, String>> headers = new ArrayList<>();
+        try {
+            Map<String, Object> json = gson.fromJson(sampleData, Map.class);
+            Map<String, String> requestHeaders = convertMap(OriginalHttpRequest.buildHeadersMap(json, "requestHeaders"));
+            for (Map.Entry<String, String> reqHeaderKeyValue : requestHeaders.entrySet()) {
+                String key = reqHeaderKeyValue.getKey();
+                if (StringUtils.containsAnyIgnoreCase(key, "Authorization")) {
+                    headers.add(Pair.of(AUTH_TOKEN, reqHeaderKeyValue.getValue()));
+                    requestHeaders.put(key, AUTH_TOKEN);
+                } else if (StringUtils.containsAnyIgnoreCase(key, "access-token")) {
+                    headers.add(Pair.of(ACCESS_TOKEN, reqHeaderKeyValue.getValue()));
+                    requestHeaders.put(key, ACCESS_TOKEN);
+                } else if (StringUtils.containsAnyIgnoreCase(key, "cookie")) {
+                    headers.add(Pair.of(COOKIE, reqHeaderKeyValue.getValue()));
+                    requestHeaders.put(key, COOKIE);
+                }
+            }
+            String modifiedRequestHeaders = gson
+                    .toJson(requestHeaders)
+                    .replaceAll("\"", "\\\"");
+            json.put("requestHeaders", modifiedRequestHeaders);
+            return Pair.of(gson.toJson(json), headers);
+        } catch (Exception e){
+            logger.error("Error while minifying headers", e);
+            return Pair.of(sampleData, headers);
+        }
+    }
+
+    private static Map<String, String> convertMap(Map<String, List<String>> requestHeaders) {
+        Map<String, String> result = new java.util.HashMap<>();
+        for (Map.Entry<String, List<String>> entry : requestHeaders.entrySet()) {
+            String key = entry.getKey();
+            List<String> value = entry.getValue();
+            if (value != null && !value.isEmpty()) {
+                result.put(key, value.get(0));
+            }
+        }
+        return result;
+    }
+
+    public static String replaceHeadersWithValues(Pair<String, List<Pair<String, String>>> response) {
+        String curl = response.getLeft();
+        for(Pair<String, String> header : response.getRight()) {
+            curl = curl.replace(header.getLeft(), header.getRight());
+        }
+        return curl;
+    }
+}
