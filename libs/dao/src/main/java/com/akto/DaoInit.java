97d031c07ddc75c07a90d51bdca47eeeb1a816d4
shivamrawat101192 <shivam@akto.io>
2023-05-05 15:49:02 +0530

    merge from test_editor_fixes

--- libs/dao/src/main/java/com/akto/DaoInit.java
+++ libs/dao/src/main/java/com/akto/DaoInit.java
@@ -217,7 +217,6 @@ public class DaoInit {
                 new EnumCodec<>(GlobalEnums.TestRunIssueStatus.class),
                 new EnumCodec<>(GlobalEnums.TestErrorSource.class),
                 new EnumCodec<>(GlobalEnums.TestCategory.class),
-                new EnumCodec<>(GlobalEnums.TestSubCategory.class),
                 new EnumCodec<>(GlobalEnums.IssueTags.class),
                 new EnumCodec<>(GlobalEnums.Severity.class),
                 new EnumCodec<>(TrafficMetrics.Name.class),
