74e6b8c1eb7abed23886fc656f7d4f307eec2942
ayushaga14 <ayush@akto.io>
2023-05-08 16:54:15 +0530

    fill current inbuilt yaml

--- apps/testing/src/main/java/com/akto/testing/yaml_tests/YamlTestTemplate.java
+++ apps/testing/src/main/java/com/akto/testing/yaml_tests/YamlTestTemplate.java
@@ -20,9 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 public class YamlTestTemplate extends SecurityTestTemplate {
 
