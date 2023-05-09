f446af4d3853aa6eb3266d9302263eb869e5692e
Ark2307 <kr.aryan2307@gmail.com>
2023-05-09 10:23:51 +0530

    Fixed editor and basic functionality of left and mid column completed

--- apps/dashboard/web/src/apps/tools/TextEditor.vue
+++ apps/dashboard/web/src/apps/tools/TextEditor.vue
@@ -4,6 +4,33 @@
             <layout-with-left-pane>
                 <search placeholder="Search Tests" @changed="setSearchText" />
                 <div class="tests-container">
+                    <v-list dense nav>
+                        <v-list-group
+                            v-for="item in selectedTestCategories"
+                            :key="item.displayName"
+                            class="tests-category-container"
+                            active-class="tests-category-container-active"
+                        >
+                            <template v-slot:appendIcon>
+                                <v-icon >$fas_angle-down</v-icon>
+                            </template>
+                            <template v-slot:activator>
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="item.displayName"></v-list-item-title>
+                                </v-list-item-content>
+                            </template>
+
+                            <v-list-item
+                                v-for="(test,index) in testsObj[item.name].all"
+                                :key="index"
+                                class="test-container"
+                            >
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="test.label" class="test-name" @click="changeValue(test.label)"></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-group>
+                    </v-list>
                 </div>
             </layout-with-left-pane>
         </div>
@@ -20,54 +47,27 @@
 
 <script>
 
-import {editor , Uri} from "monaco-editor"
+import {editor} from "monaco-editor/esm/vs/editor/editor.api"
 import Search from '../dashboard/shared/components/inputs/Search.vue';
 import LayoutWithLeftPane from '../dashboard/layouts/LayoutWithLeftPane.vue';
-// import { setDiagnosticsOptions } from "monaco-yaml"
 
-const modelUri = Uri.parse('a://b/foo.yaml');
+import 'monaco-editor/esm/vs/editor/contrib/find/browser/findController';
+import 'monaco-editor/esm/vs/editor/contrib/folding/browser/folding';
+import 'monaco-editor/esm/vs/editor/contrib/bracketMatching/browser/bracketMatching';
+import 'monaco-editor/esm/vs/editor/contrib/comment/browser/comment';
+import 'monaco-editor/esm/vs/editor/contrib/codelens/browser/codelensController';
+import 'monaco-editor/esm/vs/editor/contrib/colorPicker/browser/color';
+import 'monaco-editor/esm/vs/editor/contrib/format/browser/formatActions';
+import 'monaco-editor/esm/vs/editor/contrib/lineSelection/browser/lineSelection';
+import 'monaco-editor/esm/vs/editor/contrib/indentation/browser/indentation';
+import 'monaco-editor/esm/vs/editor/contrib/inlineCompletions/browser/inlineCompletionsController';
+import 'monaco-editor/esm/vs/editor/contrib/snippet/browser/snippetController2'
+import 'monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController';
+import 'monaco-editor/esm/vs/editor/contrib/wordHighlighter/browser/wordHighlighter';
 
-// setDiagnosticsOptions({
-//   enableSchemaRequest: true,
-//   hover: true,
-//   completion: true,
-//   validate: true,
-//   format: true,
-//   schemas: [
-//     {
-//       // Id of the first schema
-//       uri: 'http://myserver/foo-schema.json',
-//       // Associate with our model
-//       fileMatch: [String(modelUri)],
-//       schema: {
-//         type: 'object',
-//         properties: {
-//           p1: {
-//             enum: ['v1', 'v2'],
-//           },
-//           p2: {
-//             // Reference the second schema
-//             $ref: 'http://myserver/bar-schema.json',
-//           },
-//         },
-//       },
-//     },
-//     {
-//       // Id of the first schema
-//       uri: 'http://myserver/bar-schema.json',
-//       schema: {
-//         type: 'object',
-//         properties: {
-//           q1: {
-//             enum: ['x1', 'x2'],
-//           },
-//         },
-//       },
-//     },
-//   ],
-// });
+import "monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution"
 
-// const value = 'p1: \np2: \n';
+import issuesApi from "../dashboard/views/issues/api"
 
 export default {
     name: "TextEditor",
@@ -84,10 +84,14 @@ export default {
                 automaticLayout:true,
                 colorDecorations:true,
                 scrollBeyondLastLine:false,
-                // model:editor.createModel(value , 'yaml' , modelUri)
             },
             textEditor:null,
             searchText: "",
+            testCategories: [],
+            testSourceConfig: [],
+            testsObj:{},
+            businessLogicSubcategories:[],
+            mapTestToYaml : {},
         }
     },
     methods: {
@@ -99,13 +103,73 @@ export default {
             this.$store.dispatch('testing/addTestTemplate',{content:this.textEditor.getValue()})
             return this.textEditor.getValue()
         },
+        changeValue(testName){
+            this.textEditor.setValue(this.mapTestToYaml[testName])
+        },
         setSearchText(val){
             this.searchText = val
+        },
+        populateMapCategoryToSubcategory() {
+            let ret = {}
+            this.testSourceConfigs.forEach(x => {
+                if (!ret[x.category.name]) {
+                    ret[x.category.name] = {all: []}
+                }
+
+                let obj = {
+                    label: x.id.substring(x.id.lastIndexOf("/")+1, x.id.lastIndexOf(".")), 
+                    value: x.id,
+                    icon: "$fab_github"
+                }
+                ret[x.category.name].all.push(obj);
+            })
+
+            this.mapTestToYaml = {}
+
+            this.businessLogicSubcategories.forEach(x => {
+                if (!ret[x.superCategory.name]) {
+                    ret[x.superCategory.name] = { all: []}
+                }
+                
+
+                let obj = {
+                    label: x.testName,
+                    value: x.name,
+                    icon: "$aktoWhite"
+                }
+                ret[x.superCategory.name].all.push(obj)
+            })
+            return ret
+        },
+        mapTestToYamlFunc(){
+            this.businessLogicSubcategories.forEach(x => {
+                this.mapTestToYaml[x.testName] = x.content
+            })
         }
     },
     async mounted() {
         this.createEditor()
+        let _this = this
+        issuesApi.fetchAllSubCategories().then(resp => {
+            _this.testCategories = resp.categories
+            _this.testSourceConfigs = resp.testSourceConfigs
+            _this.businessLogicSubcategories = resp.subCategories
+            _this.testsObj = _this.populateMapCategoryToSubcategory()
+            this.mapTestToYamlFunc()
+        })
+        
     },
+    computed:{
+        selectedTestCategories(){
+            let arr = []
+            this.testCategories.forEach((test)=>{
+                if(this.testsObj[test.name]){
+                    arr.push(test)
+                }
+            })
+            return arr
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
