aa375c309e6f2dad6c0882fc6de7a3c723fe9dda
ayushaga14 <ayush@akto.io>
2023-05-15 14:39:50 +0530

    resolve comments

--- apps/testing/src/main/java/com/akto/test_editor/filter/data_operands_impl/GreaterThanEqFilter.java
+++ apps/testing/src/main/java/com/akto/test_editor/filter/data_operands_impl/GreaterThanEqFilter.java
@@ -1,8 +1,7 @@
 package com.akto.test_editor.filter.data_operands_impl;
 
-import java.util.List;
-
 import com.akto.dto.test_editor.DataOperandFilterRequest;
+import com.akto.test_editor.Utils;
 
 public class GreaterThanEqFilter extends DataOperandsImpl {
     
@@ -13,26 +12,7 @@ public class GreaterThanEqFilter extends DataOperandsImpl {
         Object data = dataOperandFilterRequest.getData();
         Object querySet = dataOperandFilterRequest.getQueryset();
         
-        try {
-
-            if (data instanceof Integer) {
-                List<Integer> queryList = (List) querySet;
-                if (queryList == null || queryList.size() == 0) {
-                    return false;
-                }
-                Integer dataInt = (Integer) data;
-                Object query = queryList.get(0);
-                if (query instanceof String) {
-                    int queryInt = Integer.parseInt((String) query);
-                    result = (int) dataInt >= queryInt;
-                } else {
-                    result = ((int) dataInt >= (int) queryList.get(0));
-                }
-            }
-            
-        } catch (Exception e) {
-            return false;
-        }
+        result = Utils.applyIneqalityOperation(data, querySet, "gte");
 
         return result;
     }
