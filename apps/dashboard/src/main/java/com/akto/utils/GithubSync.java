3bbf3c5c555cbb12fc594d6c8d19fd2cec5a7f90
Oren Saldanha <oren@akto.io>
2023-05-24 12:59:05 +0530

    added sync from github url and error handling changes

--- apps/dashboard/src/main/java/com/akto/utils/GithubSync.java
+++ apps/dashboard/src/main/java/com/akto/utils/GithubSync.java
@@ -12,46 +12,57 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.akto.log.LoggerMaker;
+import com.akto.log.LoggerMaker.LogDb;
+
 public class GithubSync {
+    private static final Logger logger = LoggerFactory.getLogger(GithubSync.class);
+    private static final LoggerMaker loggerMaker = new LoggerMaker(GithubSync.class);
+    private static final OkHttpClient client = new OkHttpClient();
 
-    public Map<String, String> syncDir(String repoUrl, String dirPath) throws Exception {
-        //todo: use repoUrl
-
+    public Map<String, String> syncDir(String repo, String dirPath) {
         Map<String, String> dirContents = new HashMap<String, String>();
-        OkHttpClient client = new OkHttpClient();
 
-        Request getRequest = new Request.Builder()
-                .url("https://api.github.com/repos/akto-api-security/akto/git/trees/master?recursive=1")
+        // Get Github repo tree - Eg: https://api.github.com/repos/akto-api-security/akto/git/trees/master?recursive=1
+        Request treeRequest = new Request.Builder()
+                .url(String.format("https://api.github.com/repos/%s/git/trees/master?recursive=1", repo))
                 .build();
 
-       
-        Response response = client.newCall(getRequest).execute();
-        ResponseBody responseBody = response.body();
+        try {
+            Response treeResponse = client.newCall(treeRequest).execute();
+            ResponseBody treeResponseBody = treeResponse.body();
 
-        if (responseBody != null) {
-            String json = responseBody.string();
-            JSONObject jsonObject = new JSONObject(json);
-            JSONArray tree = jsonObject.getJSONArray("tree");
-            for (Object treeNodeObject : tree) {
-                JSONObject treeNode = (JSONObject) treeNodeObject;
-                String path = treeNode.getString("path");
-                
-                if(path.contains(dirPath)) {
-                    Request fileRequest = new Request.Builder()
-                        .url("https://raw.githubusercontent.com/akto-api-security/akto/master/" + path)
-                        .build();
-                    Response fileResponse = client.newCall(fileRequest).execute();
-                    ResponseBody fileResponseBody = fileResponse.body();
-                    String fileContents = fileResponseBody.string();
-                
-                    String[] pathSplit = path.split("/");
-                    String filename = pathSplit[pathSplit.length - 1];
+            if (treeResponseBody != null) {
+                String json = treeResponseBody.string();
+                JSONObject jsonObject = new JSONObject(json);
+                JSONArray tree = jsonObject.getJSONArray("tree");
+                for (Object treeNodeObject : tree) {
+                    JSONObject treeNode = (JSONObject) treeNodeObject;
+                    String path = treeNode.getString("path");
                     
-                    dirContents.put(filename, fileContents);
+                    // Retreive Github file contents 
+                    if(path.contains(dirPath)) {
+                        Request fileRequest = new Request.Builder()
+                            .url(String.format("https://raw.githubusercontent.com/%s/master/%s", repo, path))
+                            .build();
+                        Response fileResponse = client.newCall(fileRequest).execute();
+                        ResponseBody fileResponseBody = fileResponse.body();
+                        String fileContents = fileResponseBody.string();
+                    
+                        String[] pathSplit = path.split("/");
+                        String filename = pathSplit[pathSplit.length - 1];
+                        
+                        dirContents.put(filename, fileContents);
+                    }
                 }
             }
+        } catch (Exception ex) {
+            loggerMaker.errorAndAddToDb(String.format("Error while syncing files from directory %s in Github repo %s %s ", repo, dirPath, ex.toString()), LogDb.DASHBOARD);
+            return null;
         }
-
+        
         return dirContents;
     }
 }
