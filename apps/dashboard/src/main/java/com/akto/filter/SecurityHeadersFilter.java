9057ecdc3693b83b4d68a0b5b6b2ee78f3fa7abb
Shivansh Agrawal <shivansh@akto.io>
2023-06-20 15:52:41 +0530

    echo user-agent and amazon trace headers in response

--- apps/dashboard/src/main/java/com/akto/filter/SecurityHeadersFilter.java
+++ apps/dashboard/src/main/java/com/akto/filter/SecurityHeadersFilter.java
@@ -1,6 +1,7 @@
 package com.akto.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.akto.utils.HttpUtils;
@@ -14,6 +15,18 @@ public class SecurityHeadersFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    void copyHeader(String headerKey, ServletRequest request, HttpServletResponse httpServletResponse) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (httpServletRequest != null) {
+            String val = httpServletRequest.getHeader(headerKey);
+            if (val == null) {
+                val = "null";
+            }
+            httpServletResponse.addHeader(headerKey, val);
+        }
+
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
@@ -22,6 +35,9 @@ public class SecurityHeadersFilter implements Filter {
         httpServletResponse.addHeader("X-XSS-Protection", "1");
         httpServletResponse.addHeader("X-Content-Type-Options", "nosniff");
         httpServletResponse.addHeader("cache-control", "no-cache, no-store, must-revalidate, pre-check=0, post-check=0");
+
+        copyHeader("User-Agent", request, httpServletResponse);
+        copyHeader("X-Amzn-Trace-Id", request, httpServletResponse);
 
         if (HttpUtils.isHttpsEnabled()) {
             httpServletResponse.addHeader("strict-transport-security","max-age=31536000; includeSubDomains; preload");
