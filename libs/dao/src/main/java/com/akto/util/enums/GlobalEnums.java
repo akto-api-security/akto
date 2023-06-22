78eabbe9cca50edc803f9a28f25406f024858ad4
Avneesh Hota <avneesh@akto.io>
2023-06-22 09:34:17 +0530

    added 3 ssti tests

--- libs/dao/src/main/java/com/akto/util/enums/GlobalEnums.java
+++ libs/dao/src/main/java/com/akto/util/enums/GlobalEnums.java
@@ -37,7 +37,8 @@ public class GlobalEnums {
         VEM("VEM", Severity.LOW, "Verbose Error Messages (VEM)", "Verbose Error Messages"),
         MHH("MHH", Severity.LOW, "Misconfigured HTTP Headers (MHH)", "Misconfigured HTTP Headers"),
         SVD("SVD", Severity.LOW, "Server Version Disclosure (SVD)", "Server Version Disclosure"),
-        CORS("CORS", Severity.HIGH, "Cross-Origin Resource Sharing (CORS)", "CORS Misconfiguration");
+        CORS("CORS", Severity.HIGH, "Cross-Origin Resource Sharing (CORS)", "CORS Misconfiguration"),
+        SSTI("SSTI", Severity.HIGH, "Server Side Template Injection (SSTI)", "Server Side Template Injection");
 
         private final String name;
         private final Severity severity;
