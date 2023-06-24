026b650d548b285dc19bab355eb5c017354fc29c
shivamrawat101192 <shivam@akto.io>
2023-06-24 09:35:42 +0530

    allowing growth tools only for flash and saas redirecting to tools page on dashboard

--- apps/dashboard/src/main/java/com/akto/filter/GrowthToolsFilter.java
+++ apps/dashboard/src/main/java/com/akto/filter/GrowthToolsFilter.java
@@ -3,6 +3,8 @@ package com.akto.filter;
 import com.akto.dao.context.Context;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class GrowthToolsFilter implements Filter {
@@ -13,9 +15,15 @@ public class GrowthToolsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String url = httpServletRequest.getRequestURL().toString();
+        if (!url.startsWith("https://app.akto.io/") && !url.startsWith("https://flash.staging.akto.io/")) {
+            httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+            return;
+        }
         Context.accountId.set(1_000_000);
         chain.doFilter(request, response);
-
     }
 
     @Override
