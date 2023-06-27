1a8cd319977363e1d2c1f97e2b4f3223963ec2a5
Shivansh Agrawal <shivansh@akto.io>
2023-06-27 09:49:07 +0530

    add improper input validation tests

--- libs/dao/src/main/java/com/akto/util/enums/GlobalEnums.java
+++ libs/dao/src/main/java/com/akto/util/enums/GlobalEnums.java
@@ -37,7 +37,8 @@ public class GlobalEnums {
         VEM("VEM", Severity.LOW, "Verbose Error Messages (VEM)", "Verbose Error Messages"),
         MHH("MHH", Severity.LOW, "Misconfigured HTTP Headers (MHH)", "Misconfigured HTTP Headers"),
         SVD("SVD", Severity.LOW, "Server Version Disclosure (SVD)", "Server Version Disclosure"),
-        CORS("CORS", Severity.HIGH, "Cross-Origin Resource Sharing (CORS)", "CORS Misconfiguration");
+        CORS("CORS", Severity.HIGH, "Cross-Origin Resource Sharing (CORS)", "CORS Misconfiguration"),
+        RPM("IIV", Severity.LOW, "Improper Input Validation (IIV)", "Improper Input Validation");
 
         private final String name;
         private final Severity severity;
