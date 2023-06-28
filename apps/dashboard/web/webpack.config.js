8734d982dd727b3bc6efb904b3f5fc341f89791e
Shivansh Agrawal <shivansh@akto.io>
2023-06-28 19:13:33 +0530

    fix webpack dependencies and restructure frontend directories

---
+++ apps/dashboard/web/webpack.config.js
@@ -0,0 +1,131 @@
+var path = require('path')
+var webpack = require('webpack')
+require("babel-polyfill")
+const TerserPlugin = require("terser-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
+
+function resolve (dir) {
+      return path.join(__dirname, '..', dir)
+}
+
+function hashGenerator() {
+  return 1
+}
+
+process.env.HASH = hashGenerator()
+
+module.exports = {
+  entry: {"babel-polyfill": "babel-polyfill", main: './web/src/apps/main/index.js'},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              // sass-loader version >= 8
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules\/(?!(tiptap|tiptap-utils|are-you-es5|tiptap-extensions|monaco-yaml|monaco-worker-manager|monaco-marker-data-provider|monaco-editor)\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+             ["@babel/preset-react", {"runtime": "automatic"}]
+          ]
+          }
+        }
+
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': resolve('/web/src')
+    },
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = 'source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new GenerateSW({
+       exclude: [/\.map$/, /asset-manifest\.json$/],
+     }),
+    // new SWPrecacheWebpackPlugin({
+    //   cacheId: 'akto-app',
+    //   filename: 'sw.js',
+    //   staticFileGlobs: ['dist/**/*.{js,css}', '/'],
+    //   minify: true,
+    //   stripPrefix: 'dist/',
+    //   dontCacheBustUrlsMatching: /\.\w{6}\./
+    // }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new TerserPlugin({
+      "terserOptions":{
+        compress:true,
+        sourceMap: true,
+        output:{
+          ascii_only:true
+        }
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
