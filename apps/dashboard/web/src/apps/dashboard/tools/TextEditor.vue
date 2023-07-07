fde57aa0045e9e166593e1dd441839ca552b96fa
Ark2307 <kr.aryan2307@gmail.com>
2023-07-07 10:56:02 +0530

    Attempt made for autocomplete

--- apps/dashboard/web/src/apps/dashboard/tools/TextEditor.vue
+++ apps/dashboard/web/src/apps/dashboard/tools/TextEditor.vue
@@ -204,7 +204,7 @@
 
 <script>
 
-import { editor } from "monaco-editor/esm/vs/editor/editor.api"
+import { editor, languages, MarkerSeverity } from "monaco-editor/esm/vs/editor/editor.api"
 import Search from '../shared/components/inputs/Search.vue';
 import LayoutWithLeftPane from '../layouts/LayoutWithLeftPane.vue';
 import SampleData from '../shared/components/SampleData.vue';
@@ -218,6 +218,7 @@ import testingApi from "../views/testing/api"
 
 import func from "@/util/func"
 import obj from "@/util/obj"
+import editorSetup from "./func"
 
 import 'monaco-editor/esm/vs/editor/contrib/find/browser/findController';
 import 'monaco-editor/esm/vs/editor/contrib/folding/browser/folding';
@@ -233,7 +234,6 @@ import 'monaco-editor/esm/vs/editor/contrib/snippet/browser/snippetController2'
 import 'monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController';
 import 'monaco-editor/esm/vs/editor/contrib/wordHighlighter/browser/wordHighlighter';
 
-import "monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution"
 
 export default {
     name: "TextEditor",
@@ -292,13 +292,19 @@ export default {
     data() {
         return {
             editorOptions: {
-                language: "yaml",
+                language: "custom_yaml",
                 minimap: { enabled: false },
                 wordWrap: true,
                 automaticLayout: true,
                 colorDecorations: true,
-                scrollBeyondLastLine: false
+                scrollBeyondLastLine: false,
+                theme: "customTheme"
             },
+            keywords: [
+                "response_code", "method", "url", "request_payload", "response_payload", "request_headers", "response_headers", "query_param", "regex", "eq", "neq", "gt", "gte", "lt", "lte", "not_contains", "not_contains_either", "contains_jwt", "contains_all", "contains_either", "for_one", "or", "and", "add_body_param", "modify_body_param", "delete_body_param", "add_query_param", "modify_query_param", "delete_query_param", "modify_url", "modify_method", "replace_body", "add_header", "modify_header", "delete_header", "remove_auth_header", "follow_redirect"
+
+            ],
+            symbols:  /[=><!~?:&|+\-*\/\^%]+/,
             textEditor: null,
             testCategories: [],
             testsObj: {},
@@ -519,7 +525,12 @@ export default {
             }
         },
         createEditor() {
+            editorSetup.registerLanguage()
+            editorSetup.setTokenizer(this.keywords,this.symbols)
+            editorSetup.setEditorTheme()
+            editorSetup.setAutoComplete(this.keywords)
             this.textEditor = editor.create(this.$refs.editor, this.editorOptions)
+            // editorSetup.findErrors(this.textEditor,this.keywords)
         },
         findSuffixForNewTest(testId) {
             let aktoTests = Object.values(this.testsObj).map (x => x.all).flat().filter(x=>x.value.indexOf(testId) == 0)            
