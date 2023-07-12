8bbe8a975f09295351ca3edb2ead1fc12939669d
shivamrawat101192 <shivam@akto.io>
2023-07-12 16:02:21 +0530

    TestExecutor code compile issue fix

--- apps/testing/src/main/java/com/akto/testing/TestExecutor.java
+++ apps/testing/src/main/java/com/akto/testing/TestExecutor.java
@@ -181,7 +181,7 @@ public class TestExecutor {
             }
             for (ApiInfo.ApiInfoKey apiInfoKey: apiInfoKeyList) {
                 try {
-                    hostName = findHost(apiInfoKey, testingUtil.getSampleMessages());
+                    hostName = findHost(apiInfoKey, testingUtil.getSampleMessages(), sampleMessageStore);
                     if (hostName == null) {
                         continue;
                     }
@@ -195,7 +195,7 @@ public class TestExecutor {
 
         for (ApiInfo.ApiInfoKey apiInfoKey: apiInfoKeyList) {
             try {
-                String hostName = findHost(apiInfoKey, testingUtil.getSampleMessages(), testingUtil.getSampleMessageStore());
+                hostName = findHost(apiInfoKey, testingUtil.getSampleMessages(), testingUtil.getSampleMessageStore());
                 if (hostName != null && hostsToApiCollectionMap.get(hostName) == null) {
                     hostsToApiCollectionMap.put(hostName, apiInfoKey.getApiCollectionId());
                 }
