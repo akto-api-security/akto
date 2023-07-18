9f012c0e8581f00ba57609187357727998a07a5b
Shivansh Agrawal <shivansh@akto.io>
2023-07-18 14:41:40 +0530

    basic issues page added

---
+++ apps/dashboard/web/src/apps/dashboard/pages/issues/IssuesPage/IssuesPage.jsx
@@ -0,0 +1,11 @@
+import PageWithMultipleCards from "../../../components/layouts/PageWithMultipleCards"
+
+function IssuesPage(){
+    return (
+        <PageWithMultipleCards
+            title="Issues"
+        />
+    )
+}
+
+export default IssuesPage
