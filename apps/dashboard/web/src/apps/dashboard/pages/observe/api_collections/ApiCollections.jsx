9a513ef92e383bfde1ab13989a0ef077e48e1cc5
Oren Saldanha <oren@akto.io>
2023-08-05 11:40:50 +0530

    added remove collections

--- apps/dashboard/web/src/apps/dashboard/pages/observe/api_collections/ApiCollections.jsx
+++ apps/dashboard/web/src/apps/dashboard/pages/observe/api_collections/ApiCollections.jsx
@@ -105,10 +105,10 @@ function ApiCollections() {
         func.setToast(true, false, `${collectionIdList.length} API collection${collectionIdList.length > 1 ? "s" : ""} deleted successfully`)
     }
 
-    const promotedBulkActions = [
+    const promotedBulkActions = (selectedResources) => [
         {
           content: 'Remove collections',
-          onAction: (collectionIdList) => handleRemoveCollections(collectionIdList)
+          onAction: () => handleRemoveCollections(selectedResources)
         },
       ];
 
@@ -159,7 +159,7 @@ function ApiCollections() {
                 disambiguateLabel={()=>{}} 
                 headers={headers}
                 selectable={true}
-                // promotedBulkActions={promotedBulkActions}
+                promotedBulkActions={promotedBulkActions}
                 />)
             ]}
         />
