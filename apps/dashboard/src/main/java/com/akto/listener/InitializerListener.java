3e9e5f182f656429cda89ff4be60383611dae723
Avneesh Hota <avneesh@akto.io>
2023-08-23 12:00:08 +0530

    fixed slack webhook failing dns check

--- apps/dashboard/src/main/java/com/akto/listener/InitializerListener.java
+++ apps/dashboard/src/main/java/com/akto/listener/InitializerListener.java
@@ -76,6 +76,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextListener;
 import java.io.*;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -605,7 +606,8 @@ public class InitializerListener implements ServletContextListener {
                                 String payload = dailyUpdate.toJSON();
                                 loggerMaker.infoAndAddToDb(payload, LogDb.DASHBOARD);
                                 try {
-                                    if (!HostDNSLookup.isRequestValid(webhookUrl)) {
+                                    URI uri = URI.create(webhookUrl);
+                                    if (!HostDNSLookup.isRequestValid(uri.getHost())) {
                                         throw new IllegalArgumentException("SSRF attack attempt");
                                     }
                                     WebhookResponse response = slack.send(webhookUrl, payload);
@@ -621,6 +623,7 @@ public class InitializerListener implements ServletContextListener {
 
                                 } catch (IOException e) {
                                     e.printStackTrace();
+                                    loggerMaker.errorAndAddToDb("Error while sending slack alert: " + e.getMessage(), LogDb.DASHBOARD);
                                 }
                             }
                         }
