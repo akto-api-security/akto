b4543bd68c998bd4e8443de538a8385dee689436
Ark2307 <kr.aryan2307@gmail.com>
2023-08-30 10:49:32 +0530

    feedbacks of testing done

--- apps/dashboard/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
+++ apps/dashboard/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
@@ -67,7 +67,7 @@ function GithubSimpleTable(props) {
     
 
     return <GithubServerTable
-        key={props.data} // passing any value as a "key" re-renders the component when the value is changed.
+        key={JSON.stringify(props.data ? props.data : "{}")} // passing any value as a "key" re-renders the component when the value is changed.
         pageLimit={props.pageLimit}
         fetchData={fetchDataSync}
         sortOptions={props.sortOptions} 
