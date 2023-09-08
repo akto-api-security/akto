3c0d5b6945e2ab99563f768edd29a88363005cb3
Shivansh Agrawal <shivansh@akto.io>
2023-09-08 18:54:35 +0530

    add snippets for common execute keywords

--- apps/dashboard/web/src/apps/dashboard/tools/snippets.js
+++ apps/dashboard/web/src/apps/dashboard/tools/snippets.js
@@ -5,9 +5,16 @@ const successResponseCode = [
     "\t\tlt: 300"
 ]
 
+const executeBasic = [
+    "execute:",
+    "\ttype: single",
+    "\trequests:",
+    "\t\t- req:"
+]
+
 const snippets = [
     {
-        label: "auth-block",
+        label: "authorization-SAMPLE",
         desc: "Makes sure that only authenticated APIs get considered for a test",
         text: [
             "auth:",
@@ -15,7 +22,7 @@ const snippets = [
         ]
     },
     {
-        label: "filter-success-response",
+        label: "filter-success-response-SAMPLE",
         desc: "API filter for success response code",
         text: [
             "api_selection_filters:",
@@ -23,14 +30,39 @@ const snippets = [
         ]
     },
     {
-        label: "validate-success-response",
+        label: "validate-success-response-SAMPLE",
         desc: "Validate success response code",
         text: [
             "validate:",
             ...successResponseCode
         ]
-    }
-
+    },
+    {
+        label: "execute-add-header-SAMPLE",
+        desc: "Add header to request while executing the test",
+        text: [
+            ...executeBasic,
+            "\t\t\t- add_header:",
+            "\t\t\t\t<HEADER_KEY>: <HEADER_VALUE>"
+        ]
+    },
+    {
+        label: "execute-replace-body-SAMPLE",
+        desc: "Replace body of the request while executing the test",
+        text: [
+            ...executeBasic,
+            "\t\t\t- replace_body: <REPLACED_BODY>"
+        ]
+    },
+    {
+        label: "execute-modify-body-param-SAMPLE",
+        desc: "Modify body param of the request while executing the test",
+        text: [
+            ...executeBasic,
+            "\t\t\t- modify_body_param:",
+            "\t\t\t\t<PAYLOAD_KEY>: <NEW_PAYLOAD_VALUE>"
+        ]
+    },
 ]
 
 export default snippets;
