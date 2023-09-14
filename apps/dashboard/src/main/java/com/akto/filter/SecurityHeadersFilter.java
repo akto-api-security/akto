1d2da69c1fb5488e65026a87ade6641379e1b39e
Ankush Jain <ankush@akto.io>
2023-09-14 14:57:11 +0530

    allow all ancestors

--- apps/dashboard/src/main/java/com/akto/filter/SecurityHeadersFilter.java
+++ apps/dashboard/src/main/java/com/akto/filter/SecurityHeadersFilter.java
@@ -20,7 +20,7 @@ public class SecurityHeadersFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
         if (!httpServletRequest.getRequestURI().startsWith("/tools/")) {
-            httpServletResponse.addHeader("X-Frame-Options", "deny");
+            httpServletResponse.addHeader("Content-Security-Policy", "frame-ancestors *");
         }
         httpServletResponse.addHeader("X-XSS-Protection", "1");
         httpServletResponse.addHeader("X-Content-Type-Options", "nosniff");
