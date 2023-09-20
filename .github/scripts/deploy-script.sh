b676c96bc515ca488cee05d7ff87688d3ee6288a
Shivansh Agrawal <shivansh@akto.io>
2023-09-20 18:04:55 +0530

    clean docker files before pull

--- .github/scripts/deploy-script.sh
+++ .github/scripts/deploy-script.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 cd ~/akto/infra
+docker system prune -a
 docker-compose -f docker-compose-dashboard.yml pull
 docker-compose -f docker-compose-dashboard.yml down
 docker-compose -f docker-compose-dashboard.yml up -d
