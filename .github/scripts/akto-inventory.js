a244c26fd594c4fd52919fbdaedd6f2b43b38fbb
Oren Saldanha <oren@akto.io>
2023-10-06 09:54:55 +0530

    add akto inventory script and workflow

---
+++ .github/scripts/akto-inventory.js
@@ -0,0 +1,95 @@
+const axios = require("axios")
+const fs = require("fs")
+const { Readable } = require('stream');
+
+const AKTO_DASHBOARD_URL = process.env.AKTO_DASHBOARD_URL
+const AKTO_API_KEY = process.env.AKTO_API_KEY
+
+const headers = {
+    'X-API-KEY': AKTO_API_KEY,
+}
+
+function processOpenAPIfile(openAPIObject) {
+    // Modify the headers in the openAPIString object
+    for (const path in openAPIObject.paths) {
+        
+        if (!path.startsWith("/api")) {
+            delete openAPIObject.paths[path]
+            continue
+        }
+
+        const pathInfo = openAPIObject.paths[path];
+        for (const method in pathInfo) {
+            if (method === "description" || method === 'parameters') {
+                continue
+            }
+            const operation = pathInfo[method];
+            if (operation.parameters) {
+                // Remove existing headers
+                operation.parameters = operation.parameters.filter(param => param.in !== 'header');
+            }
+            // Add new headers
+            operation.parameters = operation.parameters || [];
+            operation.parameters.push(
+                {
+                    name: 'X-API-KEY',
+                    in: 'header',
+                    schema: {
+                        type: 'string',
+                        example: 'your-api-key',
+                    },
+                },
+                {
+                    name: 'content-type',
+                    in: 'header',
+                    schema: {
+                        type: 'string',
+                        example: 'application/json',
+                    },
+                }
+            );
+
+        }
+    }
+
+    return openAPIObject
+}
+
+async function saveOpenAPIfile(openAPIObject) {
+    // Convert the JSON object to a JSON string
+    const jsonString = JSON.stringify(openAPIObject, null, 2);
+
+    // Specify the file path where you want to save the JSON
+    const filePath = './akto_open_api.json';
+
+    // Write the JSON string to the file
+    fs.writeFile(filePath, jsonString, 'utf8', (err) => {
+        if (err) {
+            console.error(`Error writing file: ${err}`);
+        } else {
+            console.log(`JSON object saved to ${filePath}`);
+        }
+    });
+
+}
+
+async function main() {
+
+    const data = {
+        apiCollectionId: "-1753579810"
+    }
+
+    try {
+        const generateOpenApiFileResponse = await axios.post(`${AKTO_DASHBOARD_URL}/api/generateOpenApiFile`, { ...data }, { headers })
+
+        if (generateOpenApiFileResponse.status === 200) {
+            const openAPIObject = JSON.parse(generateOpenApiFileResponse.data.openAPIString)
+            const processedOpenAPIObject = processOpenAPIfile(openAPIObject)
+            saveOpenAPIfile(processedOpenAPIObject)
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+main()
