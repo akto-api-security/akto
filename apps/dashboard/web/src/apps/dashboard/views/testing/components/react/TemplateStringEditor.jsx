fc88c5d206c76db17baf7257ee279b1ad980c6ea
Adarsh Jha <132337675+adarsh-jha-dev@users.noreply.github.com>
2023-10-20 10:31:42 +0530

    Update TemplateStringEditor.jsx

--- apps/dashboard/web/src/apps/dashboard/views/testing/components/react/TemplateStringEditor.jsx
+++ apps/dashboard/web/src/apps/dashboard/views/testing/components/react/TemplateStringEditor.jsx
@@ -1,40 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faCheckSquare } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
-import InputBase from "@mui/material/TextField"
-import IconButton from "@mui/material/IconButton"
-import TextFieldCloseable from './TextFieldCloseable.jsx'
-
+import InputBase from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
 
 import './start-node.css';
 
-const TemplateStringEditor = ({defaultText, onChange, usePureJs=false}) => {
+const TemplateStringEditor = ({ defaultText, onChange, usePureJs = false, storageKey }) => {
+  const [toggle, setToggle] = useState(true);
+  const [text, setText] = useState(() => {
+    // Retrieve data from localStorage on component initialization
+    const storedData = localStorage.getItem(storageKey);
+    return storedData || defaultText;
+  });
 
-    const [toggle, setToggle] = useState(true);
-    const toggleChecked = () => { 
-      if (!toggle) {
-        onChange(text)
-      }
-      setToggle(toggle => !toggle);
+  const toggleChecked = () => {
+    if (!toggle) {
+      // Save the data to localStorage when toggling
+      localStorage.setItem(storageKey, text);
+      onChange(text);
     }
-    let [text, setText] = React.useState(defaultText);
+    setToggle((prevToggle) => !prevToggle);
+  };
 
-    const onChangeInputBase = (a, b) => {
-        setText(a.target.value)
+  const onChangeInputBase = (e) => {
+    const updatedText = e.target.value;
+    setText(updatedText);
+
+    if (toggle) {
+      // Save the data to localStorage as the user types
+      localStorage.setItem(storageKey, updatedText);
     }
+  };
 
-    return (
-       <div style={{position: "relative"}}>
-          {toggle && <TextFieldCloseable text={text} usePureJs={usePureJs}/> }
-          {!toggle && <InputBase value={text} onChange={onChangeInputBase} fullWidth multiline inputProps={{className: 'request-editor'}} variant="standard"/>}
-          <div style={{position: "absolute", top: "4px", right: "10px"}}>
-            <IconButton onClick={toggleChecked}>
-                <FontAwesomeIcon icon={toggle ? faEdit : faCheckSquare} className="primary-btn" />
-            </IconButton>
-          </div>
-       </div>
-    );
-  }
+  return (
+    <div style={{ position: 'relative' }}>
+      {toggle && <TextFieldCloseable text={text} usePureJs={usePureJs} />}
+      {!toggle && (
+        <InputBase
+          value={text}
+          onChange={onChangeInputBase}
+          fullWidth
+          multiline
+          inputProps={{ className: 'request-editor' }}
+          variant="standard"
+        />
+      )}
+      <div style={{ position: 'absolute', top: '4px', right: '10px' }}>
+        <IconButton onClick={toggleChecked}>
+          <FontAwesomeIcon icon={toggle ? faEdit : faCheckSquare} className="primary-btn" />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
-export default TemplateStringEditor
+export default TemplateStringEditor;
