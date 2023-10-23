30a8ba8ae4fe3b7aad7f98552bee1177a208ee0c
Zubeen <zubeenqadry@gmail.com>
2023-10-24 00:43:08 +0530

    Add tests to TestFintechTypes.java

--- apps/dashboard/src/test/java/com/akto/listener/TestFintechTypes.java
+++ apps/dashboard/src/test/java/com/akto/listener/TestFintechTypes.java
@@ -69,21 +69,25 @@ public class TestFintechTypes extends MongoBasedTest {
                     assertTrue(cdt.validate("123 MAIN ST, SAN JOSE, CA 11111", "foo"));
                     assertFalse(cdt.validate("PO BOX 123, SAN JOSE, CA 11111", "foo"));
                     break;
-                case "MySQL URI":
-                    assertTrue(cdt.validate("mysqlx://user_name@server.example.com/", "foo"));
-                    assertFalse(cdt.validate("mysql://user@localhost:3306?get-server-public-key=true", "foo"));
+                 case "AWS Secret Access Key":
+                    assertTrue(cdt.validate("3pHXfQaoxG0ZUlgor7GpflAgOYnIZiHCgtgEgIJUyMC", "foo"));
+                    assertFalse(cdt.validate("1g47WR2iBOWklti3ywvbEXC/97k0H4KmF3uR8VyGUe1", "foo"));
                     break;
-                 case "Redis URI":
-                    assertTrue(cdt.validate("redis://localhost:6379?ConnectTimeout=5000&IdleTimeOutSecs=180", "foo"));
-                    assertFalse(cdt.validate("rediss://myuser:mypassword@redis.example.com:6380/0?timeout=30s&clientName=myClient&libraryName=redis-lib&libraryVersion=2.0", "foo"));
+                 case "AWS Secret Access ID":
+                    assertTrue(cdt.validate("nA3mQGcxhzIoGwNZWgL0", "foo"));
+                    assertFalse(cdt.validate("tMB873rCOIwY9qyVCBTX", "foo"));
                     break;
-                 case "Amazon S3":
-                    assertTrue(cdt.validate("http://s3.amazonaws.com/domain/photos/user_thumbnail/casing-earphones.jpg?1365720318", "foo"));
-                    assertFalse(cdt.validate("http://s3.amazonaws.com/text1/test2.csv", "foo"));
+                 case "Github Personal Access Token (Fine Grained)":
+                    assertTrue(cdt.validate("github_pat_2Py9UArHCDGMhveqKDLfm29BtckaU797TPgapL0F83r33JVjwH3b7U5RLPVcDVXKmHtZHDVScF", "foo"));
+                    assertFalse(cdt.validate("github_pat_xh9NXr8Me5iy9YtCsXo9MKyg82Wzzhwn5wqdjT9dznlKG6PTGvUPHDQrKgipb530wf63wnwiD7", "foo"));
                     break;
-                  case "MongoDB URI":
-                    assertTrue(cdt.validate("mongodb://sally:sallyspassword@dbserver.example:5555/userdata?tls=true&connectionTimeout=5000", "foo"));
-                    assertFalse(cdt.validate("mongodb+srv://rohit:asdf1234@beyondthebasics.abcde.mongodb.net/test", "foo"));
+                 case "Github Personal Access Token (Classic)":
+                    assertTrue(cdt.validate("ghp_fIBZhdFTJiR1Xzxst2yD9pYUx1RF5fweugymMG", "foo"));
+                    assertFalse(cdt.validate("ghp_fIBZhdFTJiR1Xzxst2yD9pYUx1RF5fweugymZZ", "foo"));
+                    break;
+                case "GCP API Access Keys":
+                    assertTrue(cdt.validate("-63SdnqdhQX1amyET4ECsAXzXZZq1ytEsrNNFfv41RYMGq", "foo"));
+                    assertFalse(cdt.validate("l1nuYtWF4oCABpD68Icw6Zz818wgxTsEcLDc5T0VFEHDqp", "foo"));
                     break;
                 default:
                     break;
