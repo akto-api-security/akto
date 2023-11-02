8d9d91fdebc0833462e04df30236739b5203dea9
Ark2307 <kr.aryan2307@gmail.com>
2023-11-02 11:41:59 +0530

    Finished api changes screen 1

---
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/shared/SummaryCardInfo.jsx
@@ -0,0 +1,27 @@
+import { Box, Card, HorizontalGrid, Text, VerticalStack } from '@shopify/polaris'
+import React from 'react'
+
+function SummaryCardInfo({summaryItems}) {
+    return (
+        <Card padding={0} key="info">
+            <Box padding={2} paddingInlineStart={4} paddingInlineEnd={4}>
+                <HorizontalGrid columns={summaryItems.length} gap={4}>
+                    {summaryItems.map((item, index) => (
+                        <Box borderInlineEndWidth={index < (summaryItems.length - 1) ? "1" : ""} key={index} paddingBlockStart={1} paddingBlockEnd={1} borderColor="border-subdued">
+                            <VerticalStack gap="1">
+                                <Text color="subdued" variant="headingXs">
+                                    {item.title}
+                                </Text>
+                                <Text variant="bodyMd" fontWeight="semibold" color={item.color ? item.color :""}>
+                                    {item.data}
+                                </Text>
+                            </VerticalStack>
+                        </Box>
+                    ))}
+                </HorizontalGrid>
+            </Box>
+        </Card>
+    )
+}
+
+export default SummaryCardInfo
