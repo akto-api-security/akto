eef6dbabd454ec2cccf4ffdc46fa946758785dbb
Ankush Jain <ankush@akto.io>
2023-11-27 20:19:31 +0530

    provision customer and report usage

--- libs/dao/src/main/java/com/akto/dto/usage/MetricTypes.java
+++ libs/dao/src/main/java/com/akto/dto/usage/MetricTypes.java
@@ -6,8 +6,18 @@ package com.akto.dto.usage;
  */
 
 public enum MetricTypes {
-    ACTIVE_ENDPOINTS,
-    CUSTOM_TESTS,
-    TEST_RUNS,
-    ACTIVE_ACCOUNTS
+    ACTIVE_ENDPOINTS ("feature-ap-is"),
+    CUSTOM_TESTS ("feature-custom-templates"),
+    TEST_RUNS ("feature-test-runs"),
+    ACTIVE_ACCOUNTS ("feature-sso");
+
+    public final String label;
+
+    private MetricTypes(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
