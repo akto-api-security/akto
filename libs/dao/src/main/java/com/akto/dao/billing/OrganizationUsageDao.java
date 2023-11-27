cc85a571d2d6dd81a5da0d60ae14c3e3b096306d
Ankush Jain <ankush@akto.io>
2023-11-27 11:21:10 +0530

    init feed framework

---
+++ libs/dao/src/main/java/com/akto/dao/billing/OrganizationUsageDao.java
@@ -0,0 +1,31 @@
+package com.akto.dao.billing;
+
+import com.akto.dao.BillingContextDao;
+import com.akto.dao.MCollection;
+import com.akto.dto.billing.OrganizationUsage;
+
+import static com.akto.dto.billing.OrganizationUsage.DATE;
+import static com.akto.dto.billing.OrganizationUsage.ORG_ID;
+
+public class OrganizationUsageDao extends BillingContextDao<OrganizationUsage> {
+
+
+    public static final OrganizationUsageDao instance = new OrganizationUsageDao();
+
+    public static void createIndexIfAbsent() {
+        String[] fieldNames = {ORG_ID, DATE};
+        MCollection.createIndexIfAbsent(instance.getDBName(), instance.getCollName(), fieldNames, false);
+    }
+
+    private OrganizationUsageDao() {}
+
+    @Override
+    public String getCollName() {
+        return "organization_usage";
+    }
+
+    @Override
+    public Class<OrganizationUsage> getClassT() {
+        return OrganizationUsage.class;
+    }
+}
