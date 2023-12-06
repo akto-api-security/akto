f852204ce498086ffcc480eff18201eee8fab604
Avneesh Hota <avneesh@akto.io>
2023-12-06 15:41:12 +0530

    added index for sorting

--- libs/dao/src/main/java/com/akto/dao/testing_run_findings/TestingRunIssuesDao.java
+++ libs/dao/src/main/java/com/akto/dao/testing_run_findings/TestingRunIssuesDao.java
@@ -1,12 +1,33 @@
 package com.akto.dao.testing_run_findings;
 
 import com.akto.dao.AccountsContextDao;
+import com.akto.dao.MCollection;
+import com.akto.dao.context.Context;
 import com.akto.dto.test_run_findings.TestingRunIssues;
 import com.akto.util.enums.MongoDBEnums;
 
 public class TestingRunIssuesDao extends AccountsContextDao<TestingRunIssues> {
 
     public static final TestingRunIssuesDao instance = new TestingRunIssuesDao();
+
+    public void createIndicesIfAbsent() {
+
+        boolean exists = false;
+        for (String col : clients[0].getDatabase(Context.accountId.get() + "").listCollectionNames()) {
+            if (getCollName().equalsIgnoreCase(col)) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            clients[0].getDatabase(Context.accountId.get() + "").createCollection(getCollName());
+        }
+
+        String[] fieldNames = {TestingRunIssues.TEST_RUN_ISSUES_STATUS, TestingRunIssues.CREATION_TIME};
+        MCollection.createIndexIfAbsent(getDBName(), getCollName(), fieldNames, true);
+
+    }
 
     private TestingRunIssuesDao() {}
     @Override
