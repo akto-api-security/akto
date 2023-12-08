01c7dd4a8831af1d70f20f41b2db4b45236a7d4c
Ark2307 <kr.aryan2307@gmail.com>
2023-12-08 13:34:42 +0530

    fixed copytoclipboard function call

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/shared/CopyCommand.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/shared/CopyCommand.jsx
@@ -1,21 +1,24 @@
 import { Box, Button, TextField, Tooltip } from '@shopify/polaris'
 import { ClipboardMinor } from "@shopify/polaris-icons"
-import React from 'react'
+import React, { useRef } from 'react'
 
 import func from "@/util/func";
 
 function CopyCommand({command}) {
 
+    const ref = useRef(null) ;
     const copyContent = () => {
-        navigator.clipboard.writeText(command)
-        func.setToast(true, false, "Command copied !")
+        func.copyToClipboard(command, ref, "Command copied.");
     }
 
     return (
         <Box background="bg-subdued" padding={2} borderRadius="1">
             <TextField 
                 connectedRight={(<Tooltip dismissOnMouseOut content="Copy URL">
-                    <Button onClick={copyContent} icon={ClipboardMinor} />
+                    <Box>
+                        <div ref={ref} />
+                        <Button onClick={copyContent} icon={ClipboardMinor} />
+                    </Box>
                 </Tooltip>)}
                 value={command}
             />
