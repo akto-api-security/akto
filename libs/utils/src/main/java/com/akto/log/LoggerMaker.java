ca55aff29adeaf7d01d30461c97b338d5bc3084f
aktoboy <bhavik@akto.io>
2023-12-22 17:21:30 +0530

    Added account id to logs

--- libs/utils/src/main/java/com/akto/log/LoggerMaker.java
+++ libs/utils/src/main/java/com/akto/log/LoggerMaker.java
@@ -99,6 +99,9 @@ public class LoggerMaker  {
     public void errorAndAddToDb(Exception e, String err, LogDb db) {
         StackTraceElement stackTraceElement = e.getStackTrace()[0];
         err = String.format("Err msg: %s\nClass: %s\nFile: %s\nLine: %d", err, stackTraceElement.getClassName(), stackTraceElement.getFileName(), stackTraceElement.getLineNumber());
+        if(Context.accountId.get() != null){
+            err = String.format("%s\nAccount id: %d", err, Context.accountId.get());
+        }
         logger.error(err);
         try{
             insert(err, "error", db);
