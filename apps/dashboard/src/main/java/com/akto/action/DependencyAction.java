d6df981f33a0bf9dbfd9e6d6cc84fe9529a575ae
Avneesh Hota <avneesh@akto.io>
2023-12-23 14:33:36 +0530

    made ui changes for showing all the parents

--- apps/dashboard/src/main/java/com/akto/action/DependencyAction.java
+++ apps/dashboard/src/main/java/com/akto/action/DependencyAction.java
@@ -1,14 +1,11 @@
 package com.akto.action;
 
-import com.akto.dao.DependencyNodeDao;
-import com.akto.dto.ApiInfo;
 import com.akto.dto.DependencyNode;
+import com.akto.dto.dependency_flow.Node;
+import com.akto.dto.dependency_flow.TreeHelper;
 import com.akto.dto.type.URLMethods;
-import com.mongodb.BasicDBObject;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.Collection;
 
 public class DependencyAction extends UserAction {
 
@@ -16,51 +13,24 @@ public class DependencyAction extends UserAction {
     private String url;
     private URLMethods.Method method;
 
-    private List<BasicDBObject> result;
+    private Collection<Node> result;
 
     @Override
     public String execute()  {
-        result = new ArrayList<>();
-        List<DependencyNode> nodes = DependencyNodeDao.fetchParentAndChildrenNodes(apiCollectionId, url, method);
-
-        List<ApiInfo.ApiInfoKey> apiInfoKeysParent = new ArrayList<>();
-        List<ApiInfo.ApiInfoKey> apiInfoKeysChildren = new ArrayList<>();
-        for (DependencyNode node : nodes) {
-            if (isParentNode(node, apiCollectionId, url, method.name())) {
-                apiInfoKeysParent.add(new ApiInfo.ApiInfoKey(Integer.parseInt(node.getApiCollectionIdResp()), node.getUrlResp(), URLMethods.Method.valueOf(node.getMethodResp())));
-            } else {
-                apiInfoKeysChildren.add(new ApiInfo.ApiInfoKey(Integer.parseInt(node.getApiCollectionIdReq()), node.getUrlReq(), URLMethods.Method.valueOf(node.getMethodReq())));
-            }
-        }
-
-        Map<ApiInfo.ApiInfoKey, List<DependencyNode>> parentNodes = DependencyNodeDao.fetchDependentNodes(apiInfoKeysParent, true);
-        Map<ApiInfo.ApiInfoKey, List<DependencyNode>> childrenNodes = DependencyNodeDao.fetchDependentNodes(apiInfoKeysChildren, false);
-
-        for (DependencyNode node: nodes) {
-            BasicDBObject basicDBObject = new BasicDBObject();
-            basicDBObject.put("node", node);
-            int count = 0;
-            if (isParentNode(node, apiCollectionId, url, method.name())) {
-                ApiInfo.ApiInfoKey apiInfoKey = new ApiInfo.ApiInfoKey(Integer.parseInt(node.getApiCollectionIdResp()), node.getUrlResp(), URLMethods.Method.valueOf(node.getMethodResp()));
-                count = parentNodes.getOrDefault(apiInfoKey, new ArrayList<>()).size();
-            } else {
-                ApiInfo.ApiInfoKey apiInfoKey = new ApiInfo.ApiInfoKey(Integer.parseInt(node.getApiCollectionIdReq()), node.getUrlReq(), URLMethods.Method.valueOf(node.getMethodReq()));
-                count = childrenNodes.getOrDefault(apiInfoKey, new ArrayList<>()).size();
-            }
-            basicDBObject.put("nodeInfo", new BasicDBObject("dependents", count));
-            result.add(basicDBObject);
-        }
-
+        TreeHelper treeHelper = new TreeHelper();
+        treeHelper.buildTree(apiCollectionId+"", url, method.name());
+        this.result = treeHelper.result.values();
         return SUCCESS.toUpperCase();
     }
-
 
 
     public static boolean isParentNode(DependencyNode node, int apiCollectionId, String url, String method) {
         return (apiCollectionId+"#"+url+"#"+method).equals(node.getApiCollectionIdReq()+"#"+ node.getUrlReq()+"#"+node.getMethodReq());
     }
 
-    public List<BasicDBObject> getResult() {
+    
+
+    public Collection<Node> getResult() {
         return result;
     }
 
