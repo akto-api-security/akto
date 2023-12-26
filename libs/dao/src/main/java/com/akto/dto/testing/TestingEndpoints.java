df01b41bceb22e28950233379be1e02400bfa381
notshivansh <shivansh@akto.io>
2023-12-26 14:31:08 +0530

    add collection types

--- libs/dao/src/main/java/com/akto/dto/testing/TestingEndpoints.java
+++ libs/dao/src/main/java/com/akto/dto/testing/TestingEndpoints.java
@@ -17,7 +17,7 @@ public abstract class TestingEndpoints {
 
 
     public enum Type {
-        CUSTOM, COLLECTION_WISE, WORKFLOW, LOGICAL_GROUP, API_LIST
+        CUSTOM, COLLECTION_WISE, WORKFLOW, LOGICAL_GROUP, API_LIST, METHOD, PARAM, TIMESTAMP
     }
 
     public Type getType() {
