a8ba7c0b302ee42908592ff638d5a93564515ad9
ayushaga14 <ayush@akto.io>
2024-01-03 12:17:25 +0530

    modify sendgrid key

--- libs/utils/src/main/java/com/akto/notifications/email/Constants.java
+++ libs/utils/src/main/java/com/akto/notifications/email/Constants.java
@@ -3,5 +3,5 @@ package com.akto.notifications.email;
 public class Constants {
     private Constants() {}
 
-    public static final String SENDGRID_TOKEN_KEY = "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";
+    public static final String SENDGRID_TOKEN_KEY = "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";
 }
