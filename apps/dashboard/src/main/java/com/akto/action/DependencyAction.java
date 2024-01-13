cd78da06f6ccc9bc435e08c91e1d33d126ce15eb
Avneesh Hota <avneesh@akto.io>
2024-01-13 12:13:04 +0530

    backend for dependency table api

--- apps/dashboard/src/main/java/com/akto/action/DependencyAction.java
+++ apps/dashboard/src/main/java/com/akto/action/DependencyAction.java
@@ -1,10 +1,15 @@
 package com.akto.action;
 
+import com.akto.dao.ApiInfoDao;
+import com.akto.dao.DependencyFlowNodesDao;
+import com.akto.dto.ApiInfo;
 import com.akto.dto.dependency_flow.Node;
 import com.akto.dto.dependency_flow.TreeHelper;
 import com.akto.dto.type.URLMethods;
+import com.mongodb.client.model.Filters;
 
 import java.util.Collection;
+import java.util.List;
 
 public class DependencyAction extends UserAction {
 
@@ -22,6 +27,16 @@ public class DependencyAction extends UserAction {
         return SUCCESS.toUpperCase();
     }
 
+    List<Integer> apiCollectionIds;
+    List<ApiInfo> apiInfoList;
+    List<Node> nodes;
+    public String buildDependencyTable() {
+        this.apiInfoList = ApiInfoDao.instance.findApiInfos(apiCollectionIds);
+        this.nodes = DependencyFlowNodesDao.instance.findNodesForCollectionIds(apiCollectionIds);
+        return SUCCESS.toUpperCase();
+    }
+
+
 
     public Collection<Node> getResult() {
         return result;
@@ -37,5 +52,17 @@ public class DependencyAction extends UserAction {
 
     public void setMethod(URLMethods.Method method) {
         this.method = method;
+    }
+
+    public void setApiCollectionIds(List<Integer> apiCollectionIds) {
+        this.apiCollectionIds = apiCollectionIds;
+    }
+
+    public List<ApiInfo> getApiInfoList() {
+        return apiInfoList;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
     }
 }
