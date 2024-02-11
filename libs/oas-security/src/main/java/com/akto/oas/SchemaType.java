f8ec3897734e2cb13dd7b12f9d1caded9ac1803e
Ankush Jain <ankush@akto.io>
2024-02-11 22:13:42 +0530

    remove oas-security

--- libs/oas-security/src/main/java/com/akto/oas/SchemaType.java
+++
@@ -1,8 +0,0 @@
-package com.akto.oas;
-public class SchemaType {
-    public static final String STRING = "string";
-    public static final String OBJECT = "object";
-    public static final String INTEGER = "integer";
-    public static final String NUMBER = "number";
-    public static final String ARRAY = "array";
-}
