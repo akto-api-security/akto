1b7fa1859d29173c5cc2c338d5fd0ad093174169
notshivansh <shivansh@akto.io>
2024-02-12 14:44:44 +0530

    update design

--- apps/dashboard/web/polaris_web/web/src/apps/main/ExpiredApp.js
+++ apps/dashboard/web/polaris_web/web/src/apps/main/ExpiredApp.js
@@ -1,9 +1,29 @@
-import CenteredCard from "../dashboard/components/shared/CenteredCard";
+import { Link, Text } from "@shopify/polaris";
+import EmptyScreensLayout from "../dashboard/components/banners/EmptyScreensLayout";
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
 
 function ExpiredApp() {
-    const message = "Please upgrade to the latest version to continue using akto. For any queries contact support@akto.io."
+    const message = <Text>Please upgrade to the latest version to continue using akto. For any queries contact <Link url="mailto:support@akto.io">support@akto.io</Link>.</Text>
+    const router = createBrowserRouter([
+        {
+            path: "*",
+            element: <div style={{
+                padding: "80px"
+            }}>
+                <EmptyScreensLayout key={"emptyScreen"}
+                    iconSrc={"/public/upgrade.svg"}
+                    headingText={"Upgrade to latest version"}
+                    description={message}
+                />
+            </div>
+        }
+    ])
+
     return (
-        <CenteredCard message={message} />
+        <RouterProvider router={router} />
     )
 }
 
