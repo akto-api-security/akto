678cdae46ef477710efff93efac6c88399166afa
notshivansh <shivansh@akto.io>
2024-02-17 18:00:57 +0530

    update code for current specs

--- apps/billing/src/main/java/com/akto/action/usage/UsageAction.java
+++ apps/billing/src/main/java/com/akto/action/usage/UsageAction.java
@@ -1,67 +1,42 @@
 package com.akto.action.usage;
 
 import java.util.Set;
-import java.util.function.Consumer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.akto.util.UsageCalculator;
 import com.akto.util.UsageUtils;
-import com.akto.util.tasks.OrganizationTask;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
 import com.akto.dao.context.Context;
 import com.akto.dao.billing.OrganizationsDao;
 import com.akto.dao.usage.UsageMetricsDao;
-import com.akto.dao.usage.UsageMetricInfoDao;
 import com.akto.dto.billing.Organization;
-import com.akto.dto.test_editor.YamlTemplate;
 import com.akto.dto.usage.MetricTypes;
 import com.akto.dto.usage.UsageMetric;
-import com.akto.dto.usage.UsageMetricInfo;
 import com.akto.dto.usage.metadata.ActiveAccounts;
+import com.akto.listener.InitializerListener;
 import com.akto.log.LoggerMaker;
 import com.akto.log.LoggerMaker.LogDb;
 import com.google.gson.Gson;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import com.opensymphony.xwork2.Action;
-import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.opensymphony.xwork2.ActionSupport;
 
-public class UsageAction implements ServletRequestAware {
+public class UsageAction extends ActionSupport implements ServletRequestAware {
     private UsageMetric usageMetric;
     private HttpServletRequest request;
+
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     private static final LoggerMaker loggerMaker = new LoggerMaker(UsageAction.class);
     private int usageLowerBound;
     private int usageUpperBound;
 
     private String organizationId;
-
-    public String aggregateAccountWiseUsage() {
-
-        OrganizationTask.instance.executeTask(new Consumer<Organization>() {
-            @Override
-            public void accept(Organization organization) {
-
-                UsageCalculator.instance.aggregateUsageForOrg(organization, usageLowerBound, usageUpperBound);
-            }
-        }, "aggregateAccountWiseUsage");
-
-
-        return SUCCESS.toUpperCase();
-    }
-
-    public String sendDataToSinks() {
-        OrganizationTask.instance.executeTask(new Consumer<Organization>() {
-            @Override
-            public void accept(Organization organization) {
-                UsageCalculator.instance.sendOrgUsageDataToAllSinks(organization);
-            }
-        }, "aggregateAccountWiseUsage");
-
-        return SUCCESS.toUpperCase();
-    }
 
     public String ingestUsage() {
         try {
@@ -113,26 +88,39 @@ public class UsageAction implements ServletRequestAware {
     }
 
     public String flushUsageDataForOrg(){
+
+        if(organizationId == null || organizationId.isEmpty()){
+            addActionError("Organization id not provided");
+            return Action.ERROR.toUpperCase();
+        }
+
         try {
             Organization organization = OrganizationsDao.instance.findOne(Filters.eq(Organization.ID, organizationId));
             if (organization == null) {
-                loggerMaker.errorAndAddToDb(String.format("Organization %s does not exist", organizationId), LogDb.BILLING);
+                String message = String.format("Organization %s does not exist", organizationId);
+                addActionError(message);
+                loggerMaker.errorAndAddToDb(message, LogDb.BILLING);
                 return Action.ERROR.toUpperCase();
             }
-            if (usageLowerBound == 0 || usageUpperBound == 0) {
-                loggerMaker.infoAndAddToDb("Usage bounds not provided. Using default bounds", LogDb.BILLING);
-                int now = Context.now();
-                usageLowerBound = now - (now % UsageUtils.USAGE_UPPER_BOUND_DL) - UsageUtils.USAGE_UPPER_BOUND_DL;
-                usageUpperBound = usageLowerBound + UsageUtils.USAGE_UPPER_BOUND_DL;
-            }
-            loggerMaker.infoAndAddToDb(String.format("Aggregating usage for organization %s between %d and %d", organizationId, usageLowerBound, usageUpperBound), LogDb.BILLING);
-            UsageCalculator.instance.aggregateUsageForOrg(organization, usageLowerBound, usageUpperBound);
-            loggerMaker.infoAndAddToDb(String.format("Sending usage data to sinks for organization %s", organizationId), LogDb.BILLING);
-            UsageCalculator.instance.sendOrgUsageDataToAllSinks(organization);
-            loggerMaker.infoAndAddToDb(String.format("Flushed usage data for organization %s", organizationId), LogDb.BILLING);
+            int now = Context.now();
+            /*
+             * since we just recorded and sent the data from dashboard,
+             * we need to set the limits to check for the current epoch,
+             * even though it would be a non-standard epoch.
+             */
+            usageLowerBound = now - UsageUtils.USAGE_UPPER_BOUND_DL;
+            usageUpperBound = usageLowerBound + UsageUtils.USAGE_UPPER_BOUND_DL;
+            executorService.schedule(new Runnable() {
+                public void run() {
+                    InitializerListener.aggregateAndSinkUsageData(organization, usageLowerBound, usageUpperBound);
+                    loggerMaker.infoAndAddToDb(String.format("Flushed usage data for organization %s", organizationId), LogDb.BILLING);
+                }
+            }, 0, TimeUnit.SECONDS);
             return SUCCESS.toUpperCase();
         } catch (Exception e) {
-            loggerMaker.errorAndAddToDb(String.format("Error while flushing usage data for organization %s. Error: %s", organizationId, e.getMessage()), LogDb.BILLING);
+            String commonMessage = "Error while flushing usage data for organization";
+            loggerMaker.errorAndAddToDb(e, String.format( commonMessage + " %s. Error: %s", organizationId, e.getMessage()), LogDb.BILLING);
+            addActionError(commonMessage);
             return Action.ERROR.toUpperCase();
         }
     }
--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/settings/billing/Billing.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/settings/billing/Billing.jsx
@@ -48,19 +48,25 @@ function Billing() {
     })
 
     async function refreshUsageData(){
-        await billingApi.refreshUsageData({organizationId: window.STIGG_CUSTOMER_ID})
+        await billingApi.refreshUsageData()
+        func.setToast(true, false, `Syncing usage data. Please refresh after some time.`)
+        setTimeout(() => {
+            window.location.reload();
+        }, 10000)
     }
 
 
     const usageTitle = (
-        <div>
-            <Box paddingBlockEnd="4">
-                <Text variant="headingMd">Your plan</Text>
-            </Box>
-            <Button onClick={() => refreshUsageData()}>
-                Sync usage data
-            </Button>
-        </div>
+        <Box paddingBlockEnd="4">
+            <HorizontalStack align="space-between" blockAlign="center">
+                <Box>
+                    <Text variant="headingMd">Your plan</Text>
+                </Box>
+                <Button onClick={() => refreshUsageData()}>
+                    Sync usage data
+                </Button>
+            </HorizontalStack>
+        </Box>
     )
 
     const usageInfo = (
