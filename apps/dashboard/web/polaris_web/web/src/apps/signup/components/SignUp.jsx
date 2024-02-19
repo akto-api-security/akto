8109eee5999d1ef98c22a2287331f8624d4b832b
Ark2307 <kr.aryan2307@gmail.com>
2024-02-19 14:26:49 +0530

    fixing reviews

--- apps/dashboard/web/polaris_web/web/src/apps/signup/components/SignUp.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/signup/components/SignUp.jsx
@@ -124,27 +124,28 @@ function SignUp() {
             <div style={{display: "flex", justifyContent: 'space-between', flexDirection: "column"}}>
               <HorizontalStack align="center">
                 <Box width='400px'>
-                  <VerticalStack gap={"16"}>
+                  <VerticalStack gap={8}>
+                    <HorizontalStack align='center'>
+                      <div className="akto-logo">
+                        <Avatar source="/public/akto_name_with_logo.svg" shape="round" size="2xl-experimental" />
+                      </div>
+                    </HorizontalStack>
                     <VerticalStack gap={8}>
-                      <HorizontalStack align='center'>
-                        <div className="akto-logo">
-                          <Avatar source="/public/akto_name_with_logo.svg" shape="round" size="2xl-experimental" />
-                        </div>
-                      </HorizontalStack>
                       <Text alignment="center" variant="heading2xl">{activeObject.headingText}</Text>
                       <VerticalStack gap={5}>
-                          {ssoCard}
-                          {signupEmailCard}
-                        </VerticalStack>
+                        {ssoCard}
+                        {signupEmailCard}
+                      </VerticalStack>
                     </VerticalStack>
-                    <Box paddingBlockStart={"2"}>
-                      <HorizontalStack gap={3} align="center">
-                        <Button plain onClick={() => window.open("https://www.akto.io/terms-and-policies","_blank")}>Terms of use</Button>
-                        <div style={{width: '1px', height: '24px', background: "#E1E3E5"}} />
-                        <Button plain onClick={() => window.open("https://www.akto.io/terms/privacy","_blank")}>Privacy policy</Button>
-                      </HorizontalStack>
-                    </Box>
+
                   </VerticalStack>
+                  <div style={{bottom: "40px", position: "absolute", width: '400px'}}>
+                    <HorizontalStack gap={3} align="center">
+                      <Button plain onClick={() => window.open("https://www.akto.io/terms-and-policies","_blank")}>Terms of use</Button>
+                      <div style={{width: '1px', height: '24px', background: "#E1E3E5"}} />
+                      <Button plain onClick={() => window.open("https://www.akto.io/terms/privacy","_blank")}>Privacy policy</Button>
+                    </HorizontalStack>
+                  </div>
                 </Box>
               </HorizontalStack>
             </div>
