3e663c17eba0faa1d3d577e9d6429d4f287ad7ef
Ark2307 <kr.aryan2307@gmail.com>
2024-02-19 13:27:31 +0530

    made request response in http format in editor

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/shared/customEditor.js
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/shared/customEditor.js
@@ -1,4 +1,4 @@
-import { editor, languages } from "monaco-editor/esm/vs/editor/editor.api"
+import { languages } from "monaco-editor/esm/vs/editor/editor.api"
 
 const LANGUAGE_ID = 'custom_http'
 const editorSetup = {
@@ -21,46 +21,23 @@ const editorSetup = {
             symbols: symbols,
             tokenizer: {
                 root: [
-                    [/#.*$/, 'comment'],
-                    [/HTTP\/[0-9.]+/, 'keyword'],
-                    [/GET|POST|PUT|DELETE|PATCH|OPTIONS|HEAD/, 'keyword'],
-                    [/"[^"]+":/, 'key'], // Match any string in double quotes followed by a colon as JSON key
-                    [/".*?"(?=[:,\s}\]])/, 'string'], // Match values enclosed in double quotes
-                    [/\btrue\b|\bfalse\b|\bnull\b/, 'keyword'], // Match JSON keywords (true, false, null)
-                    [/[0-9]+/, 'number'], // Match numbers
-                    [/[{}\[\],]/, 'delimiter'], // Match JSON delimiters: {}, [], and commas
-                    [/[a-zA-Z-]+(?=:)/, 'key'], // Match other keys (e.g., response_type)
-                    [/[^:\s]+/, 'value'], // Match any remaining text as a value
+                    [/#.*$/, 'comment-http'],
+                    [/HTTP\/[0-9.]+/, 'keyword-http'],
+                    [/GET|POST|PUT|DELETE|PATCH|OPTIONS|HEAD/, 'keyword-http'],
+                    [/"[^"]+":/, 'key-http'], // Match any string in double quotes followed by a colon as JSON key
+                    [/".*?"(?=[:,\s}\]])/, 'string-http'], // Match values enclosed in double quotes
+                    [/\btrue\b|\bfalse\b|\bnull\b/, 'keyword-http'], // Match JSON keywords (true, false, null)
+                    [/[0-9]+/, 'number-http'], // Match numbers
+                    [/[{}\[\],]/, 'delimiter-http'], // Match JSON delimiters: {}, [], and commas
+                    [/[a-zA-Z-]+(?=:)/, 'key-http'], // Match other keys (e.g., response_type)
+                    [/[^:\s]+/, 'value-http'], // Match any remaining text as a value
                   ],
                 whitespace: [
                     [/[ \t\r\n]+/, 'white'],
                 ],
             },
         });
-    },
-
-    setEditorTheme: function(){
-        editor.defineTheme("customTheme", {
-            base: "vs",
-            inherit: false,
-            rules: [
-                { token: 'comment', foreground: '#008800', fontStyle: 'italic' },
-                { token: 'keyword', foreground: '#0000FF' },
-                { token: 'key', foreground: '#A31515'}, // Color for keys
-                { token: 'jsonkey', foreground: '#A31515' }, // Color for strings
-                { token: 'number', foreground: '#0451A5', fontStyle: "bold" }, // Color for numbers
-                { token: 'value', foreground: '#0451A5' }, // Color for values
-                { token: 'delimeter', foreground: '#000000'},
-                { token: 'default', foreground: '#0451A5'}
-            ],
-                colors: {
-                    'editorLineNumber.foreground': '#000000',
-                    'editorLineNumber.activeForeground': '#000000',
-                    'editorIndentGuide.background': '#D3D3D3'
-                }
-        });
-    },
-
+    }
 }
 
 export default editorSetup
