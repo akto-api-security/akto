91f9ad9e5da9b4d7bcb36fbb3fe4cf5bbb58f164
TangoBeee <zonebattle90@gmail.com>
2024-04-09 14:51:43 +0530

    fix: Remove unwanted github workflow

--- .github/scripts/deploy-script.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd ~/akto/infra
-docker-compose -f docker-compose-dashboard.yml pull
-docker-compose -f docker-compose-dashboard.yml down
-docker-compose -f docker-compose-dashboard.yml up -d
