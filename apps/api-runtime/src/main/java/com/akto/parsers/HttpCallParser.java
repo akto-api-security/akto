9d8f21eafa6590ebd44088fb224b6f8dbb1b50c9
Avneesh Hota <avneesh@akto.io>
2024-04-18 19:59:26 +0530

    disable dependency graph

--- apps/api-runtime/src/main/java/com/akto/parsers/HttpCallParser.java
+++ apps/api-runtime/src/main/java/com/akto/parsers/HttpCallParser.java
@@ -204,17 +204,17 @@ public class HttpCallParser {
             apiCatalogSync.computeDelta(aggregator, false, apiCollectionId);
         }
 
-         for (HttpResponseParams responseParam: filteredResponseParams) {
-             dependencyAnalyser.analyse(responseParam.getOrig(), responseParam.requestParams.getApiCollectionId());
-         }
+        //  for (HttpResponseParams responseParam: filteredResponseParams) {
+        //      dependencyAnalyser.analyse(responseParam.getOrig(), responseParam.requestParams.getApiCollectionId());
+        //  }
 
         this.sync_count += filteredResponseParams.size();
         int syncThresh = numberOfSyncs < 10 ? 10000 : sync_threshold_count;
         if (syncImmediately || this.sync_count >= syncThresh || (Context.now() - this.last_synced) > this.sync_threshold_time || isHarOrPcap) {
             numberOfSyncs++;
             apiCatalogSync.syncWithDB(syncImmediately, fetchAllSTI);
-            dependencyAnalyser.dbState = apiCatalogSync.dbState;
-            dependencyAnalyser.syncWithDb();
+            // dependencyAnalyser.dbState = apiCatalogSync.dbState;
+            // dependencyAnalyser.syncWithDb();
             syncTrafficMetricsWithDB();
             this.last_synced = Context.now();
             this.sync_count = 0;
