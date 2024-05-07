0aa00bbb7a96c73416b985c67409c7a02eb3894e
notshivansh <shivansh@akto.io>
2024-05-08 03:18:10 +0530

    fix TLS proxy implementation and add unit tests

--- libs/utils/src/main/java/com/akto/testing/HTTPClientHandler.java
+++ libs/utils/src/main/java/com/akto/testing/HTTPClientHandler.java
@@ -8,14 +8,8 @@ import okhttp3.*;
 import okio.Buffer;
 import org.jetbrains.annotations.NotNull;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -30,7 +24,7 @@ public class HTTPClientHandler {
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(readTimeout, TimeUnit.SECONDS)
                 .connectionPool(new ConnectionPool(256, 5L, TimeUnit.MINUTES))
-                .sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager)trustAllCerts[0])
+                .sslSocketFactory(CoreHTTPClient.trustAllSslSocketFactory, (X509TrustManager)CoreHTTPClient.trustAllCerts[0])
                 .hostnameVerifier((hostname, session) -> true)
                 .followRedirects(followRedirects);
     }
@@ -129,35 +123,6 @@ public class HTTPClientHandler {
             this.testLogs = testLogs;
         }
     }
-
-    private static final TrustManager[] trustAllCerts = new TrustManager[] {
-            new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                @Override
-                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                @Override
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return new java.security.cert.X509Certificate[]{};
-                }
-            }
-    };
-
-    private static final SSLContext trustAllSslContext;
-    static {
-        try {
-            trustAllSslContext = SSLContext.getInstance("SSL");
-            trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
 
     public static HTTPClientHandler instance = null;
 
