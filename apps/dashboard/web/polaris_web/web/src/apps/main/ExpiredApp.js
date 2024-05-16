f948062cd1d4797a57fcd8245ca32c48174f294a
notshivansh <shivansh@akto.io>
2024-05-16 11:45:42 +0530

    update message

--- apps/dashboard/web/polaris_web/web/src/apps/main/ExpiredApp.js
+++ apps/dashboard/web/polaris_web/web/src/apps/main/ExpiredApp.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 
 function ExpiredApp() {
-    const message = <Text>Please upgrade to the latest version to continue using Akto, <Link url="https://docs.akto.io/support">learn more</Link>. For any queries contact <Link url="mailto:support@akto.io">support@akto.io</Link>.</Text>
+    const message = <Text>To continue using Akto, please reach out to <Link url="mailto:support@akto.io">support@akto.io</Link>.</Text>
     const router = createBrowserRouter([
         {
             path: "*",
@@ -15,7 +15,7 @@ function ExpiredApp() {
             }}>
                 <EmptyScreensLayout key={"emptyScreen"}
                     iconSrc={"/public/upgrade.svg"}
-                    headingText={"Hey! we noticed you're on an unsupported version"}
+                    headingText={"Hey! Your free trial has expired"}
                     description={message}
                 />
             </div>
