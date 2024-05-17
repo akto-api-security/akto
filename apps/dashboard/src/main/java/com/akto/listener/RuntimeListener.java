a60c5695aec8ba999b21f50a6effd38c063dbb5d
harsh-akto <167057320+harsh-akto@users.noreply.github.com>
2024-05-17 22:57:40 +0530

    update urlList size

--- apps/dashboard/src/main/java/com/akto/listener/RuntimeListener.java
+++ apps/dashboard/src/main/java/com/akto/listener/RuntimeListener.java
@@ -230,7 +230,7 @@ public class RuntimeListener extends AfterMongoConnectListener {
             for (SingleTypeInfo singleTypeInfo: params) {
                 urlList.add(singleTypeInfo.getUrl());
             }
-            if (urlList.size() != 202) {
+            if (urlList.size() != 207) {
                 Utils.pushDataToKafka(VULNERABLE_API_COLLECTION_ID, "", result, new ArrayList<>(), true);
             }
 
