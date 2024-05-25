0edcc09e5c112d5d14ba55d8afd8ffdadfbd7ace
Oren Saldanha <oren@akto.io>
2024-05-25 15:09:05 +0530

    write in batches

--- apps/dashboard/src/main/java/com/akto/listener/InitializerListener.java
+++ apps/dashboard/src/main/java/com/akto/listener/InitializerListener.java
@@ -2589,6 +2589,12 @@ public class InitializerListener implements ServletContextListener {
                         }
                     }
                 }, "automated-api-groups-scheduler");
+
+                 // Wait for 500ms before processing groups for next account
+                 try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
             }
         }, 0, 4, TimeUnit.HOURS);
     }
