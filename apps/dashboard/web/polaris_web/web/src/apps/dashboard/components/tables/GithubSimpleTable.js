f766c820a42b86e43e2fdeefd390b96ce9586d09
Oren Saldanha <oren@akto.io>
2024-06-03 09:57:04 +0530

    add akto findings table, toc and issues table

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
@@ -5,8 +5,10 @@ import tableFunc from "./transform";
 function GithubSimpleTable(props) {
 
     const [filters, setFilters] = useState([])
+
     return <GithubServerTable
-        key={JSON.stringify(props.data ? props.data : "{}")} // passing any value as a "key" re-renders the component when the value is changed.
+        key="hst"
+        //key={JSON.stringify(props.data ? props.data : "{}")} // passing any value as a "key" re-renders the component when the value is changed.
         pageLimit={props.pageLimit}
         fetchData={(sortKey, sortOrder, skip, limit, filters, filterOperators, queryValue) => tableFunc.fetchDataSync(sortKey, sortOrder, skip, limit, filters, filterOperators, queryValue, setFilters, props)}
         sortOptions={props.sortOptions} 
