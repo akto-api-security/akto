6be84e282652b41955af298ea7d357d1a2b2aedc
Oren Saldanha <oren@akto.io>
2024-06-18 11:44:51 +0530

    fix table

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/tables/GithubSimpleTable.js
@@ -7,8 +7,7 @@ function GithubSimpleTable(props) {
     const [filters, setFilters] = useState([])
 
     return <GithubServerTable
-        key="hst"
-        //key={JSON.stringify(props.data ? props.data : "{}")} // passing any value as a "key" re-renders the component when the value is changed.
+        key={JSON.stringify(props.data ? props.data : "{}")} // passing any value as a "key" re-renders the component when the value is changed.
         pageLimit={props.pageLimit}
         fetchData={(sortKey, sortOrder, skip, limit, filters, filterOperators, queryValue) => tableFunc.fetchDataSync(sortKey, sortOrder, skip, limit, filters, filterOperators, queryValue, setFilters, props)}
         sortOptions={props.sortOptions} 
