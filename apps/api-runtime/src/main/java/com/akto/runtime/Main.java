01e0e3c3cc1234128cc6955f726f2bb0f7b7958d
notshivansh <shivansh@akto.io>
2024-06-25 15:40:04 +0530

    create indices only once

--- apps/api-runtime/src/main/java/com/akto/runtime/Main.java
+++ apps/api-runtime/src/main/java/com/akto/runtime/Main.java
@@ -450,7 +450,6 @@ public class Main {
 
     public static void initializeRuntimeHelper() {
         SingleTypeInfoDao.instance.getMCollection().updateMany(Filters.exists("apiCollectionId", false), Updates.set("apiCollectionId", 0));
-        DaoInit.createIndices();
         insertRuntimeFilters();
         try {
             AccountSettingsDao.instance.updateVersion(AccountSettings.API_RUNTIME_VERSION);
