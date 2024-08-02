c1ce46f59ff3abfac9ae86f05c9b4cac1e5d2158
Avneesh Hota <avneesh@akto.io>
2024-08-02 12:23:36 +0530

    fixed getter for runDependencyAnalyser

--- libs/dao/src/main/java/com/akto/dto/ApiCollection.java
+++ libs/dao/src/main/java/com/akto/dto/ApiCollection.java
@@ -304,6 +304,10 @@ public class ApiCollection {
         return runDependencyAnalyser;
     }
 
+    public boolean getRunDependencyAnalyser() {
+        return runDependencyAnalyser;
+    }
+
     public void setRunDependencyAnalyser(boolean runDependencyAnalyser) {
         this.runDependencyAnalyser = runDependencyAnalyser;
     }
