d9cb6eb507fe50399816beda04986dc1ea780ebc
shivamrawat101192 <shivam@akto.io>
2024-08-17 09:29:31 +0530

    API inventory query action

---
+++ apps/dashboard/src/main/java/com/akto/action/observe/ApiInventoryQueryAction.java
@@ -0,0 +1,10 @@
+package com.akto.action.observe;
+
+import com.akto.action.UserAction;
+
+public class ApiInventoryQueryAction extends UserAction {
+    public enum QueryType {
+        ENDPOINT,
+
+    }
+}
