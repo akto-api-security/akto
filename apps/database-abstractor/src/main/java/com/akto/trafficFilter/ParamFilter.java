691d7dffeee614675f8a4234561e90769b9799dd
notshivansh <shivansh@akto.io>
2024-08-18 20:53:57 +0530

    add a separate filter for host

--- apps/database-abstractor/src/main/java/com/akto/trafficFilter/ParamFilter.java
+++ apps/database-abstractor/src/main/java/com/akto/trafficFilter/ParamFilter.java
@@ -1,7 +1,9 @@
 package com.akto.trafficFilter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.akto.dao.context.Context;
 import com.akto.log.LoggerMaker;
@@ -14,15 +16,18 @@ public class ParamFilter {
     private static final LoggerMaker loggerMaker = new LoggerMaker(ParamFilter.class, LogDb.DB_ABS);
 
     private static List<BloomFilter<CharSequence>> filterList = new ArrayList<>();
+    private static BloomFilter<CharSequence> hostFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), 1_000_000,0.001);
     private static int currentFilterIndex = -1;
     private static int filterFillStart = 0;
     private static final int TIME_LIMIT = 5 * 60;
     private static final int FILTER_LIMIT = 5;
     private static final String DOLLAR = "$";
+    private static final String HOST = "host";
 
     private static void insertInFilter(String key) {
         filterList.get(currentFilterIndex).put(key);
     }
+    private static Map<Integer, Integer> filterListHitCount = new HashMap<>();
 
     private static void refreshFilterList() {
         int now = Context.now();
@@ -33,11 +38,19 @@ public class ParamFilter {
             currentFilterIndex %= FILTER_LIMIT;
             BloomFilter<CharSequence> newFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), 1_000_000,
                     0.001);
+            
+            String hitCountLog = "";
+            for (int i = 0; i < FILTER_LIMIT; i++) {
+                hitCountLog += filterListHitCount.getOrDefault(i, 0) + " ";
+            }
+            loggerMaker.infoAndAddToDb(String.format("ParamFilter hitCounts: %s",hitCountLog));
+
             if (currentFilterIndex < filterList.size()) {
                 filterList.set(currentFilterIndex, newFilter);
             } else {
                 filterList.add(newFilter);
             }
+            filterListHitCount.put(currentFilterIndex, 0);
         }
     }
 
@@ -50,40 +63,67 @@ public class ParamFilter {
     private static int firstPrintTime = 0;
     private static final int PRINT_INTERVAL = 60;
     private static final int DEBUG_COUNT = 50;
+    private static int hostHits = 0;
+    private static int hostMisses = 0;
 
-    private static void printL(Object o, boolean isHit) {
+    private static void printL(Object o, boolean print) {
         int now = Context.now();
+        if (print) {
+            loggerMaker.infoAndAddToDb(o.toString());
+        }
         if ((firstPrintTime + PRINT_INTERVAL) < now) {
-            loggerMaker.infoAndAddToDb(String.format("ParamFilter hits: %d , misses: %d , firstPrintTime: %d , now : %d",hits, misses, firstPrintTime, now));
+            loggerMaker.infoAndAddToDb(String.format("ParamFilter hits: %d , misses: %d , hostHits %d, hostMisses %d, firstPrintTime: %d , now : %d",hits, misses, hostHits, hostMisses, firstPrintTime, now));
             firstPrintTime = now;
             hits = 0;
             misses = 0;
-        }
-        if (isHit) {
-            hits++;
-        } else {
-            misses++;
-        }
-        if ((isHit && hits < DEBUG_COUNT) || (!isHit && misses < DEBUG_COUNT)) {
-            loggerMaker.infoAndAddToDb(o.toString());
+            hostHits = 0;
+            hostMisses = 0;
         }
     }
 
     public static boolean isNewEntry(int accountId, int apiCollectionId, String url, String method, String param) {
         String key = createKey(accountId, apiCollectionId, url, method, param);
 
-        boolean isNew = true;
-        refreshFilterList();
-        for (BloomFilter<CharSequence> filter : filterList) {
-            isNew &= (!filter.mightContain(key));
-        }
-        if (isNew) {
-            printL("ParamFilter inserting: " + key, false);
-            insertInFilter(key);
+        if (HOST.equals(param.toLowerCase())) {
+            if (!hostFilter.mightContain(key)) {
+                hostMisses++;
+                printL("ParamFilter inserting host: " + key, hostMisses < DEBUG_COUNT);
+                // return true;
+            } else {
+                hostHits++;
+                printL("ParamFilter skipping host: " + key, hostHits < DEBUG_COUNT);
+                // return false;
+            }
+            hostFilter.put(key);
+            // to make it no-op.
             return true;
         }
-        printL("ParamFilter skipping: " + key, true);
-        return false;
+
+        boolean isNew = true;
+        refreshFilterList();
+        int i=0;
+        for (BloomFilter<CharSequence> filter : filterList) {
+            boolean notFound = (!filter.mightContain(key));
+            isNew &= notFound;
+            if(!notFound){
+                int temp = filterListHitCount.getOrDefault(i, 0)+1;
+                filterListHitCount.put(i, temp);
+            }
+            i++;
+        }
+        if (isNew) {
+            misses++;
+            printL("ParamFilter inserting: " + key, misses < DEBUG_COUNT);
+            insertInFilter(key);
+            // return true;
+        }else {
+            hits++;
+            printL("ParamFilter skipping: " + key, hits < DEBUG_COUNT);
+        }
+        // return false;
+
+        // to make it no-op.
+        return true;
     }
 
 }
