b0c1b653c018e275f71185515f7cb09bb2960207
Avneesh Hota <avneesh@akto.io>
2024-08-31 16:56:31 +0530

    use capped collections

--- libs/dao/src/main/java/com/akto/dao/traffic_collector/TrafficCollectorInfoDao.java
+++ libs/dao/src/main/java/com/akto/dao/traffic_collector/TrafficCollectorInfoDao.java
@@ -3,6 +3,9 @@ package com.akto.dao.traffic_collector;
 import com.akto.dao.AccountsContextDao;
 import com.akto.dao.context.Context;
 import com.akto.dto.traffic_collector.TrafficCollectorInfo;
+import com.akto.util.DbMode;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 
@@ -20,6 +23,29 @@ public class TrafficCollectorInfoDao extends AccountsContextDao<TrafficCollector
         return TrafficCollectorInfo.class;
     }
 
+    public static final int maxDocuments = 10_000;
+    public static final int sizeInBytes = 10_000_000;
+
+    public void createIndicesIfAbsent() {
+        boolean exists = false;
+        String dbName = Context.accountId.get()+"";
+        MongoDatabase db = clients[0].getDatabase(dbName);
+        for (String col: db.listCollectionNames()){
+            if (getCollName().equalsIgnoreCase(col)){
+                exists = true;
+                break;
+            }
+        };
+
+        if (!exists) {
+            if (DbMode.allowCappedCollections()) {
+                db.createCollection(getCollName(), new CreateCollectionOptions().capped(true).maxDocuments(maxDocuments).sizeInBytes(sizeInBytes));
+            } else {
+                db.createCollection(getCollName());
+            }
+        }
+    }
+
 
     public void updateHeartbeat(String id, String runtimeId) {
         instance.updateOne(
@@ -27,7 +53,7 @@ public class TrafficCollectorInfoDao extends AccountsContextDao<TrafficCollector
                 Updates.combine(
                         Updates.set(TrafficCollectorInfo.LAST_HEARTBEAT, Context.now()),
                         Updates.setOnInsert(TrafficCollectorInfo.START_TIME, Context.now()),
-                        Updates.setOnInsert(TrafficCollectorInfo.RUNTIME_ID, Context.now())
+                        Updates.setOnInsert(TrafficCollectorInfo.RUNTIME_ID, runtimeId)
                 )
         );
     }
--- libs/dao/src/main/java/com/akto/dao/traffic_collector/TrafficCollectorMetricsDao.java
+++ libs/dao/src/main/java/com/akto/dao/traffic_collector/TrafficCollectorMetricsDao.java
@@ -1,8 +1,12 @@
 package com.akto.dao.traffic_collector;
 
 import com.akto.dao.AccountsContextDao;
+import com.akto.dao.context.Context;
 import com.akto.dto.traffic_collector.TrafficCollectorInfo;
 import com.akto.dto.traffic_collector.TrafficCollectorMetrics;
+import com.akto.util.DbMode;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import org.apache.commons.collections.ArrayStack;
@@ -24,6 +28,29 @@ public class TrafficCollectorMetricsDao extends AccountsContextDao<TrafficCollec
     @Override
     public Class<TrafficCollectorMetrics> getClassT() {
         return TrafficCollectorMetrics.class;
+    }
+
+    public static final int maxDocuments = 10_000;
+    public static final int sizeInBytes = 10_000_000;
+
+    public void createIndicesIfAbsent() {
+        boolean exists = false;
+        String dbName = Context.accountId.get()+"";
+        MongoDatabase db = clients[0].getDatabase(dbName);
+        for (String col: db.listCollectionNames()){
+            if (getCollName().equalsIgnoreCase(col)){
+                exists = true;
+                break;
+            }
+        };
+
+        if (!exists) {
+            if (DbMode.allowCappedCollections()) {
+                db.createCollection(getCollName(), new CreateCollectionOptions().capped(true).maxDocuments(maxDocuments).sizeInBytes(sizeInBytes));
+            } else {
+                db.createCollection(getCollName());
+            }
+        }
     }
 
     public void updateCount(TrafficCollectorMetrics trafficCollectorMetrics) {
