f937f2ea1dbb4105c132b6c457dcc2ae5046e2f8
SHIVAM RAWAT <shivam@akto.io>
2024-09-05 11:40:55 +0530

    update return statement

--- apps/source-code-analyser/src/main/java/com/akto/Main.java
+++ apps/source-code-analyser/src/main/java/com/akto/Main.java
@@ -38,59 +38,60 @@ public class Main {
     }
 
     private static Map<String, String> fetchAllProjectKeys() {
-
+        return null;
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         while (true) {
             List<CodeAnalysisRepo> repos = fetchReposToSync();
+            String repoUrl = "http://localhost:7990/rest/api/1.0/projects/SPRIN/repos/spring-boot-tutorial-master/archive"; // Replace with your repo URL
+            String outputFilePath = "repository.zip"; // The local file where the repository will be saved
+            File file = new File(outputFilePath);
+
+            Request.Builder builder = new Request.Builder();
+            builder.url(repoUrl);
+            builder.get();
+            builder.addHeader("Authorization", "Bearer " + BITBUCKET_TOKEN);
+            HTTPClientHandler.initHttpClientHandler(false);
+            OkHttpClient okHttpClient = HTTPClientHandler.instance.getHTTPClient(false, JSON_CONTENT_TYPE);
+            Request request = builder.build();
+            try {
+                Response response = okHttpClient.newCall(request).execute();
+                if (response.isSuccessful() && response.body() != null) {
+                    // Get input stream from the response body
+                    InputStream inputStream = Objects.requireNonNull(response.body()).byteStream();
+
+                    // Write the input stream to a file
+                    try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            fileOutputStream.write(buffer, 0, bytesRead);
+                        }
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                        System.out.println("Repository downloaded successfully.");
+                        Request.Builder codeAnalyserRequest = new Request.Builder();
+                        codeAnalyserRequest.url(SOURCE_CODE_HOST + SOURCE_CODE_URL);
+                        codeAnalyserRequest.method("POST", RequestBody.create("{\"path\":\"" + file.getAbsolutePath() + "\"}", MediaType.parse(JSON_CONTENT_TYPE)));
+                        response = okHttpClient.newCall(codeAnalyserRequest.build()).execute();
+                        if (response.isSuccessful() && response.body() != null) {
+                            ResponseBody responseBody = response.peekBody(1024*1024);
+                            System.out.println(responseBody.string());
+
+                        }
+
+                    }
+                } else {
+                    System.out.println("Failed to download repository. Response code: " + response.code());
+                }
+            } catch (Exception e) {
+                System.out.println("failed to call" + e.getMessage());
+            }
+
             Thread.sleep(1000);
         }
 
-        String repoUrl = "http://localhost:7990/rest/api/1.0/projects/SPRIN/repos/spring-boot-tutorial-master/archive"; // Replace with your repo URL
-        String outputFilePath = "repository.zip"; // The local file where the repository will be saved
-        File file = new File(outputFilePath);
-
-        Request.Builder builder = new Request.Builder();
-        builder.url(repoUrl);
-        builder.get();
-        builder.addHeader("Authorization", "Bearer " + BITBUCKET_TOKEN);
-        HTTPClientHandler.initHttpClientHandler(false);
-        OkHttpClient okHttpClient = HTTPClientHandler.instance.getHTTPClient(false, JSON_CONTENT_TYPE);
-        Request request = builder.build();
-        try {
-            Response response = okHttpClient.newCall(request).execute();
-            if (response.isSuccessful() && response.body() != null) {
-                // Get input stream from the response body
-                InputStream inputStream = Objects.requireNonNull(response.body()).byteStream();
-
-                // Write the input stream to a file
-                try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, bytesRead);
-                    }
-                    fileOutputStream.flush();
-                    fileOutputStream.close();
-                    System.out.println("Repository downloaded successfully.");
-                    Request.Builder codeAnalyserRequest = new Request.Builder();
-                    codeAnalyserRequest.url(SOURCE_CODE_HOST + SOURCE_CODE_URL);
-                    codeAnalyserRequest.method("POST", RequestBody.create("{\"path\":\"" + file.getAbsolutePath() + "\"}", MediaType.parse(JSON_CONTENT_TYPE)));
-                    response = okHttpClient.newCall(codeAnalyserRequest.build()).execute();
-                    if (response.isSuccessful() && response.body() != null) {
-                        ResponseBody responseBody = response.peekBody(1024*1024);
-                        System.out.println(responseBody.string());
-
-                    }
-
-                }
-            } else {
-                System.out.println("Failed to download repository. Response code: " + response.code());
-            }
-        } catch (Exception e) {
-            System.out.println("failed to call" + e.getMessage());
-        }
     }
 }
