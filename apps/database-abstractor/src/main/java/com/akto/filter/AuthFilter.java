49dc00a27573075c7f1293781da7836819039a68
SHIVAM RAWAT <shivam@akto.io>
2024-09-06 13:39:48 +0530

    reverting authfilter local changes

--- apps/database-abstractor/src/main/java/com/akto/filter/AuthFilter.java
+++ apps/database-abstractor/src/main/java/com/akto/filter/AuthFilter.java
@@ -25,14 +25,13 @@ public class AuthFilter implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
         String accessTokenFromRequest = httpServletRequest.getHeader("authorization");
 
-        Context.accountId.set(1_000_000);
-//        try {
-//            Jws<Claims> claims = JwtAuthenticator.authenticate(accessTokenFromRequest);
-//            Context.accountId.set((int) claims.getBody().get("accountId"));
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            httpServletResponse.sendError(401);
-//        }
+        try {
+            Jws<Claims> claims = JwtAuthenticator.authenticate(accessTokenFromRequest);
+            Context.accountId.set((int) claims.getBody().get("accountId"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            httpServletResponse.sendError(401);
+        }
         chain.doFilter(servletRequest, servletResponse);
 
     }
