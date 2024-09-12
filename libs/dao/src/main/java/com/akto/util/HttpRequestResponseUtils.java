5ba4332523ddfc17a3b07001f2a4f00a72f86422
notshivansh <shivansh@akto.io>
2024-09-12 11:24:32 +0530

    add comment

--- libs/dao/src/main/java/com/akto/util/HttpRequestResponseUtils.java
+++ libs/dao/src/main/java/com/akto/util/HttpRequestResponseUtils.java
@@ -143,6 +143,12 @@ public class HttpRequestResponseUtils {
         return formUrlEncoded.toString();
     }
     public static String encode(String s) throws UnsupportedEncodingException {
+
+        /*
+         * No need to reverse the encoding for application/x-www-form-urlencoded  
+         * Ref: https://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.1
+         */
+
         return URLEncoder.encode(s, java.nio.charset.StandardCharsets.UTF_8.name());
                 // .replaceAll("\\+", "%20")
                 // .replaceAll("\\%21", "!")
