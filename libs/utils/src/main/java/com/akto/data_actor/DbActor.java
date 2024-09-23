25c36d37870a40cb867ab7dcac0a83f88032335c
ayushaga14 <ayush@akto.io>
2024-09-23 11:51:15 +0530

    add apis for stop tests

--- libs/utils/src/main/java/com/akto/data_actor/DbActor.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbActor.java
@@ -1,5 +1,7 @@
 package com.akto.data_actor;
 
+import com.akto.dao.context.Context;
+import com.akto.dao.testing.TestingRunResultSummariesDao;
 import com.akto.dto.*;
 import com.akto.dto.ApiInfo.ApiInfoKey;
 import com.akto.dto.billing.Organization;
@@ -15,6 +17,7 @@ import com.akto.dto.testing.AccessMatrixUrlToRole;
 import com.akto.dto.testing.EndpointLogicalGroup;
 import com.akto.dto.testing.TestRoles;
 import com.akto.dto.testing.TestingRun;
+import com.akto.dto.testing.TestingRun.State;
 import com.akto.dto.testing.TestingRunConfig;
 import com.akto.dto.testing.TestingRunResult;
 import com.akto.dto.testing.TestingRunResultSummary;
@@ -407,6 +410,10 @@ public class DbActor extends DataActor {
     public TestingRunResultSummary updateIssueCountInSummary(String summaryId,
             Map<String, Integer> totalCountIssues) {
         return DbLayer.updateIssueCountInSummary(summaryId, totalCountIssues);
+    }
+
+    public TestingRunResultSummary updateIssueCountAndStateInSummary(String summaryId, Map<String, Integer> totalCountIssues, String state) {
+        return DbLayer.updateIssueCountAndStateInSummary(summaryId, totalCountIssues, state);
     }
 
     public List<Integer> fetchDeactivatedCollections() {
