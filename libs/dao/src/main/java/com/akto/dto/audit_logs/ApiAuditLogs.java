bac5f491481662f9107a0b2881bff5bd7245f430
TangoBeeAkto <umesh@akto.io>
2024-10-11 13:57:43 +0530

    feat: created dao and dto for api audit logs

---
+++ libs/dao/src/main/java/com/akto/dto/audit_logs/ApiAuditLogs.java
@@ -0,0 +1,81 @@
+package com.akto.dto.audit_logs;
+
+public class ApiAuditLogs {
+
+    public static final String TIMESTAMP = "timestamp";
+    private long timestamp;
+
+    public static final String API_ENDPOINT = "apiEndpoint";
+    private String apiEndpoint;
+
+    public static final String ACTION_DESCRIPTION = "actionDescription";
+    private String actionDescription;
+
+    public static final String USER_EMAIL = "userEmail";
+    private String userEmail;
+
+    public static final String USER_AGENT = "userAgent";
+    private String userAgent;
+
+    public static final String USER_IP_ADDRESS = "userIpAddress";
+    private String userIpAddress;
+
+    public ApiAuditLogs() {}
+
+    public ApiAuditLogs(long timestamp, String apiEndpoint, String actionDescription, String userEmail, String userAgent, String userIpAddress) {
+        this.timestamp = timestamp;
+        this.apiEndpoint = apiEndpoint;
+        this.actionDescription = actionDescription;
+        this.userEmail = userEmail;
+        this.userAgent = userAgent;
+        this.userIpAddress = userIpAddress;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getApiEndpoint() {
+        return apiEndpoint;
+    }
+
+    public void setApiEndpoint(String apiEndpoint) {
+        this.apiEndpoint = apiEndpoint;
+    }
+
+    public String getActionDescription() {
+        return actionDescription;
+    }
+
+    public void setActionDescription(String actionDescription) {
+        this.actionDescription = actionDescription;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public String getUserIpAddress() {
+        return userIpAddress;
+    }
+
+    public void setUserIpAddress(String userIpAddress) {
+        this.userIpAddress = userIpAddress;
+    }
+}
