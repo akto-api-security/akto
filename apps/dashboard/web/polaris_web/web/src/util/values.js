3572f335be2aba98db4c307a1b3802d23ad49e4d
TangoBeeAkto <umesh@akto.io>
2024-10-15 00:31:46 +0530

    feat: changing summary graphs allTime value and critical findings stacked graph to bar graph

--- apps/dashboard/web/polaris_web/web/src/util/values.js
+++ apps/dashboard/web/polaris_web/web/src/util/values.js
@@ -73,7 +73,7 @@ const ranges = [
         title: 'All time',
         alias: "allTime",
         period:{
-            since: new Date(0),
+            since: window.ACTIVE_ACCOUNT ? new Date(window.ACTIVE_ACCOUNT * 1000) : new Date(0),
             until: todayDayEnd,
         }
     }
