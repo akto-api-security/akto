de6aea3654f200cba36b99c9e674d7d25b375fb6
Ajinkya <ajinkya@akto.io>
2024-11-22 17:32:09 +0530

    added authorization interceptor for grpc server

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
+++ apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
@@ -7,19 +7,22 @@ import com.mongodb.ReadPreference;
 import com.mongodb.client.MongoClient;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String mongoURI = System.getenv("AKTO_MONGO_CONN");;
-        DaoInit.init(new ConnectionString(mongoURI));
+  public static void main(String[] args) throws Exception {
+    String mongoURI = System.getenv("AKTO_MONGO_CONN");
 
-        MongoClient threatProtectionMongo = DaoInit.createMongoClient(
-                new ConnectionString(System.getenv("AKTO_THREAT_PROTECTION_MONGO")),
-                ReadPreference.secondary());
+    DaoInit.init(new ConnectionString(mongoURI));
 
-        new CleanupTask(threatProtectionMongo).init();
+    MongoClient threatProtectionMongo =
+        DaoInit.createMongoClient(
+            new ConnectionString(System.getenv("AKTO_THREAT_PROTECTION_MONGO_CONN")),
+            ReadPreference.secondary());
 
-        int port = Integer.parseInt(System.getenv().getOrDefault("AKTO_THREAT_PROTECTION_BACKEND_PORT", "8980"));
-        BackendServer server = new BackendServer(port, threatProtectionMongo);
-        server.start();
-        server.blockUntilShutdown();
-    }
+    new CleanupTask(threatProtectionMongo).init();
+    int port =
+        Integer.parseInt(
+            System.getenv().getOrDefault("AKTO_THREAT_PROTECTION_BACKEND_PORT", "8980"));
+    BackendServer server = new BackendServer(port, threatProtectionMongo);
+    server.start();
+    server.blockUntilShutdown();
+  }
 }
