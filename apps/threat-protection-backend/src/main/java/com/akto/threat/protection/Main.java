330b1050388d895ec0ba47258656a5de74c813d6
Ajinkya <ajinkya@akto.io>
2024-11-22 14:49:16 +0530

    moved clean up task to threat protection service

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
+++ apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
@@ -1,15 +1,24 @@
 package com.akto.threat.protection;
 
 import com.akto.DaoInit;
+import com.akto.threat.protection.tasks.CleanupTask;
 import com.mongodb.ConnectionString;
+import com.mongodb.ReadPreference;
+import com.mongodb.client.MongoClient;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String mongoURI = System.getenv("AKTO_MONGO_CONN");;
         DaoInit.init(new ConnectionString(mongoURI));
 
+        MongoClient threatProtectionMongo = DaoInit.createMongoClient(
+                new ConnectionString(System.getenv("AKTO_THREAT_PROTECTION_MONGO")),
+                ReadPreference.secondary());
+
+        new CleanupTask(threatProtectionMongo).init();
+
         int port = Integer.parseInt(System.getenv().getOrDefault("AKTO_THREAT_PROTECTION_BACKEND_PORT", "8980"));
-        BackendServer server = new BackendServer(port);
+        BackendServer server = new BackendServer(port, threatProtectionMongo);
         server.start();
         server.blockUntilShutdown();
     }
