5235da8681b1a654b27e5ad4be6262f288b6d6d2
Ajinkya <ajinkya@akto.io>
2024-11-22 12:04:08 +0530

    removed db service and using mongo client everywhere

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
+++ apps/threat-protection-backend/src/main/java/com/akto/threat/protection/Main.java
@@ -5,7 +5,7 @@ import com.mongodb.ConnectionString;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String mongoURI = System.getenv("AKTO_THREAT_DETECTION_MONGO_CONN");
+        String mongoURI = System.getenv("AKTO_MONGO_CONN");;
         DaoInit.init(new ConnectionString(mongoURI));
 
         int port = Integer.parseInt(System.getenv().getOrDefault("AKTO_THREAT_PROTECTION_BACKEND_PORT", "8980"));
