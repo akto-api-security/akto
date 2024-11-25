ee7a11a595370adf33ff80010b58cccd1a51eaed
Ajinkya <ajinkya@akto.io>
2024-11-25 13:18:21 +0530

    added new api threat detection
    removed old api-threat-detection-module

---
+++ apps/threat-detection/src/main/java/com/akto/threat/detection/tasks/FlushSampleDataTask.java
@@ -0,0 +1,10 @@
+package com.akto.threat.detection.tasks;
+
+/*
+This will read sample malicious data from kafka topic and save it to DB.
+ */
+public class FlushSampleDataTask implements Task {
+
+  @Override
+  public void run() {}
+}
