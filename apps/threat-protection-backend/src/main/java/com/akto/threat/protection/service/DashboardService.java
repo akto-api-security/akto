b92a6ffb7cf225301773e0de5025eb24715a494b
Ajinkya <ajinkya@akto.io>
2024-12-06 12:46:47 +0530

    using grpc API instead of mongo calls

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/service/DashboardService.java
+++ apps/threat-protection-backend/src/main/java/com/akto/threat/protection/service/DashboardService.java
@@ -1,24 +1,63 @@
 package com.akto.threat.protection.service;
 
 import com.akto.proto.threat_protection.service.dashboard_service.v1.DashboardServiceGrpc.DashboardServiceImplBase;
+import com.akto.proto.threat_protection.service.dashboard_service.v1.FetchAlertFiltersRequest;
+import com.akto.proto.threat_protection.service.dashboard_service.v1.FetchAlertFiltersResponse;
 import com.akto.proto.threat_protection.service.dashboard_service.v1.ListMaliciousRequestsRequest;
 import com.akto.proto.threat_protection.service.dashboard_service.v1.ListMaliciousRequestsResponse;
 import com.akto.proto.threat_protection.service.dashboard_service.v1.MaliciousRequest;
 import com.akto.threat.protection.db.MaliciousEventModel;
 import com.akto.threat.protection.interceptors.Constants;
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.DistinctIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
 import io.grpc.stub.StreamObserver;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.bson.conversions.Bson;
 
 public class DashboardService extends DashboardServiceImplBase {
   private final MongoClient mongoClient;
 
   public DashboardService(MongoClient mongoClient) {
     this.mongoClient = mongoClient;
+  }
+
+  private static <T> Set<T> findDistinctFields(
+      MongoCollection<MaliciousEventModel> coll, String fieldName, Class<T> tClass, Bson filters) {
+    DistinctIterable<T> r = coll.distinct(fieldName, filters, tClass);
+    Set<T> result = new HashSet<>();
+    MongoCursor<T> cursor = r.cursor();
+    while (cursor.hasNext()) {
+      result.add(cursor.next());
+    }
+    return result;
+  }
+
+  @Override
+  public void fetchAlertFilters(
+      FetchAlertFiltersRequest request,
+      StreamObserver<FetchAlertFiltersResponse> responseObserver) {
+    int accountId = Constants.ACCOUNT_ID_CONTEXT_KEY.get();
+    MongoCollection<MaliciousEventModel> coll =
+        this.mongoClient
+            .getDatabase(accountId + "")
+            .getCollection("malicious_events", MaliciousEventModel.class);
+
+    Set<String> actors =
+        DashboardService.<String>findDistinctFields(coll, "actor", String.class, Filters.empty());
+    Set<String> urls =
+        DashboardService.<String>findDistinctFields(coll, "url", String.class, Filters.empty());
+
+    FetchAlertFiltersResponse response =
+        FetchAlertFiltersResponse.newBuilder().addAllActors(actors).addAllUrls(urls).build();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
   }
 
   @Override
