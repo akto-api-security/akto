09bf062bc1aa6efef34c634a090a4bb2b700d809
Ajinkya <109141486+ag060@users.noreply.github.com>
2024-12-10 12:25:02 +0530

    added dashboard rpc service (#1808)

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/service/MaliciousAlertService.java
+++ apps/threat-protection-backend/src/main/java/com/akto/threat/protection/service/MaliciousAlertService.java
@@ -1,9 +1,9 @@
-package com.akto.threat.protection;
+package com.akto.threat.protection.service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.akto.proto.threat_protection.message.malicious_event.v1.MaliciousEvent;
+import com.akto.dto.type.URLMethods;
 import com.akto.proto.threat_protection.service.malicious_alert_service.v1.MaliciousAlertServiceGrpc.MaliciousAlertServiceImplBase;
 import com.akto.proto.threat_protection.service.malicious_alert_service.v1.RecordAlertRequest;
 import com.akto.proto.threat_protection.service.malicious_alert_service.v1.RecordAlertResponse;
@@ -12,21 +12,12 @@ import com.akto.threat.protection.db.MaliciousEventModel;
 import com.akto.threat.protection.db.SmartEventModel;
 import com.akto.threat.protection.interceptors.Constants;
 import com.akto.threat.protection.utils.KafkaUtils;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.model.BulkWriteOptions;
-import com.mongodb.client.model.InsertOneModel;
 import com.mongodb.client.model.WriteModel;
-
 import io.grpc.stub.StreamObserver;
-import javassist.tools.rmi.Sample;
 
 public class MaliciousAlertService extends MaliciousAlertServiceImplBase {
 
-  private final MongoClient mongoClient;
-
-  public MaliciousAlertService(MongoClient mongoClient) {
-    this.mongoClient = mongoClient;
-  }
+  public MaliciousAlertService() {}
 
   @Override
   public void recordAlert(
@@ -40,21 +31,25 @@ public class MaliciousAlertService extends MaliciousAlertServiceImplBase {
     List<SampleMaliciousEvent> maliciousEvents = request.getSampleDataList();
     List<MaliciousEventModel> events = new ArrayList<>();
     for (SampleMaliciousEvent maliciousEvent : maliciousEvents) {
-        events.add(new MaliciousEventModel(filterId,
-                actor,
-                maliciousEvent.getIp(),
-                maliciousEvent.getUrl(),
-                maliciousEvent.getMethod(),
-                maliciousEvent.getPayload(),
-                maliciousEvent.getTimestamp()));
+      events.add(
+          MaliciousEventModel.newBuilder()
+              .setActor(actor)
+              .setIp(maliciousEvent.getIp())
+              .setUrl(maliciousEvent.getUrl())
+              .setMethod(URLMethods.Method.fromString(maliciousEvent.getMethod()))
+              .setOrig(maliciousEvent.getPayload())
+              .setRequestTime(maliciousEvent.getTimestamp())
+              .setFilterId(filterId)
+              .build());
     }
 
     KafkaUtils.insertData(events, "maliciousEvents", accountId);
-    KafkaUtils.insertData(new SmartEventModel(filterId, actor, request.getTotalEvents(), request.getDetectedAt()), "smartEvent", accountId);
-
+    KafkaUtils.insertData(
+        new SmartEventModel(filterId, actor, request.getTotalEvents(), request.getDetectedAt()),
+        "smartEvent",
+        accountId);
 
     responseObserver.onNext(RecordAlertResponse.newBuilder().build());
     responseObserver.onCompleted();
   }
-
 }
