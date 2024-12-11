56b9eb36af50f9eaba7e4c4944e1e75c9ff85dbd
Ajinkya <ajinkya@akto.io>
2024-12-11 12:24:11 +0530

    moved kafka config from threat detection to utils

--- apps/threat-detection/src/main/java/com/akto/threat/detection/tasks/FlushSampleDataTask.java
+++ apps/threat-detection/src/main/java/com/akto/threat/detection/tasks/FlushSampleDataTask.java
@@ -1,9 +1,8 @@
 package com.akto.threat.detection.tasks;
 
 import com.akto.dto.type.URLMethods;
-import com.akto.proto.threat_protection.message.malicious_event.v1.MaliciousEvent;
+import com.akto.kafka.KafkaConfig;
 import com.akto.proto.threat_protection.message.sample_request.v1.SampleMaliciousRequest;
-import com.akto.threat.detection.config.kafka.KafkaConfig;
 import com.akto.threat.detection.db.entity.MaliciousEventEntity;
 import com.akto.threat.detection.dto.MessageEnvelope;
 import com.google.protobuf.InvalidProtocolBufferException;
