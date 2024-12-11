d1595cac98c91e1880e475ac4d272e9c791d447a
Ajinkya <ajinkya@akto.io>
2024-12-11 13:50:54 +0530

    moved consumer and producer on same machine for threat protection backend

--- libs/utils/src/main/java/com/akto/kafka/Kafka.java
+++ libs/utils/src/main/java/com/akto/kafka/Kafka.java
@@ -9,65 +9,72 @@ import java.time.Duration;
 import java.util.Properties;
 
 public class Kafka {
-    private static final Logger logger = LoggerFactory.getLogger(Kafka.class);
-    private KafkaProducer<String, String> producer;
-    public boolean producerReady;
+  private static final Logger logger = LoggerFactory.getLogger(Kafka.class);
+  private KafkaProducer<String, String> producer;
+  public boolean producerReady;
 
-    public Kafka(String brokerIP, int lingerMS, int batchSize) {
-        producerReady = false;
-        try {
-            setProducer(brokerIP, lingerMS, batchSize);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  public Kafka(KafkaConfig kafkaConfig) {
+    this(
+        kafkaConfig.getBootstrapServers(),
+        kafkaConfig.getProducerConfig().getLingerMs(),
+        kafkaConfig.getProducerConfig().getBatchSize());
+  }
+
+  public Kafka(String brokerIP, int lingerMS, int batchSize) {
+    producerReady = false;
+    try {
+      setProducer(brokerIP, lingerMS, batchSize);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public void send(String message, String topic) {
-        if (!this.producerReady) return;
+  public void send(String message, String topic) {
+    if (!this.producerReady) return;
 
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
-        producer.send(record, new DemoProducerCallback());
+    ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+    producer.send(record, new DemoProducerCallback());
+  }
+
+  public void close() {
+    this.producerReady = false;
+    producer.close(Duration.ofMillis(0)); // close immediately
+  }
+
+  private void setProducer(String brokerIP, int lingerMS, int batchSize) {
+    if (producer != null) close(); // close existing producer connection
+
+    int requestTimeoutMs = 5000;
+    Properties kafkaProps = new Properties();
+    kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerIP);
+    kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    kafkaProps.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+    kafkaProps.put(ProducerConfig.LINGER_MS_CONFIG, lingerMS);
+    kafkaProps.put(ProducerConfig.RETRIES_CONFIG, 0);
+    kafkaProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);
+    kafkaProps.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, lingerMS + requestTimeoutMs);
+    producer = new KafkaProducer<String, String>(kafkaProps);
+
+    // test if connection successful by sending a test message in a blocking way
+    // calling .get() blocks the thread till we receive a message
+    // if any error then close the connection
+    ProducerRecord<String, String> record = new ProducerRecord<>("akto.misc", "ping");
+    try {
+      producer.send(record).get();
+      producerReady = true;
+    } catch (Exception ignored) {
+      close();
     }
+  }
 
-    public void close() {
-        this.producerReady = false;
-        producer.close(Duration.ofMillis(0)); // close immediately
+  private class DemoProducerCallback implements Callback {
+    @Override
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+      if (e != null) {
+        Kafka.this.close();
+        logger.error("onCompletion error: " + e.getMessage());
+      }
     }
-
-    private void setProducer(String brokerIP, int lingerMS, int batchSize) {
-        if (producer != null) close(); // close existing producer connection
-
-        int requestTimeoutMs = 5000;
-        Properties kafkaProps = new Properties();
-        kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerIP);
-        kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        kafkaProps.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
-        kafkaProps.put(ProducerConfig.LINGER_MS_CONFIG, lingerMS);
-        kafkaProps.put(ProducerConfig.RETRIES_CONFIG, 0);
-        kafkaProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);
-        kafkaProps.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, lingerMS + requestTimeoutMs);
-        producer = new KafkaProducer<String, String>(kafkaProps);
-
-        // test if connection successful by sending a test message in a blocking way
-        // calling .get() blocks the thread till we receive a message
-        // if any error then close the connection
-        ProducerRecord<String, String> record = new ProducerRecord<>("akto.misc", "ping");
-        try {
-            producer.send(record).get();
-            producerReady = true;
-        } catch (Exception ignored) {
-            close();
-        }
-    }
-
-    private class DemoProducerCallback implements Callback {
-        @Override
-        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-            if (e != null) {
-                Kafka.this.close();
-                logger.error("onCompletion error: " + e.getMessage());
-            }
-        }
-    }
+  }
 }
