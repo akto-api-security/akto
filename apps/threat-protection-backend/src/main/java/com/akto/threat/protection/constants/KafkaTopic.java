9b7ec0ec91b2d223cdbd7470840534c631399b9c
Ajinkya <ajinkya@akto.io>
2024-12-17 14:43:23 +0530

    renamed threat detection backend service

--- apps/threat-protection-backend/src/main/java/com/akto/threat/protection/constants/KafkaTopic.java
+++
@@ -1,7 +0,0 @@
-package com.akto.threat.protection.constants;
-
-public class KafkaTopic {
-  public static class ThreatDetection {
-    public static final String INTERNAL_DB_MESSAGES = "akto.threat_detection.internal_db_messages";
-  }
-}
