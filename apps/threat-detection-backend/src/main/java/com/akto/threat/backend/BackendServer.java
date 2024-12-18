ec9a39206373e5eda2fa702d7eb51c485db8aadf
Ajinkya <ajinkya@akto.io>
2024-12-18 13:01:55 +0530

    added health check for rpc and enabled reflection

--- apps/threat-detection-backend/src/main/java/com/akto/threat/backend/BackendServer.java
+++ apps/threat-detection-backend/src/main/java/com/akto/threat/backend/BackendServer.java
@@ -7,6 +7,9 @@ import com.akto.threat.backend.service.MaliciousEventService;
 import com.mongodb.client.MongoClient;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
+import io.grpc.protobuf.services.HealthStatusManager;
+import io.grpc.protobuf.services.ProtoReflectionServiceV1;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -15,13 +18,19 @@ public class BackendServer {
   private final Server server;
 
   public BackendServer(int port, MongoClient mongoClient, KafkaConfig kafkaConfig) {
+    HealthStatusManager health = new HealthStatusManager();
+
     this.port = port;
     this.server =
         ServerBuilder.forPort(port)
+            .addService(ProtoReflectionServiceV1.newInstance())
             .addService(new MaliciousEventService(kafkaConfig))
             .addService(new DashboardService(mongoClient))
+            .addService(health.getHealthService())
             .intercept(new AuthenticationInterceptor())
             .build();
+
+    health.setStatus("", ServingStatus.SERVING);
   }
 
   public void stop() throws InterruptedException {
