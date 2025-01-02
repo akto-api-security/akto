b73c7b06b882fefe4625647502d57773cf421655
notshivansh <shivansh@akto.io>
2025-01-02 14:33:37 +0530

    add missing API

--- libs/utils/src/main/java/com/akto/data_actor/DbActor.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbActor.java
@@ -547,4 +547,8 @@ public class DbActor extends DataActor {
         return DbLayer.fetchTestScript();
     }
 
+    public List<DependencyNode> findDependencyNodes(Bson filter){
+        return DbLayer.findDependencyNodes(filter);
+    }
+
 }
