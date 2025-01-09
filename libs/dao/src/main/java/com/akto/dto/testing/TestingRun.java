c451f1db62c43429e10af76deec2950640d20aa4
Ark2307 <kr.aryan2307@gmail.com>
2025-01-09 11:20:25 +0530

    Adding a field for new testing run

--- libs/dao/src/main/java/com/akto/dto/testing/TestingRun.java
+++ libs/dao/src/main/java/com/akto/dto/testing/TestingRun.java
@@ -28,6 +28,8 @@ public class TestingRun {
     public static final String _API_COLLECTION_ID = "testingEndpoints.apiCollectionId";
     public static final String _API_COLLECTION_ID_IN_LIST = "testingEndpoints.apisList.apiCollectionId";
 
+    private boolean isNewTestRun = true;
+
     @BsonIgnore
     private String hexId;
     @BsonIgnore
@@ -59,6 +61,7 @@ public class TestingRun {
         this.name = name;
         this.triggeredBy = triggeredBy;
         this.sendSlackAlert = sendSlackAlert;
+        this.isNewTestRun = true;
     }
     public TestingRun(int scheduleTimestamp, String userEmail, TestingEndpoints testingEndpoints, int testIdConfig, State state, int periodInSeconds, String name, int testRunTime, int maxConcurrentRequests, boolean sendSlackAlert) {
         this.scheduleTimestamp = scheduleTimestamp;
@@ -73,6 +76,7 @@ public class TestingRun {
         this.periodInSeconds = periodInSeconds;
         this.name = name;
         this.sendSlackAlert = sendSlackAlert;
+        this.isNewTestRun = true;
     }
 
     public TestingRunConfig getTestingRunConfig() {
@@ -222,6 +226,14 @@ public class TestingRun {
             ", periodInSeconds='" + getPeriodInSeconds() + "'" +
             ", name='" + getName() + "'" +
             "}";
+    }
+
+    public boolean isNewTestRun() {
+        return isNewTestRun;
+    }
+
+    public void setNewTestRun(boolean isNewTestRun) {
+        this.isNewTestRun = isNewTestRun;
     }
 
 }
