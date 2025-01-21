0c1f0ad5a8ee8e91f3c62cbdf9a0db6f461e294e
Ark2307 <kr.aryan2307@gmail.com>
2025-01-21 16:48:16 +0530

    Adding more checks to modify payload correctly

--- apps/dashboard/src/test/java/com/akto/utils/TestModifyPayload.java
+++ apps/dashboard/src/test/java/com/akto/utils/TestModifyPayload.java
@@ -4,12 +4,15 @@ package com.akto.utils;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.akto.dto.test_editor.Util;
 
 public class TestModifyPayload {
     
+    // modifying string in root and nested payload
     @Test
     public void testModifyNestedPayload() {
         BasicDBObject root = new BasicDBObject();
@@ -32,5 +35,45 @@ public class TestModifyPayload {
         BasicDBObject addressObj = (BasicDBObject) detailsObj.get("address");
         assertEquals(detailsObj.getString("name"), "hello-world");
         assertEquals(addressObj.getString("name"), "hello-world");  
+    }
+
+    // replace allowed only for same type in object.
+    @Test
+    public void testReplaceWithTypeChange(){
+        BasicDBObject root = new BasicDBObject();
+        root.put("name", new BasicDBObject()
+                .append("first", "Aryan")
+                .append("last", "Gupta"));
+        boolean result = Util.modifyValueInPayload(root, null, "name", "hello-world");
+        assertEquals(result, false);
+    }
+
+    // replace allowed only for same type in list.
+    @Test
+    public void testModifyNestedPayload2() {
+        BasicDBObject root = new BasicDBObject();
+        root.put("details", new BasicDBObject()
+                .append("name", new BasicDBList() {{
+                    add("reading");
+                    add("coding");
+                }}));
+
+        boolean result = Util.modifyValueInPayload(root, null, "name", Arrays.asList("hello-world"));
+        assertEquals(result, true);
+        BasicDBObject detailsObj = (BasicDBObject) root.get("details");
+        assertEquals(detailsObj.get("name"), Arrays.asList("hello-world"));
+    }
+
+    // replace allowed only for same type in list, negative case
+    @Test
+    public void testModifyNestedPayload3() {
+        BasicDBObject root = new BasicDBObject();
+        root.put("details", new BasicDBObject()
+                .append("name", new BasicDBList() {{
+                    add("reading");
+                    add("coding");
+                }}));
+        boolean result = Util.modifyValueInPayload(root, null, "name", "hello-world");
+        assertEquals(result, false);
     }
 }
