afc5483d7595ed5be528e517cd01af4ce811a6f6
amitpanwar789 <amitpanwar02705@gmail.com>
2025-02-21 11:18:38 +0530

    resourceList removed, extra css removed

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/testSuite/FlyLayoutSuite.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/testSuite/FlyLayoutSuite.jsx
@@ -1,9 +1,9 @@
-import { Button, HorizontalStack, Text, VerticalStack, Box, Scrollable, Icon, TextField, ResourceList} from "@shopify/polaris"
-import {
-    CancelMajor, ChevronDownMinor, SearchMinor
+import { Button, HorizontalStack, Text, Box, Icon, TextField } from "@shopify/polaris"
+import { SearchMinor
 } from '@shopify/polaris-icons';
 import { useEffect, useState } from "react";
 import TestSuiteRow from "./TestSuiteRow";
+import FlyLayout from "../../../components/layouts/FlyLayout";
 
 function FlyLayoutSuite(props) {
     const { show, setShow, width, selectedTestSuite} = props;
@@ -29,7 +29,7 @@ function FlyLayoutSuite(props) {
             setTestSuiteName("");
         }
     }, [selectedTestSuite]);
-    
+
 
 
     function handleSearch(val) {
@@ -43,137 +43,115 @@ function FlyLayoutSuite(props) {
             deepCopy = JSON.parse(JSON.stringify(categories));
         }
         let updatedCategories = [...deepCopy];
-    
+
         if (testSearchValue.length > 0) {
-          updatedCategories = updatedCategories.filter(category => {
-            const tests = category.tests.filter(test =>
-              test.label.toLowerCase().includes(testSearchValue.toLowerCase())
-            );
-    
-            if (tests.length > 0) {
-              if (testSearchValue !== prevSearchValue) {
-                category.selected = true;
-              }
-              else {
-                category.selected = category.selected || false;
-              }
-              category.tests = tests;
-              return true;
-            } else {
-              return false;
-            }
-          });
+            updatedCategories = updatedCategories.filter(category => {
+                const tests = category.tests.filter(test =>
+                    test.label.toLowerCase().includes(testSearchValue.toLowerCase())
+                );
+
+                if (tests.length > 0) {
+                    if (testSearchValue !== prevSearchValue) {
+                        category.selected = true;
+                    }
+                    else {
+                        category.selected = category.selected || false;
+                    }
+                    category.tests = tests;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
         }
         setFilteredCategories(updatedCategories);
-        setPrevSearchValue(testSearchValue); 
+        setPrevSearchValue(testSearchValue);
     }, [testSearchValue, categories]);
 
     function checkExpand() {
         return filteredCategories.some(category => !category.selected);
     }
-    
+
     const countSearchResults = () => {
         let count = 0;
         filteredCategories.forEach(category => { count += category.tests.length });
         return count;
     }
 
-    const setSearchVal = (val) => { 
+    const setSearchVal = (val) => {
         handleSearch(val);
     };
- 
+
     const headingComponents = (
-        <HorizontalStack align="space-between">
-            <div style={{ width: "40%" }}>
-                <TextField disabled={true} value={testSuiteName} onChange={(val) => setTestSuiteName(val)} label="Test Suite Name" placeholder="Test_suite_name" />
-            </div>
-            <div style={{ width: "58%", paddingTop: "1.5rem" }}>
-                <TextField value={testSearchValue} onChange={(val) => { setSearchVal(val) }} prefix={<Icon source={SearchMinor} />} placeholder="Search" />
-            </div>
-        </HorizontalStack>
-
+        <Box borderColor="border-subdued" borderBlockStartWidth="1" borderBlockEndWidth="1" background="bg-subdued" padding={4}>
+            <HorizontalStack align="space-between">
+                <div style={{ width: "40%" }}>
+                    <TextField disabled={true} value={testSuiteName} onChange={(val) => setTestSuiteName(val)} label="Test Suite Name" placeholder="Test_suite_name" />
+                </div>
+                <div style={{ width: "58%", paddingTop: "1.5rem" }}>
+                    <TextField value={testSearchValue} onChange={(val) => { setSearchVal(val) }} prefix={<Icon source={SearchMinor} />} placeholder="Search" />
+                </div>
+            </HorizontalStack>
+        </Box>
     )
-
-
-    const divWidth = width || "50vw";
-
-    let id = 1;
-    function renderItem(item) {
-        return (
-            <TestSuiteRow category={item} setCategories={setCategories} id={id++} setFilteredCategories={setFilteredCategories}/>
-        );
-    }
 
     function extendAllHandler() {
         setCategories(prev => {
             return prev.map(category => ({ ...category, selected: true }));
         });
     }
-    
+
     function collapseAllHandler() {
         setCategories(prev => {
             return prev.map(category => ({ ...category, selected: false }));
         });
     }
-    
+
 
     function totalTestsCount() {
         return categories.reduce((count, category) => count + category.tests.length, 0);
     }
-    
+
     function totalSelectedTestsCount() {
         return filteredCategories.reduce((count, category) => count + category.tests.length, 0);
     }
-    
+
+    const listComponents = (
+        <div style={{ margin: "20px", borderRadius: "0.5rem", boxShadow: " 0px 0px 5px 0px #0000000D, 0px 1px 2px 0px #00000026" }}>
+            <Box borderRadius="2" borderColor="border-subdued" >
+                <Box borderColor="border-subdued" paddingBlockEnd={3} paddingBlockStart={3} paddingInlineStart={5} paddingInlineEnd={5}>
+                    <HorizontalStack align="space-between">
+                        <HorizontalStack align="start">
+                            <Text fontWeight="semibold" as="h3">{testSearchValue.length > 0 ? `Showing ${countSearchResults()} result` : `${filteredCategories.length} ${filteredCategories.length > 1 ? 'categories' : 'category'} & ${totalSelectedTestsCount()} tests`}</Text>
+                        </HorizontalStack>
+                        {testSearchValue.trim().length === 0 ? <Button onClick={() => { checkExpand() ? extendAllHandler() : collapseAllHandler() }} plain><Text>{checkExpand() ? "Expand all" : "Collapse all"}</Text></Button> : <></>}
+                    </HorizontalStack>
+                </Box>
+                {filteredCategories.length > 0 && filteredCategories.map((category, index) => {
+                    return (
+                        <TestSuiteRow isLast={index === filteredCategories.length - 1} key={index} category={category} setCategories={setCategories}  setFilteredCategories={setFilteredCategories} />
+                    )
+                })}
+            </Box>
+        </div>
+    );
+
+    const components = [headingComponents,listComponents];
+
 
 
     return (
-        <div className={"flyLayoutSuite " + (show ? "show" : "")} style={{ width: divWidth }}>
-            <div className="innerFlyLayout">
-                <Box borderColor="border-subdued" borderWidth="1" background="bg" width={divWidth} minHeight="100%">
-                    <VerticalStack>
-                        <Box borderColor="border-subdued" borderBlockEndWidth="1" paddingBlockStart={4} paddingBlockEnd={4} paddingInlineStart={5} paddingInlineEnd={5}>
-                            <HorizontalStack align="space-between">
-
-                                <Text variant="headingMd">
-                                    {"Test Suite Details"}
-                                </Text>
-
-                                <Button icon={CancelMajor} onClick={() => { handleExit() }} plain></Button>
-                            </HorizontalStack>
-                        </Box>
-                        <Box paddingBlockEnd={5}>
-                            <Scrollable style={{ height: "90vh" }}>
-
-                                <VerticalStack>
-                                    <Box borderColor="border-subdued" borderBlockEndWidth="1" background="bg-subdued" padding={4}>
-                                        {headingComponents}
-                                    </Box>
-
-                                    <div style={{ margin: "20px", borderRadius: "0.5rem", boxShadow: " 0px 0px 5px 0px #0000000D, 0px 1px 2px 0px #00000026" }}>
-                                        <Box borderRadius="2" borderColor="border-subdued" >
-                                            <Box borderColor="border-subdued" paddingBlockEnd={3} paddingBlockStart={3} paddingInlineStart={5} paddingInlineEnd={5}>
-                                                <HorizontalStack align="space-between">
-                                                    <HorizontalStack align="start">
-                                                        <Text fontWeight="semibold" as="h3">{testSearchValue.length > 0 ? `Showing ${countSearchResults()} result` : `${filteredCategories.length} ${filteredCategories.length>1?'categories':'category'} & ${totalSelectedTestsCount()} tests`}</Text>
-                                                    </HorizontalStack>
-                                                    {testSearchValue.trim().length === 0 ? <Button onClick={() => { checkExpand() ? extendAllHandler() : collapseAllHandler() }} plain><Text>{checkExpand() ? "Expand all" : "Collapse all"}</Text></Button> : <></>}
-                                                </HorizontalStack>
-                                            </Box>
-
-                                            <ResourceList items={filteredCategories} renderItem={renderItem} />
-                                        </Box>
-                                    </div>
-                                    <div style={{height:"20px"}}></div>
-                                </VerticalStack>
-
-                            </Scrollable>
-                        </Box>
-                    </VerticalStack>
-                </Box>
-
-            </div>
-        </div>
+        <FlyLayout
+            title="Test Suite"
+            show={show}
+            setShow={setShow}
+            components={components}
+            handleClose={handleExit}
+            loading={false}
+            showDivider={false}
+            newComp={true}
+            isHandleClose={true}>
+        </FlyLayout>
     )
 }
 
--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/testSuite/TestSuiteRow.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/testSuite/TestSuiteRow.jsx
@@ -1,11 +1,11 @@
-import { Button, HorizontalStack, Text, ResourceList, Collapsible, ResourceItem, Checkbox, Box } from "@shopify/polaris"
+import { Button, HorizontalStack, Text, ResourceList, Collapsible, ResourceItem, Checkbox, Box, Link } from "@shopify/polaris"
 import {
     ChevronDownMinor,
     ChevronUpMinor
 } from '@shopify/polaris-icons';
 import "./flyLayoutSuite.css"
 
-function TestSuiteRow({ category, id, setCategories, setFilteredCategories }) {
+function TestSuiteRow({ category, setFilteredCategories, isLast }) {
     let displayName = category.displayName;
     let subCategories = category.tests;
 
@@ -23,36 +23,39 @@ function TestSuiteRow({ category, id, setCategories, setFilteredCategories }) {
 
 
     return (
-        <div className="category-list-item " style={{ borderTop: "0.5px solid rgba(221, 224, 228, 1)" }}>
-            <ResourceItem onClick={() => { toggleOpen() }} id={id}>
-                <HorizontalStack align="space-between">
-                    <HorizontalStack >
-                        <Text fontWeight="medium" as="h3">{displayName}</Text>
+        <Box borderRadiusEndEnd={(isLast) ? 2 : 0} borderRadiusEndStart={(isLast) ? 2 : 0} borderColor="border-subdued" borderBlockStartWidth="1" >
+            <div className="category-list" style={{ cursor: "pointer", ...(isLast && !category.selected && { borderBottomLeftRadius: "0.5rem", borderBottomRightRadius: "0.5rem" }) }}>
+                <Box onClick={() => { toggleOpen() }} paddingInlineStart={5} paddingBlockEnd={3} paddingBlockStart={3} paddingInlineEnd={5}>
+
+                    <HorizontalStack align="space-between">
+                        <HorizontalStack >
+                            <Text fontWeight="medium" as="h3">{displayName}</Text>
+                        </HorizontalStack>
+                        <HorizontalStack gap={4}>
+                            <span style={{ color: "#6D7175" }}>{`${category.tests.length}`}</span>
+                            <Button plain monochrome size="micro" icon={category.selected ? ChevronUpMinor : ChevronDownMinor}></Button>
+                        </HorizontalStack>
                     </HorizontalStack>
 
-                    <HorizontalStack gap={4}>
-                        <span style={{ color: "#6D7175" }}>{`${category.tests.length}`}</span>
-                        <Button plain size="micro" icon={category.selected ? ChevronUpMinor : ChevronDownMinor}></Button>
-                    </HorizontalStack>
-                </HorizontalStack>
-            </ResourceItem>
+                </Box>
+            </div>
             <div className="sub-category-lists">
-            <Collapsible open={category?.selected}>
-                {subCategories.map((subCategory) => {
-                    return (
-                        <div style={{backgroundColor:" #FAFBFB"}} className="sub-category-lists-item" onClick={() => window.open(`${window.location.origin}/dashboard/test-editor/${subCategory.value}`)}>
-                        <Box borderColor="border-subdued" borderBlockStartWidth="1" paddingInlineStart={10} paddingBlockEnd={2} paddingBlockStart={2} >
-                            <HorizontalStack key={1} align="start">
-                                <Text color="subdued" fontWeight="regular" as="h3">{subCategory.label}</Text>
-                            </HorizontalStack>
-                        </Box>
-                        </div>
-                    )
-                })}
+                <Collapsible open={category?.selected}>
+                    {subCategories.map((subCategory, index) => {
+                        return (
+
+                            <Box background="bg-subdued" borderRadiusEndEnd={(isLast && subCategories.length - 1 === index) ? 2 : 0} borderRadiusEndStart={(isLast && subCategories.length - 1 === index) ? 2 : 0} borderColor="border-subdued" borderBlockStartWidth="1" paddingInlineStart={10} paddingBlockEnd={2} paddingBlockStart={2} >
+                                <HorizontalStack key={1} align="start">
+                                    <Link onClick={() => window.open(`${window.location.origin}/dashboard/test-editor/${subCategory.value}`)} monochrome removeUnderline >
+                                        <Text color="subdued" fontWeight="regular" as="h3">{subCategory.label}</Text>
+                                    </Link>
+                                </HorizontalStack>
+                            </Box>
+                        )
+                    })}
                 </Collapsible>
             </div>
-
-        </div>
+        </Box>
     )
 }
 
