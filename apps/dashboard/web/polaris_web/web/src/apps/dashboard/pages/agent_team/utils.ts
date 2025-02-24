05f829ab1aef0da1db6e1c3766f03cb5b51146be
Devender Shekhawat <devender.shekhawat0296@gmail.com>
2025-02-24 19:08:05 +0530

    cleanup:remove unused imports

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/agent_team/utils.ts
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/agent_team/utils.ts
@@ -1,5 +1,4 @@
 import { Editor } from "@tiptap/react";
-import { ProseMirrorUnified } from 'prosemirror-unified';
 import { fromHtml } from 'hast-util-from-html';
 import { toMdast } from 'hast-util-to-mdast';
 
@@ -8,23 +7,6 @@ import { gfm } from 'micromark-extension-gfm';
 import { gfmToMarkdown } from 'mdast-util-gfm';
 import { PromptContent } from "./types";    
 
-import { 
-  GFMExtension, 
-  OrderedListExtension, 
-  UnorderedListExtension, 
-  TextExtension, 
-  ListItemExtension,
-  ParagraphExtension,
-} from 'prosemirror-remark';
-
-const pmu = new ProseMirrorUnified([
-  new GFMExtension(),
-  new OrderedListExtension(),
-  new UnorderedListExtension(),
-  new ListItemExtension(),
-  new TextExtension(),
-  new ParagraphExtension(),
-]);
 
 export const getPromptContent = (editor: Editor): PromptContent => {
     const html = editor.getHTML();
