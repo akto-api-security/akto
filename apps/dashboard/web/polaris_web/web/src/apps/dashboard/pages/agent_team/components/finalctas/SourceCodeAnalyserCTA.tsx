2cf644b0dfe007520ee74aee62ac67b129c98383
SHIVAM RAWAT <shivam@akto.io>
2025-03-25 16:25:46 +0530

    final CTA source code analyser

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/agent_team/components/finalctas/SourceCodeAnalyserCTA.tsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/agent_team/components/finalctas/SourceCodeAnalyserCTA.tsx
@@ -1,27 +1,49 @@
 import { Modal } from '@shopify/polaris'
 import React, { useState } from 'react'
 import { useAgentsStore } from '../../agents.store'
+import { intermediateStore } from '../../intermediate.store';
+import func from '../../../../../../util/func';
+import apiCollectionApi from "../../../../pages/observe/api"
 
 function SourceCodeAnalyserCTA() {
     const { finalCTAShow, setFinalCTAShow } = useAgentsStore()
-    const { setCurrentAgent} = useAgentsStore()
+
+    const { filteredUserInput, outputOptions, agentInitDocument } = intermediateStore();
+
+    function getApiCollectionName() {
+        let apiCollectionName = "source-code-ai-agent"
+        if (agentInitDocument?.sourceCodeType) {
+            apiCollectionName = `${agentInitDocument?.repository}/${agentInitDocument?.project}`
+        }
+        return apiCollectionName
+    }
+
+    async function saveApis() {
+        let apiCollectionName = getApiCollectionName()
+        let projectDir = apiCollectionName
+        let codeAnalysisApisList = filteredUserInput
+
+        await apiCollectionApi.syncExtractedAPIs(apiCollectionName, projectDir, codeAnalysisApisList)
+        func.setToast(true, false, `All the api's are now saved in ${apiCollectionName} collection`)
+    }
+
     return (
        <Modal
-            title={"Save the apis with schema into a collection"}
+            title={`Save all extracted apis`}
             open={finalCTAShow}
-            onClose={() => setFinalCTAShow(false)}
+            onClose={() => setFinalCTAShow(true)}
             primaryAction={{
                 content: 'Save apis',
-                onAction: () => {} /* setCurrentAgent as source code agent here */
+                onAction: () => {saveApis} /* setCurrentAgent as source code agent here */
             }}
             secondaryActions={[{
                 content: 'Cancel',
-                onAction: () => setFinalCTAShow(false)
+                onAction: () => setFinalCTAShow(true)
             }]}
         >
             <Modal.Section>
                 <p>
-                    You need to have APIs from Source code analyzer agent on this directory first. Please get the APIs from the agent.
+                    Save all ${filteredUserInput.length} apis with schema into "${getApiCollectionName()}" collection
                 </p>
             </Modal.Section>
         </Modal>
