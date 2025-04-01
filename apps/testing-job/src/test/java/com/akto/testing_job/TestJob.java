b80e990e08ddc75f57ac9f71e47a3de1d7ca8ff9
Oren Saldanha <oren+1@akto.io>
2025-04-01 14:53:51 +0530

    Created testing-job package

---
+++ apps/testing-job/src/test/java/com/akto/testing_job/TestJob.java
@@ -0,0 +1,11 @@
+package com.akto.testing_job;
+
+import org.junit.Test;
+
+public class TestJob { 
+    
+    @Test
+    public void testPojoConversion() {
+       System.out.println("Testing pojo conversion");
+    }
+}
