f40b81b63e4e90a99f9b397b59a67a8defecf016
Ark2307 <kr.aryan2307@gmail.com>
2025-04-01 16:03:03 +0530

    Fixing compilation errors

--- libs/utils/src/main/java/com/akto/test_editor/filter/data_operands_impl/NeqFilterObj.java
+++ libs/utils/src/main/java/com/akto/test_editor/filter/data_operands_impl/NeqFilterObj.java
@@ -5,26 +5,25 @@ import static com.akto.testing.Utils.compareWithOriginalResponse;
 import java.util.HashMap;
 import java.util.List;
 
-import com.akto.dao.test_editor.TestEditorEnums;
 import com.akto.dto.test_editor.DataOperandFilterRequest;
 
 public class NeqFilterObj extends DataOperandsImpl {
     
     @Override
-    public ValidationResult isValid(DataOperandFilterRequest dataOperandFilterRequest) {
+    public Boolean isValid(DataOperandFilterRequest dataOperandFilterRequest) {
 
         try {
             Object data = dataOperandFilterRequest.getData();
             Object querySet = dataOperandFilterRequest.getQueryset();
             List<String> queryList = (List) querySet;
             if (queryList == null || queryList.size() == 0) {
-                return new ValidationResult(false, TestEditorEnums.DataOperands.EQ_OBJ.name().toLowerCase() + " validation failed because of empty query");
+                return false;
             }
             Double matchVal = compareWithOriginalResponse(data.toString(), queryList.get(0), new HashMap<>());   
             boolean res = (matchVal < 100.0);
-            return new ValidationResult(res, TestEditorEnums.DataOperands.EQ.name().toLowerCase() + " validation passed ");            
+            return res;          
         } catch (Exception e) {
-            return new ValidationResult(false, TestEditorEnums.DataOperands.EQ_OBJ.name().toLowerCase() + " validation failed because of error " + e.getMessage());
+            return false;
         }
 
     }
