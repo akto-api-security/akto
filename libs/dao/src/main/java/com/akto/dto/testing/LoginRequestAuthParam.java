7cb4c13ca9af1bfa9c6a72855361e8649f0b997a
ayushaga14 <ayush@akto.io>
2025-04-04 14:47:04 +0530

    supprot payload token repalce

--- libs/dao/src/main/java/com/akto/dto/testing/LoginRequestAuthParam.java
+++ libs/dao/src/main/java/com/akto/dto/testing/LoginRequestAuthParam.java
@@ -36,6 +36,7 @@ public class LoginRequestAuthParam extends AuthParam {
         return Utils.isRequestKeyPresent(this.key, request, where);
     }
 
+    @Override
     public Location getWhere() {
         return where;
     }
