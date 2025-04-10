be95ca2f2054d1496c32121bfa8b99448f928a60
Fenil Shah <119055286+notlazykid@users.noreply.github.com>
2025-04-10 16:17:02 +0530

    Add 'all' key to data state in AllSensitiveData component; update defined table tabs

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/AllSensitiveData/AllSensitiveData.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/AllSensitiveData/AllSensitiveData.jsx
@@ -138,7 +138,7 @@ const convertToDataTypesData = (type, collectionsMap, countMap, subtypeToApiColl
 
 function AllSensitiveData() {
 
-    const [data, setData] = useState({'detected': [], "enabled":[], 'disabled': []})
+    const [data, setData] = useState({"all":[], "detected": [], "enabled":[], 'disabled': []})
     const [mapData, setMapData] = useState({})
     const [prompts, setPrompts] = useState([])
     const [isGptScreenActive, setIsGptScreenActive] = useState(false)
@@ -154,7 +154,7 @@ function AllSensitiveData() {
     const [severityCountMap, setSeverityCountMap] = useState([])
     const [loading, setLoading] = useState(false)
 
-    const definedTableTabs = ["Detected", "Enabled", "Disabled"]
+    const definedTableTabs = ["All", "Detected", "Enabled", "Disabled"]
     const tableSelectedTab = PersistStore.getState().tableSelectedTab[window.location.pathname]
     const initialSelectedTab = tableSelectedTab || "detected"
     const [selectedTab, setSelectedTab] = useState(initialSelectedTab)
@@ -244,6 +244,7 @@ function AllSensitiveData() {
             }
             return convertToDataTypesData(type, collectionsMap, reqResCountMap, dataTypesVsApisCount?.apiCollectionsMap || {})
         })
+        temp.all = tempArr
         temp.detected = tempArr.filter((x) => x.response > 0 || x.request > 0)
         temp.enabled = tempArr.filter((x) => x.active === true)
         temp.disabled = tempArr.filter((x) => x.active === false)
