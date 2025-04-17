8195b61ad5c7e8e02dbef58aef9362ec03761929
Fenil Shah <119055286+notlazykid@users.noreply.github.com>
2025-04-17 10:52:26 +0530

    added license key

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/api_collections/ApiCollections.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/api_collections/ApiCollections.jsx
@@ -36,7 +36,7 @@ import { AllEnterpriseModule, LicenseManager } from "ag-grid-enterprise";
 ModuleRegistry.registerModules([AllCommunityModule]);
 ModuleRegistry.registerModules([AllEnterpriseModule]);
 import { AgGridReact } from 'ag-grid-react';
-LicenseManager.setLicenseKey("api key");
+LicenseManager.setLicenseKey("[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-086015}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{14 May 2025}____[v3]_[0102]_MTc0NzE3NzIwMDAwMA==1127112b2eb5fda550f75b9d26691ef0");
 
 const headers = [
     {
