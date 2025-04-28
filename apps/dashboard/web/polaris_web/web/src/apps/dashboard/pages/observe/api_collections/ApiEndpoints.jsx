6f39c8f99ccc9e09327d685728aa41ca39d4e50f
amitpanwar789 <amitpanwar02705@gmail.com>
2025-04-28 16:31:39 +0530

    fix up for empty description saved

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/api_collections/ApiEndpoints.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/observe/api_collections/ApiEndpoints.jsx
@@ -1132,6 +1132,7 @@ function ApiEndpoints(props) {
         }
         
         setIsEditingDescription(false);
+        if(editableDescription === description) return;
         api.saveCollectionDescription(apiCollectionId, editableDescription)
             .then(() => {
                 updateCollectionDescription(allCollections, apiCollectionId, editableDescription);
