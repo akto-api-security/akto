149bf03e5312439e71b56bfe58dcdc40bb940eb7
nayan <nayan@akto.io>
2025-04-29 16:41:00 +0530

    add API to support bidirectional settings

---
+++ libs/dao/src/main/java/com/akto/dto/jira_integration/ProjectMapping.java
@@ -0,0 +1,17 @@
+package com.akto.dto.jira_integration;
+
+import com.akto.dto.jira_integration.JiraStatusApiResponse.JiraStatus;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectMapping {
+    private Set<JiraStatus> statuses;
+    private BiDirectionalSyncSettings biDirectionalSyncSettings;
+}
---
+++ libs/utils/src/main/java/com/akto/utils/JsonUtils.java
@@ -0,0 +1,34 @@
+package com.akto.utils;
+
+import com.akto.log.LoggerMaker;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JsonUtils {
+
+    private static final LoggerMaker logger = new LoggerMaker(JsonUtils.class);
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (Exception e) {
+            logger.error("Error while parsing JSON", e);
+            return null;
+        }
+    }
+
+    public static <T> T jsonToMap(String json, TypeReference<T> typeRef) {
+        try {
+            return mapper.readValue(json, typeRef);
+        } catch (Exception e) {
+            logger.error("Error while parsing JSON", e);
+            return null;
+        }
+    }
+}
