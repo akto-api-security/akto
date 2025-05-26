5294bfca12ce26f2c1e2e3c025798a4855385760
SHIVAM RAWAT <shivam@akto.io>
2025-05-26 17:40:18 +0530

    remove unused imports

--- libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ThreadLocalRandom;
 
 import com.akto.bulk_update_util.ApiInfoBulkUpdate;
 import com.akto.dao.*;
