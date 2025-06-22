5dd626cdd21839ec7518263e8065c4f6500bfa20
ayushaga14 <ayush@akto.io>
2025-06-22 11:13:34 +0530

    add log

--- apps/mini-runtime/src/main/java/com/akto/hybrid_runtime/Main.java
+++ apps/mini-runtime/src/main/java/com/akto/hybrid_runtime/Main.java
@@ -195,6 +195,7 @@ public class Main {
             System.exit(0);
         }
         actualAccountId = aSettings.getId();
+        loggerMaker.infoAndAddToDb("Fetched account settings ", LogDb.RUNTIME);
 
         DataControlFetcher.init(dataActor);
 
