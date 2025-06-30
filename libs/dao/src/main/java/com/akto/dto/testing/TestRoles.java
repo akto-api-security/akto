c797662958ea4277616a04e9cb9e17162fd4ad44
aanchalakto <aanchal@akto.io>
2025-06-30 09:44:10 +0530

    Redundant imports removed

--- libs/dao/src/main/java/com/akto/dto/testing/TestRoles.java
+++ libs/dao/src/main/java/com/akto/dto/testing/TestRoles.java
@@ -7,8 +7,6 @@ import org.bson.codecs.pojo.annotations.BsonIgnore;
 import org.bson.types.ObjectId;
 
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.akto.util.Constants.ID;
 
