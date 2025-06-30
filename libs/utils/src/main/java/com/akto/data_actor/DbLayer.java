1f989b9d4e4e15369aa0834366f03276ef87ee6b
gauravmann <gaurav@akto.io>
2025-06-30 09:10:28 +0530

    append only tags.

--- libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
@@ -479,19 +479,18 @@ public class DbLayer {
     }
 
     private static List<CollectionTags> getPreservedTags(ApiCollection apiCollection, List<CollectionTags> tags) {
-        // Replace only the KUBERNETES source tags
-        List<CollectionTags> preservedTags = new ArrayList<>();
-        if (apiCollection!= null && apiCollection.getTagsList() != null) {
-            for (CollectionTags tag : apiCollection.getTagsList()) {
-                if (!CollectionTags.TagSource.KUBERNETES.equals(tag.getSource())) {
-                    preservedTags.add(tag);
-                }
+        if(tags == null || tags.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<String> igNoreList = Arrays.asList("pod-template-hash");
+        // Ignore tags from the ignore list
+        for (CollectionTags tag : tags) {
+            if (tag.getKeyName() != null && igNoreList.contains(tag.getKeyName())) {
+                tags.remove(tag);
             }
         }
 
-        // Merge preserved tags with input tags
-        preservedTags.addAll(tags);
-        return preservedTags;
+        return tags;
     }
 
     public static void createCollectionSimpleForVpc(int vxlanId, String vpcId, List<CollectionTags> tags) { 
@@ -521,7 +520,7 @@ public class DbLayer {
 
         if (tags != null && !tags.isEmpty()) {
             // Update the entire tagsList
-            update = Updates.combine(update, Updates.set(ApiCollection.TAGS_STRING, getPreservedTags(apiCollection, tags)));
+            update = Updates.combine(update, Updates.addEachToSet(ApiCollection.TAGS_STRING, getPreservedTags(apiCollection, tags)));
         }
 
         ApiCollectionsDao.instance.getMCollection().updateOne(
@@ -570,7 +569,7 @@ public class DbLayer {
         }
 
         if(tags != null && !tags.isEmpty()) {
-            updates = Updates.combine(updates, Updates.set(ApiCollection.TAGS_STRING, getPreservedTags(apiCollection, tags)));
+            updates = Updates.combine(updates, Updates.addEachToSet(ApiCollection.TAGS_STRING, getPreservedTags(apiCollection, tags)));
         }
 
         ApiCollectionsDao.instance.getMCollection().findOneAndUpdate(Filters.eq(ApiCollection.HOST_NAME, host), updates, updateOptions);
