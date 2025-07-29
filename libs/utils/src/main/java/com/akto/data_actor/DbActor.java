74a450639b282113b38990b6d5c1998a41f20ddc
SHIVAM RAWAT <shivam@akto.io>
2025-07-30 00:12:30 +0530

    splitting url and method

--- libs/utils/src/main/java/com/akto/data_actor/DbActor.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbActor.java
@@ -33,7 +33,7 @@ import com.akto.dto.testing.WorkflowTestResult;
 import com.akto.dto.testing.sources.TestSourceConfig;
 import com.akto.dto.traffic.CollectionTags;
 import com.akto.dto.traffic.SampleData;
-import com.akto.dto.billing.UningesetedApiOverage;
+import com.akto.dto.billing.UningestedApiOverage;
 import com.akto.dto.traffic.TrafficInfo;
 import com.akto.dto.traffic_metrics.TrafficMetrics;
 import com.akto.dto.type.SingleTypeInfo;
@@ -138,16 +138,16 @@ public class DbActor extends DataActor {
     }
 
     public void bulkWriteOverageInfo(List<Object> writesForOverageInfo) {
-        ArrayList<WriteModel<UningesetedApiOverage>> writes = new ArrayList<>();
+        ArrayList<WriteModel<UningestedApiOverage>> writes = new ArrayList<>();
         for (Object obj : writesForOverageInfo) {
-            WriteModel<UningesetedApiOverage> write = (WriteModel<UningesetedApiOverage>) obj;
+            WriteModel<UningestedApiOverage> write = (WriteModel<UningestedApiOverage>) obj;
             writes.add(write);
         }
         DbLayer.bulkWriteOverageInfo(writes);
     }
 
-    public boolean overageApisExists(int apiCollectionId, String urlType, String methodAndUrl) {
-        return com.akto.dao.billing.UningestedApiOverageDao.instance.exists(apiCollectionId, urlType, methodAndUrl);
+    public boolean overageApisExists(int apiCollectionId, String urlType, Method method, String url) {
+        return com.akto.dao.billing.UningestedApiOverageDao.instance.exists(apiCollectionId, urlType, method, url);
     }
 
     public TestSourceConfig findTestSourceConfig(String subType){
