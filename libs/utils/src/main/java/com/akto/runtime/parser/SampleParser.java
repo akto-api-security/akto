352b4b08208988055c6cd3329d67a1126255dc97
ayushaga14 <ayush@akto.io>
2025-08-06 15:42:42 +0530

    add check for incomplete json in request payload

--- libs/utils/src/main/java/com/akto/runtime/parser/SampleParser.java
+++ libs/utils/src/main/java/com/akto/runtime/parser/SampleParser.java
@@ -43,7 +43,7 @@ public class SampleParser {
     public static boolean isPossiblyIncompleteJson(String json) {
         if (json == null || json.trim().isEmpty()) return true;
 
-        if (json.startsWith("{") && !json.endsWith("}") || json.endsWith("[") && !json.endsWith("]")) {
+        if ((json.startsWith("{") && !json.endsWith("}")) || (json.endsWith("[") && !json.endsWith("]"))) {
             return true;
         }
         return false;
