fdc61f056e5437727afae8845bcff74e728ebdd8
aanchalakto <aanchal@akto.io>
2025-08-18 14:45:53 +0530

    mcp audit logging added

--- apps/dashboard/src/main/java/com/akto/action/MCPScanAction.java
+++ apps/dashboard/src/main/java/com/akto/action/MCPScanAction.java
@@ -61,7 +61,7 @@ public class MCPScanAction extends UserAction {
 
                 //New MCP server detected, audit it
                 McpAuditInfo auditInfo = new McpAuditInfo(
-                        Context.now(), "", AKTO_MCP_SERVER_TAG , 0, // updatedTs set to null
+                        Context.now(), "", AKTO_MCP_SERVER_TAG , 0,
                         hostName, "", null,
                         collectionId
                 );
