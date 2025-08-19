a414c4a00fc1e5b077a9d4a9fc75bb4b5054019a
aanchalakto <aanchal@akto.io>
2025-08-19 11:24:07 +0530

    MCP Audit data logging details

--- apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
+++ apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
@@ -67,6 +67,7 @@ import lombok.Getter;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.model.*;
+import lombok.Setter;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import com.google.gson.Gson;
@@ -107,6 +108,7 @@ public class DbAction extends ActionSupport {
     List<DependencyNode> dependencyNodeList;
     TestScript testScript;
     String openApiSchema;
+    @Getter @Setter
     McpAuditInfo auditInfo;
 
     private ModuleInfo moduleInfo;
