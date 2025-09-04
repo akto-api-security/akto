ea3d90748e7e247a60e534fe928b315b44a9043b
gauravmann <gaurav@akto.io>
2025-09-04 12:37:48 +0530

    keep ip regex constant

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/threat_detection/utils/formatUtils.js
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/threat_detection/utils/formatUtils.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text } from "@shopify/polaris";
 
+// Regular expression to validate IP address (IPv4 and IPv6)
+const IPV4_REGEX = /^(\d{1,3}\.){3}\d{1,3}$/;
+const IPV6_REGEX = /^([0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}$/;
+
 export const formatActorId = (actorId) => {
   if (!actorId) return "-";
   
-  // Regular expression to validate IP address (IPv4 and IPv6)
-  const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
-  const ipv6Regex = /^([0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}$/;
-  
-  const isValidIP = ipv4Regex.test(actorId) || ipv6Regex.test(actorId);
+  const isValidIP = IPV4_REGEX.test(actorId) || IPV6_REGEX.test(actorId);
   
   if (isValidIP) {
     return (
