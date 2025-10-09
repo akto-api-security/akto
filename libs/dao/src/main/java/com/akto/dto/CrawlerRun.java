670c07a3637fd24487a8ced7b03f41f651cd4893
notshivansh <shivansh@akto.io>
2025-10-09 15:48:46 +0530

    clean code

--- libs/dao/src/main/java/com/akto/dto/CrawlerRun.java
+++ libs/dao/src/main/java/com/akto/dto/CrawlerRun.java
@@ -3,36 +3,26 @@ package com.akto.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class CrawlerRun {
 
     public static final String STARTED_BY = "startedBy";
-    @Getter
-    @Setter
     private String startedBy;
 
     public static final String START_TIMESTAMP = "startTimestamp";
-    @Getter
-    @Setter
     private int startTimestamp;
 
     public static final String END_TIMESTAMP = "endTimestamp";
-    @Getter
-    @Setter
     private int endTimestamp;
 
     public static final String CRAWL_ID = "crawlId";
-    @Getter
-    @Setter
     private String crawlId;
 
     public static final String HOSTNAME = "hostname";
-    @Getter
-    @Setter
     private String hostname;
 
     public static final String OUT_SCOPE_URLS = "outScopeUrls";
-    @Getter
-    @Setter
     private String outScopeUrls;
 
     public CrawlerRun() {
--- libs/dao/src/main/java/com/akto/dto/CrawlerUrl.java
+++ libs/dao/src/main/java/com/akto/dto/CrawlerUrl.java
@@ -3,26 +3,20 @@ package com.akto.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class CrawlerUrl {
 
     public static final String URL = "url";
-    @Getter
-    @Setter
     private String url;
 
     public static final String ACCEPTED = "accepted";
-    @Getter
-    @Setter
     private boolean accepted;
 
     public static final String TIMESTAMP = "timestamp";
-    @Getter
-    @Setter
     private int timestamp;
 
     public static final String CRAWL_ID = "crawlId";
-    @Getter
-    @Setter
     private String crawlId;
 
     public CrawlerUrl() {
