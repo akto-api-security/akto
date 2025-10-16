d97fa3579e769a21f5972c6e62d304badef5a735
harshithb3304 <harsh03032004@gmail.com>
2025-10-16 22:44:48 +0530

    added show/hide button

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/TestRoleSettings/ParamsCard.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/TestRoleSettings/ParamsCard.jsx
@@ -1,9 +1,10 @@
 import { Box, Button, HorizontalStack, Text, VerticalStack } from '@shopify/polaris'
-import React from 'react'
+import React, { useState } from 'react'
 import TooltipText from "../../../components/shared/TooltipText"
 import { DeleteMajor } from "@shopify/polaris-icons"
 
 function ParamsCard({dataObj, handleDelete, showEdit}) {
+    const [hideValues, setHideValues] = useState(true)
     const authMechanism = dataObj.authMechanism
     const headerConditions = dataObj.headerKVPairs || {}
     const headerKey = Object.keys(headerConditions).length > 0 ? Object.keys(headerConditions)[0] : ''
@@ -32,7 +33,7 @@ function ParamsCard({dataObj, handleDelete, showEdit}) {
                 {valuesList.map((param,index) => {
                     return(
                         <HorizontalStack blockAlign="start" gap={"4"} key={index}>
-                            <LineComponent title={(param.key || '-') + " :"} value={(param.value || '-')} />
+                            <LineComponent title={(param.key || '-') + " :"} value={(hideValues ? '********' : (param.value || '-'))} />
                             {param.showHeader !== null ? 
                                 <HorizontalStack blockAlign="start" gap={1}><Box borderInlineEndWidth='1' borderColor="border-subdued" minHeight='20px'/><LineComponent title={"Position :"} value={param.where}/></HorizontalStack>
                             :null}
@@ -58,7 +59,14 @@ function ParamsCard({dataObj, handleDelete, showEdit}) {
                         <Box paddingInlineStart={4}>
                             <VerticalStack gap={2}>
                                 <LineComponent title={"Token type :"} value={authMechanism.type}/>
-                                <LineComponent title={"Token values :"} />
+                                <HorizontalStack gap="4" blockAlign="center">
+                                    <Box maxWidth='200px'>
+                                        <TooltipText tooltip={"Token values"} text={"Token values :"} textProps={{variant:"bodyMd", fontWeight: "medium"}} />
+                                    </Box>
+                                    <Button size="slim" onClick={() => setHideValues(!hideValues)}>
+                                        {hideValues ? 'Show' : 'Hide'}
+                                    </Button>
+                                </HorizontalStack>
                             </VerticalStack>
                             <Box paddingInlineStart={4}>
                                 <ParamsList valuesList={authMechanism.authParams} />
