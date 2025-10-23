f1ca3dd6a0d8758c9f29dd69fd794b4f56864eba
nayan <nayan@akto.io>
2025-10-23 11:49:44 +0530

    add guardrail policy fetch api

--- apps/database-abstractor/src/main/java/com/akto/action/GuardrailPoliciesAction.java
+++ apps/database-abstractor/src/main/java/com/akto/action/GuardrailPoliciesAction.java
@@ -1,23 +1,26 @@
 package com.akto.action;
 
-import com.akto.dao.GuardrailPoliciesDao;
+import com.akto.data_actor.DbLayer;
 import com.akto.dto.GuardrailPolicies;
 import com.akto.log.LoggerMaker;
 import com.akto.log.LoggerMaker.LogDb;
 import com.opensymphony.xwork2.ActionSupport;
 import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class GuardrailPoliciesAction extends ActionSupport {
 
     private static final LoggerMaker loggerMaker = new LoggerMaker(GuardrailPoliciesAction.class, LogDb.DB_ABS);
 
     private List<GuardrailPolicies> guardrailPolicies;
+    private Integer updatedAfter;
 
     public String fetchGuardrailPolicies() {
         try {
-            this.guardrailPolicies = GuardrailPoliciesDao.instance.findAll();
+            this.guardrailPolicies = DbLayer.fetchGuardrailPolicies(updatedAfter);
             return SUCCESS.toUpperCase();
         } catch (Exception e) {
             loggerMaker.errorAndAddToDb("Error fetching guardrail policies: " + e.getMessage(), LogDb.DASHBOARD);
