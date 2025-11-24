cddce22068cebda270f2513ab933a84aa4b60416
TangoBeeAkto <umesh@akto.io>
2025-11-24 17:30:36 +0530

    added custom model support

--- apps/database-abstractor/src/main/java/com/akto/action/LLMAction.java
+++ apps/database-abstractor/src/main/java/com/akto/action/LLMAction.java
@@ -1,11 +1,15 @@
 package com.akto.action;
 
+import com.akto.dao.agents.AgentModelDao;
+import com.akto.dto.agents.Model;
+import com.akto.dto.agents.ModelType;
 import com.akto.dto.type.URLMethods.Method;
 import com.akto.log.LoggerMaker;
 import com.akto.log.LoggerMaker.LogDb;
 import com.akto.util.http_util.CoreHTTPClient;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.mongodb.client.model.Filters;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 import java.util.HashMap;
@@ -39,11 +43,10 @@ public class LLMAction extends ActionSupport {
 
     // Azure Open AI urls
     private static final String AZURE_API_ENDPOINT = "%s/openai/deployments/%s/chat/completions?api-version=%s";
-    private static final String AZURE_OPENAI_HOST = System.getenv("AZURE_OPENAI_HOST");
-    private static final String AZURE_OPENAI_DEPLOYMENT = System.getenv("AZURE_OPENAI_DEPLOYMENT");
-    private static final String AZURE_OPENAI_API_KEY = System.getenv("AZURE_OPENAI_API_KEY");
-    private static final String AZURE_OPENAI_API_VERSION = System.getenv("AZURE_OPENAI_API_VERSION");
-    private static final String AZURE_OPENAI_ENDPOINT = buildAzureOpenAIUrl();
+    private static final String AZURE_OPENAI_HOST = System.getenv().getOrDefault("AZURE_OPENAI_HOST", "");
+    private static final String AZURE_OPENAI_DEPLOYMENT = System.getenv().getOrDefault("AZURE_OPENAI_DEPLOYMENT", "");
+    private static final String AZURE_OPENAI_API_KEY = System.getenv().getOrDefault("AZURE_OPENAI_API_KEY", "");
+    private static final String AZURE_OPENAI_API_VERSION = System.getenv().getOrDefault("AZURE_OPENAI_API_VERSION", "");
 
 
     private static String buildLlmServerUrl() {
@@ -65,16 +68,35 @@ public class LLMAction extends ActionSupport {
     Map<String, Object> llmResponsePayload;
 
     public String getLLMResponse() {
-        return executeLLMRequest(OLLAMA_SERVER_ENDPOINT, null);
+        return executeLLMRequest(OLLAMA_SERVER_ENDPOINT, null, null);
     }
 
     public String getLLMResponseV2() {
         Map<String, String> headers = new HashMap<>();
-        headers.put("api-key", AZURE_OPENAI_API_KEY);
-        return executeLLMRequest(AZURE_OPENAI_ENDPOINT, headers);
+
+        Model model = AgentModelDao.instance.findOne(Filters.eq("type", ModelType.MISTRAL_AI.name()));
+        String endpoint = "";
+        try {
+            endpoint = buildAzureOpenAIUrl();
+        } catch (Exception e) { logger.error(e.getMessage()); }
+        String apiKey = AZURE_OPENAI_API_KEY;
+        String modelNameFinal = "";
+        if(model != null) {
+            endpoint = model.getParams().getOrDefault("azureOpenAIEndpoint", "");
+            apiKey = model.getParams().getOrDefault("apiKey", "");
+            headers.put("authorization", apiKey);
+            String modelName = model.getParams().getOrDefault("model", "");
+            if(modelName != null && !modelName.isEmpty()){
+                modelNameFinal = modelName;
+            }
+        }
+
+        headers.put("api-key", apiKey);
+
+        return executeLLMRequest(endpoint, headers, modelNameFinal);
     }
 
-    private String executeLLMRequest(String endpoint, Map<String, String> additionalHeaders) {
+    private String executeLLMRequest(String endpoint, Map<String, String> additionalHeaders, String modelName) {
         MediaType mediaType = MediaType.parse("application/json");
 
         if (llmPayload == null || llmPayload.isEmpty()) {
@@ -83,6 +105,9 @@ public class LLMAction extends ActionSupport {
         }
 
         JSONObject payload = new JSONObject(llmPayload);
+        if(modelName != null && !modelName.isEmpty()){
+            payload.put("model", modelName);
+        }
         RequestBody body = RequestBody.create(payload.toString(), mediaType);
 
         Request.Builder requestBuilder = new Request.Builder()
