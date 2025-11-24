153b360be74d90b1ce4f451a7f830bbcc7a8afc8
notshivansh <shivansh@akto.io>
2025-11-24 16:50:08 +0530

    more logs

--- apps/mini-testing/src/main/java/com/akto/crons/GetRunningTestsStatus.java
+++ apps/mini-testing/src/main/java/com/akto/crons/GetRunningTestsStatus.java
@@ -12,11 +12,14 @@ import com.akto.data_actor.DataActor;
 import com.akto.data_actor.DataActorFactory;
 import com.akto.dto.testing.TestingRun;
 import com.akto.dto.testing.TestingRunResultSummary;
+import com.akto.log.LoggerMaker;
+import com.akto.log.LoggerMaker.LogDb;
 
 public class GetRunningTestsStatus {
     private final ConcurrentHashMap<ObjectId, TestingRun.State> currentRunningTestsMap = new ConcurrentHashMap<>();
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     public static final DataActor dataActor = DataActorFactory.fetchInstance();
+    private static final LoggerMaker loggerMaker = new LoggerMaker(GetRunningTestsStatus.class, LogDb.TESTING);
     private GetRunningTestsStatus () {
     }
 
@@ -43,6 +46,7 @@ public class GetRunningTestsStatus {
                 } 
             } catch (Exception e) {
                 e.printStackTrace();
+                loggerMaker.errorAndAddToDb(e, "Error in getStatusOfRunningTests");
             }
         }
         }, 0 , 10, TimeUnit.SECONDS);
