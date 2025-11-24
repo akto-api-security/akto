ddbbbec11ffc5af5be3bdfe2f86328f53e87aac4
Ark2307 <kr.aryan2307@gmail.com>
2025-11-24 11:37:13 +0530

    Fixing compilation error

--- apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
+++ apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
@@ -1597,32 +1597,6 @@ public class DbAction extends ActionSupport {
     List<Map<String, Object>> apiInfoKeysList;
     int timestamp;
 
-    public String bulkUpdateLastTestedField() {
-        try {
-            if (apiInfoKeysList == null || apiInfoKeysList.isEmpty()) {
-                return Action.ERROR.toUpperCase();
-            }
-            List<ApiInfo.ApiInfoKey> apiInfoKeys = new ArrayList<>();
-            for (Map<String, Object> keyMap : apiInfoKeysList) {
-                int apiCollectionIdVal = 0;
-                try {
-                    apiCollectionIdVal = (int)(long) keyMap.get("apiCollectionId");
-                } catch (Exception e) {
-                    apiCollectionIdVal = (int) keyMap.get("apiCollectionId");
-                }
-                String urlVal = (String) keyMap.get("url");
-                String methodStr = (String) keyMap.get("method");
-                URLMethods.Method methodEnum = URLMethods.Method.fromString(methodStr);
-                ApiInfo.ApiInfoKey apiInfoKey = new ApiInfo.ApiInfoKey(apiCollectionIdVal, urlVal, methodEnum);
-                apiInfoKeys.add(apiInfoKey);
-            }
-            DbLayer.bulkUpdateLastTestedField(apiInfoKeys, timestamp);
-        } catch (Exception e) {
-            return Action.ERROR.toUpperCase();
-        }
-        return Action.SUCCESS.toUpperCase();
-    }
-
     public String updateTestInitiatedCountInTestSummary() {
         try {
             DbLayer.updateTestInitiatedCountInTestSummary(summaryId, testInitiatedCount);
