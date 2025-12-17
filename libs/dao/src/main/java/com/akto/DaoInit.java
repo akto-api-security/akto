7c41c8b68f328dd08fcbc0004c7f9f3488f9e98a
Karan <karan@akto.io>
2025-12-17 15:49:01 +0530

    Add expiration handling for AgentTrafficLog and create indices for AgentTrafficLogDao
    - Introduced `expiresAtEpoch` field in `AgentTrafficLog` for better expiration management.
    - Updated `bulkWriteAgentTrafficLogs` method to convert `expiresAtEpoch` to `expiresAt` Date before insertion.
    - Added index creation for `AgentTrafficLogDao` in `DaoInit` class.

--- libs/dao/src/main/java/com/akto/DaoInit.java
+++ libs/dao/src/main/java/com/akto/DaoInit.java
@@ -412,6 +412,7 @@ public class DaoInit {
         McpReconRequestDao.instance.createIndicesIfAbsent();
         McpReconResultDao.instance.createIndicesIfAbsent();
         LogsEndpointShieldDao.instance.createIndicesIfAbsent();
+        AgentTrafficLogDao.instance.createIndicesIfAbsent();
     }
 
 }
