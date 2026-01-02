5fe41695d196528a2b75e981dd783c3c0d92ea30
nayanakto <nayan@akto.io>
2026-01-02 12:37:19 +0530

    add limit check. decrease max iterations to 10 from 20 (#3880)
    * add limit check. decrease max iterations to 10 from 20
    
    * add comments
    
    * shift test curl above
    
    * add body limit
    
    * add limit
    
    * revert the check

--- apps/dashboard/src/main/java/com/akto/action/DevRevIntegrationAction.java
+++ apps/dashboard/src/main/java/com/akto/action/DevRevIntegrationAction.java
@@ -77,6 +77,7 @@ public class DevRevIntegrationAction extends UserAction {
 
     public String createDevRevTickets() {
         try {
+
             DevRevIntegrationService devRevService = new DevRevIntegrationService();
             TicketCreationResult result = devRevService.createTickets(testingIssuesIdList, partId, workItemType, aktoDashboardHost);
 
