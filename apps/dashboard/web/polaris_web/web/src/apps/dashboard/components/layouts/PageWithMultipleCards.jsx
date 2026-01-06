9098eb2e6f34f93e771e83eee90d24615d4f2704
vrushabh-akto <vrushabh@akto.io>
2026-01-06 12:33:41 +0530

    fix: Update Learn More button visibility logic to use category check functions

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/layouts/PageWithMultipleCards.jsx
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/components/layouts/PageWithMultipleCards.jsx
@@ -1,7 +1,7 @@
 import {  HorizontalStack,  Page, VerticalStack } from "@shopify/polaris";
 import { useNavigate, useLocation } from "react-router-dom";
 import { learnMoreObject } from "../../../main/onboardingData"
-import { getDashboardCategory, CATEGORY_AGENTIC_SECURITY, CATEGORY_ENDPOINT_SECURITY } from "../../../main/labelHelper"
+import { getDashboardCategory, isAgenticSecurityCategory, isEndpointSecurityCategory } from "../../../main/labelHelper"
 import LearnPopoverComponent from "./LearnPopoverComponent";
 import func from  "@/util/func"
 import { useEffect, useRef } from "react";
@@ -61,8 +61,7 @@ const PageWithMultipleCards = (props) => {
 
     const learnMoreObj = learnMoreObject.hasOwnProperty(func.transformString(location.pathname)) ? learnMoreObject[func.transformString(location.pathname)] : null
 
-    const currentCategory = getDashboardCategory()
-    const showLearnButton = learnMoreObj && currentCategory !== CATEGORY_AGENTIC_SECURITY && currentCategory !== CATEGORY_ENDPOINT_SECURITY
+    const showLearnButton = learnMoreObj && !isAgenticSecurityCategory() && !isEndpointSecurityCategory()
 
     const learnMoreComp = (
         showLearnButton ?
