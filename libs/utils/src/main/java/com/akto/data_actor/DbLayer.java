adfd2dc614f4f43582c55d81ecf4e156f33594c3
kural-akto <kural@akto.io>
2026-01-08 18:38:05 +0530

    final changes

--- libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
@@ -18,6 +18,9 @@ import java.util.stream.Collectors;
 
 import com.akto.bulk_update_util.ApiInfoBulkUpdate;
 import com.akto.dao.*;
+import com.akto.dao.AccountsContextDao;
+import com.akto.dao.AgentTrafficLogDao;
+import com.akto.dao.CyborgLogsDao;
 import com.akto.dao.filter.MergedUrlsDao;
 import com.akto.dao.graph.SvcToSvcGraphEdgesDao;
 import com.akto.dao.graph.SvcToSvcGraphNodesDao;
@@ -27,6 +30,7 @@ import com.akto.dao.settings.DataControlSettingsDao;
 import com.akto.dao.testing.config.TestSuiteDao;
 import com.akto.dependency_analyser.DependencyAnalyserUtils;
 import com.akto.dto.*;
+import com.akto.dto.AgentTrafficLog;
 import com.akto.dto.filter.MergedUrls;
 import com.akto.dto.graph.SvcToSvcGraphEdge;
 import com.akto.dto.graph.SvcToSvcGraphNode;
@@ -212,6 +216,7 @@ public class DbLayer {
         for (ModuleInfo moduleInfo : moduleInfoList) {
             Bson filter = Filters.eq(ModuleInfoDao.ID, moduleInfo.getId());
             Bson updates = Updates.combine(
+                    //putting class name because findOneAndUpdate doesn't put class name by default
                     Updates.setOnInsert("_t", moduleInfo.getClass().getName()),
                     Updates.setOnInsert(ModuleInfo.MODULE_TYPE, moduleInfo.getModuleType().name()),
                     Updates.setOnInsert(ModuleInfo.STARTED_TS, moduleInfo.getStartedTs()),
