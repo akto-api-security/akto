5edc5b4ddd2c0c941fb1c312d4e92881280469f1
Ark2307 <kr.aryan2307@gmail.com>
2026-01-15 12:30:49 +0530

    Adding normal matching case

--- libs/utils/src/main/java/com/akto/runtime/RuntimeUtil.java
+++ libs/utils/src/main/java/com/akto/runtime/RuntimeUtil.java
@@ -151,7 +151,7 @@ public class RuntimeUtil {
 
     }
 
-    public static void fillURLTemplatesMap(List<ApiInfo> apiInfos, Map<String, Set<URLMethods.Method>> apiInfoUrlToMethods, Map<Integer, List<URLTemplate>> apiCollectionUrlTemplates){
+    public static void fillURLTemplatesMap(List<ApiInfo> apiInfos, Map<String, Set<URLMethods.Method>> apiInfoUrlToMethods, Map<Integer, List<URLTemplate>> apiCollectionUrlTemplates, Map<String, ApiInfo.ApiInfoKey> apiInfoKeyToApiInfo){
         if (apiInfos != null && !apiInfos.isEmpty()) {
             for (ApiInfo apiInfo : apiInfos) {
                 String url = apiInfo.getId().getUrl();
@@ -173,6 +173,10 @@ public class RuntimeUtil {
                     }
 
                     apiCollectionUrlTemplates.get(apiCollectionId).add(urlTemplate);
+                }else if(apiInfoKeyToApiInfo != null){
+                    // remove host from url
+                    String urlWithoutHost = url.replaceAll("https?://[^/]+", "");
+                    apiInfoKeyToApiInfo.put(apiCollectionId + " " + urlWithoutHost + " " + method.name(), apiInfo.getId());
                 }
             }
         }
