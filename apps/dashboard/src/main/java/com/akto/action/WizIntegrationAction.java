56d64eae0a9affed34c04af9ff34dd575f52c403
orenakto <oren+1@akto.io>
2026-02-12 12:22:03 +0530

    create wiz integration action

---
+++ apps/dashboard/src/main/java/com/akto/action/WizIntegrationAction.java
@@ -0,0 +1,10 @@
+package com.akto.action;
+
+import com.akto.log.LoggerMaker;
+import com.akto.log.LoggerMaker.LogDb;
+
+public class WizIntegrationAction extends UserAction {
+
+    private static final LoggerMaker logger = new LoggerMaker(WizIntegrationAction.class, LogDb.DASHBOARD);
+
+}
