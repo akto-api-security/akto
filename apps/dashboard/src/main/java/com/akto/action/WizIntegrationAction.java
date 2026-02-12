3293dfc02e04de79fc1f65edd22560d2461b6f4a
orenakto <oren+1@akto.io>
2026-02-12 12:26:05 +0530

    add methods

--- apps/dashboard/src/main/java/com/akto/action/WizIntegrationAction.java
+++ apps/dashboard/src/main/java/com/akto/action/WizIntegrationAction.java
@@ -2,9 +2,23 @@ package com.akto.action;
 
 import com.akto.log.LoggerMaker;
 import com.akto.log.LoggerMaker.LogDb;
+import com.opensymphony.xwork2.Action;
+
+import lombok.Getter;
+import lombok.Setter;
 
 public class WizIntegrationAction extends UserAction {
 
     private static final LoggerMaker logger = new LoggerMaker(WizIntegrationAction.class, LogDb.DASHBOARD);
 
+    public String addWizIntegration() {
+        return Action.SUCCESS.toUpperCase();
+    }
+    @Getter
+    @Setter
+    private TestingIssuesId testingIssuesId;
+
+    public String sendToWiz() {
+        return Action.SUCCESS.toUpperCase();
+    }
 }
