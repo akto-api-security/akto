07e565416102bb0f66bd4204a1f51b7f9ae3fbab
ayushaga14 <ayush@akto.io>
2026-02-13 21:30:07 +0530

    optimise fetchSensitiveAndUnauthenticatedValue

--- apps/dashboard/src/main/java/com/akto/action/ApiCollectionsAction.java
+++ apps/dashboard/src/main/java/com/akto/action/ApiCollectionsAction.java
@@ -1165,6 +1165,27 @@ public class ApiCollectionsAction extends UserAction {
 
     public String fetchSensitiveAndUnauthenticatedValue() {
         Bson filterQ = UsageMetricCalculator.excludeDemosAndDeactivated(ApiInfo.ID_API_COLLECTION_ID);
+
+        if (!this.showApiInfo) {
+            // Optimized path: total count - excluded count
+            Bson baseFilter = Filters.and(
+                Filters.eq(ApiInfo.IS_SENSITIVE, true),
+                Filters.eq(ApiInfo.ALL_AUTH_TYPES_FOUND, ApiInfo.AuthType.UNAUTHENTICATED)
+            );
+
+            int totalCount = (int) ApiInfoDao.instance.count(baseFilter);
+
+            Set<Integer> demosAndDeactivated = UsageMetricCalculator.getDemosAndDeactivated();
+            Bson excludedFilter = Filters.and(
+                baseFilter,
+                Filters.in(ApiInfo.ID_API_COLLECTION_ID, demosAndDeactivated)
+            );
+            int excludedCount = (int) ApiInfoDao.instance.count(excludedFilter);
+
+            this.sensitiveUnauthenticatedEndpointsCount = totalCount - excludedCount;
+            return Action.SUCCESS.toUpperCase();
+        }
+
         List<ApiInfo> sensitiveEndpoints = ApiInfoDao.instance.findAll(Filters.and(filterQ, Filters.eq(ApiInfo.IS_SENSITIVE, true)));
         for (ApiInfo apiInfo : sensitiveEndpoints) {
             if (apiInfo.getAllAuthTypesFound() != null && !apiInfo.getAllAuthTypesFound().isEmpty()) {
