f6ee741ff89975d38208445f8a94cb9088131a7e
nayan <nayan@akto.io>
2026-02-16 13:27:39 +0530

    fix thest cases

--- apps/api-runtime/src/test/java/com/akto/utils/McpRequestResponseUtilsTest.java
+++ apps/api-runtime/src/test/java/com/akto/utils/McpRequestResponseUtilsTest.java
@@ -24,6 +24,7 @@ public class McpRequestResponseUtilsTest {
 
         HttpResponseParams responseParams = new HttpResponseParams();
         responseParams.setRequestParams(reqParams);
+        responseParams.setSource(HttpResponseParams.Source.MIRRORING);
 
         return responseParams;
     }
