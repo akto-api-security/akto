1c4058f5c866e3fcf9c1cc1ce562128e071a68f1
gauravmann <gaurav@akto.io>
2026-02-16 17:21:18 +0530

    fix env errors

--- libs/utils/src/main/java/com/akto/util/DashboardMode.java
+++ libs/utils/src/main/java/com/akto/util/DashboardMode.java
@@ -25,6 +25,9 @@ public enum DashboardMode {
     // modify this and remove getActualDashboardMode method
     public static DashboardMode getDashboardMode(){
         String dashboardMode = System.getenv("DASHBOARD_MODE");
+        if (dashboardMode == null || dashboardMode.isEmpty()) {
+            dashboardMode = System.getProperty("DASHBOARD_MODE");
+        }
         if("on_prem".equalsIgnoreCase(dashboardMode)){
             return ON_PREM;
         }
