9b92bc1734fff23e6d36878d4948a4abaa5706d7
gauravmann <gaurav@akto.io>
2026-03-09 12:14:20 +0530

    don't ignore if coverage test header

--- libs/dao/src/main/java/com/akto/util/Constants.java
+++ libs/dao/src/main/java/com/akto/util/Constants.java
@@ -20,6 +20,7 @@ public class Constants {
     public static final int TWO_HOURS_TIMESTAMP = ( 60 * 60 * 2 );
 
     public static final String AKTO_IGNORE_FLAG = "x-akto-ignore";
+    public static final String AKTO_COVERAGE_FLAG = "x-akto-coverage-check";
     public static final String AKTO_ATTACH_FILE = "x-akto-attach-file";
     public static final String AKTO_TOKEN_KEY = "x-akto-key";
     public static final String AKTO_NODE_ID = "x-akto-node";
