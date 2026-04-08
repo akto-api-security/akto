3e847a62bc2ed1e0a964603029fd9f9141719098
gauravmann <gaurav@akto.io>
2026-04-08 12:01:09 +0530

    fix restart pid issue

--- apps/mini-runtime/start.sh
+++ apps/mini-runtime/start.sh
@@ -87,7 +87,7 @@ done &
 # Start Java and monitor it
 start_java() {
     
-    java -XX:+ExitOnOutOfMemoryError -Xmx${XMX_MEM}m -jar /app/mini-runtime-1.0-SNAPSHOT-jar-with-dependencies.jar >> "$LOG_FILE" 2>&1 &
+    java -XX:+ExitOnOutOfMemoryError -Xmx${XMX_MEM}m -jar /app/mini-runtime-1.0-SNAPSHOT-jar-with-dependencies.jar > >(tee -a "$LOG_FILE") 2>&1 &
 
     JAVA_PID=$!
     echo "Started Java with PID: $JAVA_PID" | tee -a "$LOG_FILE"
