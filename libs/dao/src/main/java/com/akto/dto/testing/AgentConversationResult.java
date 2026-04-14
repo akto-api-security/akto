abf9aef3220a4339b9e950d7a1f35415ae8449f1
Ark2307 <kr.aryan2307@gmail.com>
2026-04-14 22:25:54 +0530

    Adding dto changes

--- libs/dao/src/main/java/com/akto/dto/testing/AgentConversationResult.java
+++ libs/dao/src/main/java/com/akto/dto/testing/AgentConversationResult.java
@@ -1,7 +1,9 @@
 package com.akto.dto.testing;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.akto.util.Pair;
 
@@ -22,6 +24,12 @@ public class AgentConversationResult extends GenericAgentConversation {
     private List<String> conversation;
     private List<Pair<String, String>> addedConversations;
 
+    // store the tools calls in that conversation
+    // key: tool_name/agent_name
+    // if type: agent_name, schedule job to find from traces {store trace ids for that conversation}
+    // if type: tool_name, store the metadata contains of the tool call
+    private Map<String,Object> toolsMetadata;
+
     public AgentConversationResult(String conversationId, String originalPrompt, String response, List<String> conversation, int timestamp, boolean validation, String validationMessage, String finalSentPrompt, String remediationMessage, int externalApiTokens){
         super("", conversationId, originalPrompt, response, finalSentPrompt, timestamp, timestamp, 0, externalApiTokens, 0, ConversationType.TEST_EXECUTION_RESULT);
         this.validation = validation;
@@ -29,5 +37,6 @@ public class AgentConversationResult extends GenericAgentConversation {
         this.remediationMessage = remediationMessage;
         this.conversation = conversation;
         this.addedConversations = new ArrayList<>();
+        this.toolsMetadata = new HashMap<>();
     }
 }
