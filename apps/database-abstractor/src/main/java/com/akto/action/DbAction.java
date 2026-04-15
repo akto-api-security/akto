e2607ed6f0e35b176a0d8c51cda00ec3e40437fa
harshithb3304 <harsh03032004@gmail.com>
2026-04-15 17:38:09 +0530

    added api to fetchApprovedMcpServer

--- apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
+++ apps/database-abstractor/src/main/java/com/akto/action/DbAction.java
@@ -121,6 +121,7 @@ public class DbAction extends ActionSupport {
     List<CustomAuthTypeMapper> customAuthTypes;
     AccountSettings accountSettings;
     Config.DatadogForwarderConfig datadogForwarderConfig;
+    List<Map<String, Object>> approvedMcpServers;
     List<ApiInfo> apiInfos;
     APIConfig apiConfig;
     List<SingleTypeInfo> stis;
@@ -568,6 +569,23 @@ public class DbAction extends ActionSupport {
             );
         } catch (Exception e) {
             loggerMaker.errorAndAddToDb(e, "error in fetchDatadogForwarderConfig " + e.toString());
+            return Action.ERROR.toUpperCase();
+        }
+        return Action.SUCCESS.toUpperCase();
+    }
+
+    public String fetchApprovedMcpServers() {
+        try {
+            int accountId = Context.accountId.get();
+            String configId = accountId + "_MCP_REGISTRY";
+            Config.McpRegistryConfig mcpRegistryConfig = (Config.McpRegistryConfig) ConfigsDao.instance.findOne(
+                com.mongodb.client.model.Filters.eq("_id", configId)
+            );
+            if (mcpRegistryConfig != null) {
+                approvedMcpServers = mcpRegistryConfig.getApprovedServers();
+            }
+        } catch (Exception e) {
+            loggerMaker.errorAndAddToDb(e, "error in fetchApprovedMcpServers " + e.toString());
             return Action.ERROR.toUpperCase();
         }
         return Action.SUCCESS.toUpperCase();
@@ -4047,6 +4065,14 @@ public class DbAction extends ActionSupport {
 
     public void setDatadogForwarderConfig(Config.DatadogForwarderConfig datadogForwarderConfig) {
         this.datadogForwarderConfig = datadogForwarderConfig;
+    }
+
+    public List<Map<String, Object>> getApprovedMcpServers() {
+        return approvedMcpServers;
+    }
+
+    public void setApprovedMcpServers(List<Map<String, Object>> approvedMcpServers) {
+        this.approvedMcpServers = approvedMcpServers;
     }
 
     public List<ApiInfo> getApiInfos() {
