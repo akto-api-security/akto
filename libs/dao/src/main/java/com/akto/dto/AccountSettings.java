1276e382fbf2dc1b623fc936db74f9418f75ae4b
gauravakto <gaurav@akto.io>
2026-04-20 10:09:34 +0530

    Threat partition key (#4860)

--- libs/dao/src/main/java/com/akto/dto/AccountSettings.java
+++ libs/dao/src/main/java/com/akto/dto/AccountSettings.java
@@ -38,6 +38,14 @@ public class AccountSettings {
     public static final String CENTRAL_KAFKA_IP = "centralKafkaIp";
     private String centralKafkaIp;
 
+    // Used by mini-runtime to send to threat topic.
+    public static final String THREAT_KAFKA_PARTITION_KEY = "threatKafkaPartitionKey";
+    private ThreatKafkaPartitionKey threatKafkaPartitionKey;
+
+    public enum ThreatKafkaPartitionKey {
+        IP
+    }
+
     public static final String MERGE_ASYNC_OUTSIDE = "mergeAsyncOutside";
     private boolean mergeAsyncOutside;
 
@@ -308,6 +316,14 @@ public class AccountSettings {
 
     public void setCentralKafkaIp(String centralKafkaIp) {
         this.centralKafkaIp = centralKafkaIp;
+    }
+
+    public ThreatKafkaPartitionKey getThreatKafkaPartitionKey() {
+        return threatKafkaPartitionKey;
+    }
+
+    public void setThreatKafkaPartitionKey(ThreatKafkaPartitionKey threatKafkaPartitionKey) {
+        this.threatKafkaPartitionKey = threatKafkaPartitionKey;
     }
 
     public boolean getMergeAsyncOutside() {
