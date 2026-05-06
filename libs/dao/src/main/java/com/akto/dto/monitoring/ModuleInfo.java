b134544ece5665588e11cd8573f3c8e08554c4f8
orenakto <oren+1@akto.io>
2026-05-06 11:39:18 +0530

    resolved comments

--- libs/dao/src/main/java/com/akto/dto/monitoring/ModuleInfo.java
+++ libs/dao/src/main/java/com/akto/dto/monitoring/ModuleInfo.java
@@ -3,9 +3,15 @@ package com.akto.dto.monitoring;
 import java.util.Map;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class ModuleInfo {
 
     public static final String MODULE_TYPE = "moduleType";
@@ -33,21 +39,6 @@ public class ModuleInfo {
     @Getter @Setter
     private boolean deleteTopicAndReboot;
     public static final String DELETE_TOPIC_AND_REBOOT = "deleteTopicAndReboot";
-
-    public ModuleInfo() {}
-
-    public ModuleInfo(ModuleInfo other) {
-        this.moduleType = other.moduleType;
-        this.currentVersion = other.currentVersion;
-        this.id = other.id;
-        this.startedTs = other.startedTs;
-        this.lastHeartbeatReceived = other.lastHeartbeatReceived;
-        this.name = other.name;
-        this.miniRuntimeName = other.miniRuntimeName;
-        this.reboot = other.reboot;
-        this.deleteTopicAndReboot = other.deleteTopicAndReboot;
-        // additionalData intentionally excluded to reduce the size of object stored in MetricData
-    }
 
     public ModuleType getModuleType() {
         return moduleType;
