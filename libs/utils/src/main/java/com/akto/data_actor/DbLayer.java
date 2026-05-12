a472a6fd1d81290dad361b7d3fb6ba28a734768b
shubham <shubham@akto.io>
2026-05-12 16:03:43 +0530

    remove the upsert func

--- libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
+++ libs/utils/src/main/java/com/akto/data_actor/DbLayer.java
@@ -435,12 +435,10 @@ public class DbLayer {
         Bson filter = AccountSettingsDao.generateFilter();
         if (toAdd != null && !toAdd.isEmpty()) {
             List<String> uniqueToAdd = new ArrayList<>(new HashSet<>(toAdd));
-            AccountSettingsDao.instance.getMCollection().updateOne(filter, Updates.addEachToSet(domainKey, uniqueToAdd),
-                    new UpdateOptions().upsert(true));
+            AccountSettingsDao.instance.getMCollection().updateOne(filter, Updates.addEachToSet(domainKey, uniqueToAdd));
         }
         if (toRemove != null && !toRemove.isEmpty()) {
-            AccountSettingsDao.instance.getMCollection().updateOne(filter, Updates.pullAll(domainKey, toRemove),
-                    new UpdateOptions().upsert(true));
+            AccountSettingsDao.instance.getMCollection().updateOne(filter, Updates.pullAll(domainKey, toRemove));
         }
     }
 
