5ee318988be50f4f711c6c9ebe215289ef428c2f
Kuralarasan C <kural@Kuralarasans-MacBook-Pro.local>
2026-06-04 11:20:37 +0530

    change to check account id

--- apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/TestRunResultPage/smartTestingUtils.js
+++ apps/dashboard/web/polaris_web/web/src/apps/dashboard/pages/testing/TestRunResultPage/smartTestingUtils.js
@@ -1,4 +1,4 @@
-export const SMART_TESTING_TRACE_ACCOUNT_ID = 1669322524;
+export const SMART_TESTING_TRACE_ACCOUNT_ID = 1000001;
 
 export function isRunAutomatedTestsEnabled(value) {
     return value === true || value === 'true' || value === 1;
