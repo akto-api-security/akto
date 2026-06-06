cd5ec3aed4216b729cc8daa687a4a8524936f9a2
Ark2307 <kr.aryan2307@gmail.com>
2026-06-06 20:00:35 +0530

    Fixing the backend code and adding support for filters

--- libs/utils/src/main/java/com/akto/utils/elasticsearch/ElasticSearchClient.java
+++ libs/utils/src/main/java/com/akto/utils/elasticsearch/ElasticSearchClient.java
@@ -10,6 +10,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
