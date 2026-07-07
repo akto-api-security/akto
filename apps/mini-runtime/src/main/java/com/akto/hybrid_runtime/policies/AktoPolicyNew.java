4a3a53f36e7b00a970d7aa9156acc7ed520d3b89
harshithb3304 <harshit@akto.io>
2026-07-07 15:49:15 +0530

    updated key name

--- apps/mini-runtime/src/main/java/com/akto/hybrid_runtime/policies/AktoPolicyNew.java
+++ apps/mini-runtime/src/main/java/com/akto/hybrid_runtime/policies/AktoPolicyNew.java
@@ -235,7 +235,7 @@ public class AktoPolicyNew {
 
         if (CONTEXT_SOURCE.ENDPOINT.name().equals(contextSource)){
             Map<String, List<String>> reqHeaders = httpResponseParams.getRequestParams().getHeaders();
-            String skillAgentHeader = RuntimeUtil.getHeaderValue(reqHeaders, "skill-agent");
+            String skillAgentHeader = RuntimeUtil.getHeaderValue(reqHeaders, "skill-tags");
             for (String skillAgent: parseSkillAgents(skillAgentHeader)) {
                 addClassifiedTag(apiInfo, "skill-tags", skillAgent);
             }
