4e892475a01ca15e5224d2e4f03d41e6f0edf396
Kuralarasan C <kural@Kurals-MacBook-Pro.local>
2026-07-16 16:28:20 +0530

    add indexes

--- libs/dao/src/main/java/com/akto/dao/SmartTestingLogsDao.java
+++ libs/dao/src/main/java/com/akto/dao/SmartTestingLogsDao.java
@@ -1,8 +1,37 @@
 package com.akto.dao;
 
+import com.akto.dao.context.Context;
+import com.akto.dto.Log;
+import com.akto.util.DbMode;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
+
 public class SmartTestingLogsDao extends LogsDao {
 
     public static final SmartTestingLogsDao instance = new SmartTestingLogsDao();
+
+    public void createIndicesIfAbsent() {
+        boolean exists = false;
+        String dbName = Context.accountId.get()+"";
+        MongoDatabase db = clients[0].getDatabase(dbName);
+        for (String col: db.listCollectionNames()){
+            if (getCollName().equalsIgnoreCase(col)){
+                exists = true;
+                break;
+            }
+        };
+
+        if (!exists) {
+            if (DbMode.allowCappedCollections()) {
+                db.createCollection(getCollName(), new CreateCollectionOptions().capped(true).maxDocuments(maxDocuments).sizeInBytes(sizeInBytes));
+            } else {
+                db.createCollection(getCollName());
+            }
+        }
+
+        String[] fieldNames = {Log.TIMESTAMP};
+        MCollection.createIndexIfAbsent(getDBName(), getCollName(), fieldNames,false);
+    }
 
     @Override
     public String getCollName() {
