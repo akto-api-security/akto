a216c28050b9d0d66026514ebee51b5a1e6cb852
shubham <shubham@akto.io>
2026-09-03 21:18:30 +0530

    remove unwanted code

--- apps/dashboard/src/main/java/com/akto/action/settings/ModuleInfoAction.java
+++ apps/dashboard/src/main/java/com/akto/action/settings/ModuleInfoAction.java
@@ -31,7 +31,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
