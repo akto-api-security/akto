ff186f30eb3a5cb4d0407ca6ef920a020745c265
TangoBeeAkto <umesh@akto.io>
2026-09-22 13:34:09 +0530

    Refactor database name handling to use DbNames constants in BillingContextDao and CommonContextDao

--- libs/dao/src/main/java/com/akto/dao/BillingContextDao.java
+++ libs/dao/src/main/java/com/akto/dao/BillingContextDao.java
@@ -1,8 +1,10 @@
 package com.akto.dao;
 
+import com.akto.util.DbNames;
+
 public abstract class BillingContextDao<T> extends MCollection<T> {
     @Override
     public String getDBName() {
-        return "billing";
+        return DbNames.BILLING;
     }
 }
